d1d7106592ef39508b8a523994f5b7bb1e65b6e7
Rafael Monnerat <rafael@nexedi.com>
2011-07-13 02:25:01 -0300

    Revert method removal
    This method was used by another function

--- slapos/proxy/views.py
+++ slapos/proxy/views.py
@@ -12,6 +12,21 @@ DB_VERSION = app.open_resource('schema.sql').readline().strip().split(':')[1]
 class UnauthorizedError(Exception):
   pass
 
+def xml2dict(xml):
+  result_dict = {}
+  if xml is not None and xml != '':
+    tree = etree.fromstring(xml.encode('utf-8'))
+    for element in tree.iter(tag=etree.Element):
+      if element.tag == 'parameter':
+        key = element.get('id')
+        value = result_dict.get(key, None)
+        if value is not None:
+          value = value + ' ' + element.text
+        else:
+          value = element.text
+        result_dict[key] = value
+  return result_dict
+
 def partitiondict2partition(partition):
   # XXX-Cedric : change function name, as it does no longer create a
   # Partition instance, but rather create a dict ready to be sent in json

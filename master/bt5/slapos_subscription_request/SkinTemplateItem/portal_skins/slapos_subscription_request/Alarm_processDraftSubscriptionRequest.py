8a578e62a41beae7e654a85179d65c847247c2ce
Rafael Monnerat <rafael@nexedi.com>
2018-10-10 09:34:54 +0000

    slapos_subscription_request: Subscription Request uses simulation state rather them validation state

--- master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/Alarm_processDraftSubscriptionRequest.py
+++ master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/Alarm_processDraftSubscriptionRequest.py
@@ -3,7 +3,7 @@ portal = context.getPortalObject()
 
 portal.portal_catalog.searchAndActivate(
   portal_type="Subscription Request",
-  validation_state="draft",
+  simulation_state="draft",
   method_id="SubscriptionRequest_verifyPaymentTransaction",
   activity_kw={tag: tag}
 

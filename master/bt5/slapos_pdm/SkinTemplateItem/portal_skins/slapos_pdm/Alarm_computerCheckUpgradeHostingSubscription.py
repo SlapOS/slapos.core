8b7a3fe2af84ca7d4e79d925b2a5d177b726a972
Rafael Monnerat <rafael@nexedi.com>
2018-11-27 17:39:10 +0000

    slapos_pdm: Check for upgrade of Hosting Subscription on all Open Computers

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_computerCheckUpgradeHostingSubscription.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_computerCheckUpgradeHostingSubscription.py
@@ -1,19 +1,15 @@
 portal = context.getPortalObject()
+default_allocation_scope_uid = [category.getUid() \
+         for category in portal.portal_categories.allocation_scope.open.objectValues()]
 
-public_scope_uid = portal.restrictedTraverse(
-  "portal_categories/allocation_scope/open/public", None).getUid()
-friend_scope_uid =  portal.restrictedTraverse(
-  "portal_categories/allocation_scope/open/friend", None).getUid()
-personal_scope_uid =  portal.restrictedTraverse(
-  "portal_categories/allocation_scope/open/personal", None).getUid()
-
-if public_scope_uid and friend_scope_uid:
+if default_allocation_scope_uid:
   portal.portal_catalog.searchAndActivate(
     portal_type='Computer',
     validation_state = 'validated',
-    default_allocation_scope_uid=[public_scope_uid, friend_scope_uid, personal_scope_uid],
+    default_allocation_scope_uid=default_allocation_scope_uid,
     method_id = 'Computer_createHostingSubscriptionUpgradeDecision',
+    packet_size=1,
     activate_kw = {'tag':tag}
   )
-  
+
   context.activate(after_tag=tag).getId()

9f50d38b60cabc0f4f65c3c3d0678b65e80ccd2a
Rafael Monnerat <rafael@nexedi.com>
2018-12-12 12:49:22 +0000

    slapos_cloud: Update order of fields

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_assertHostingSubscriptionHasPredecessor.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_assertHostingSubscriptionHasPredecessor.py
@@ -4,10 +4,9 @@ from Products.ZSQLCatalog.SQLCatalog import SimpleQuery, NegatedQuery
 portal.portal_catalog.searchAndActivate(
   portal_type='Hosting Subscription',
   validation_state='validated',
-  where_expression="related_predecessor_title_1_catalog.title <> catalog.title",
+  related_predecessor_but_with_different_title_than_catalog_title="%",
   predecessor_title=NegatedQuery(SimpleQuery(predecessor_title=None, comparison_operator='is')),
   method_id='HostingSubscription_assertPredecessor',
-  activate_kw={'tag': tag}
-)
+  activate_kw={'tag': tag})
 
 context.activate(after_tag=tag).getId()

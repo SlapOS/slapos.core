765c89f8d88eeacc702681ec3daec36785955629
Rafael Monnerat <rafael@nexedi.com>
2018-12-28 21:08:09 +0000

    slapos_accounting: Index with specific tag and set tag on the request
    This indicates on the transaction and on indexation that a payment was created for the invoice. This allow us to avoid duplicated creation due race conditions on activities.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/PaymentTransaction_postOrderBuild.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/PaymentTransaction_postOrderBuild.py
@@ -3,3 +3,11 @@ payment_transaction = context
 
 comment = translateString("Initialised by Order Builder.")
 payment_transaction.confirm(comment=comment)
+
+# Reindex with a tag to ensure that there will be no generation while the object isn't
+# reindexed.
+payment_tag ="sale_invoice_transaction_order_builder_%s" % context.getCausalityUid()
+payment_transaction.activate(tag=payment_tag).immediateReindexObject()
+
+# Set a flag on the request for prevent 2 calls on the same transaction
+context.REQUEST.set(payment_tag, 1)

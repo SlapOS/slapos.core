cb1e6dd71a10d1bc065d4c1278ebf6f359cad43b
Guillaume Hervier <guillaume.hervier@nexedi.com>
2019-01-15 08:38:15 -0500

    slapos_cloud: Computer_viewCurrentUsage: Make column links proper and compatible with RenderJS UI.

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerPartition_getOwnerUrl.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerPartition_getOwnerUrl.py
@@ -1,22 +1,23 @@
 if brain is None:
   brain = context
 
-software_instance = brain.getAggregateRelatedValue(portal_type=["Software Instance"])
-if software_instance is None:
+instance = brain.getAggregateRelatedValue(portal_type=["Software Instance"])
+if instance is None:
   return None
 
+owner = instance.getSpecialiseValue().getDestinationSectionValue()
+
 if url_dict: # If RenderJS UI
-  jio_key = software_instance.getRelativeUrl()
+  jio_key = owner.getRelativeUrl()
   return {
     'command': 'push_history',
     'view_kw': {
       'view': 'view',
       'jio_key': jio_key,
-      'editable': 'true',
     },
     'options': {
       'jio_key': jio_key
     },
   }
 else:
-  return software_instance.absolute_url()
+  return owner.absolute_url()

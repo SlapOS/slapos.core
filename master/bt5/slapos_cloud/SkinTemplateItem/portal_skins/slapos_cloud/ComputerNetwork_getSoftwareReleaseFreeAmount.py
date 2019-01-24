2fe0f7565661c473cf17828af84fbff897fcdc9c
Guillaume Hervier <guillaume.hervier@nexedi.com>
2019-01-24 13:11:32 +0000

    Improve Usable Networks view for Software Release
    - Add free capacity quantity property for Computer, updated by alarm
      when computing capacity scope
    - Refactor said alarm script (split one big function into multiples
      small reusable scripts)
    - Add "Free Instances Amount" column on "Usable Networks" view
      showing the number of free slots for a specific Software
      Release on the public computers in the network
    - Add "Free Instances Amount" column on "Usable Computers" view
      showing the number of free slots for a specific Software
      Release on the computer
    - Remove "Allocation Scope" column from "Usable Networks" view
      as it caused time-out for big networks

---
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerNetwork_getSoftwareReleaseFreeAmount.py
@@ -0,0 +1,12 @@
+network = context
+portal = context.getPortalObject()
+software_release = context.REQUEST.get('here')
+if software_release is None or software_release.portal_type != 'Software Release':
+  software_release = portal.portal_catalog.getResultValue(
+    portal_type='Software Release',
+    url_string=software_release_url,
+  )
+
+return sum([computer.Computer_getSoftwareReleaseFreePartitions(software_release.getUrlString())
+            for computer in network.getSubordinationRelatedValueList()
+            if computer.getAllocationScope() in ['open/public']])

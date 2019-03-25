8daf7af2d333d1d65660c89c31457ed4f1a09224
Rafael Monnerat <rafael@nexedi.com>
2019-03-25 20:16:29 +0000

    slapos_accouting: Don't split the Packing Lists with Consumption information like in Subscription Request
    The Consumption Information should SUM always until the packing list be stopped. The Subscription Request however should be kept separated.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/OrderBuilder_selectSlapOSAggregatedDeliveryList.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/OrderBuilder_selectSlapOSAggregatedDeliveryList.py
@@ -3,8 +3,8 @@
 person_delivery_mapping = {}
 portal = context.getPortalObject()
 
-def newPackingList(movement, causality):
-  return portal.sale_packing_list_module.newContent(
+def newPackingList(movement, causality, message):
+  delivery = portal.sale_packing_list_module.newContent(
         portal_type='Sale Packing List',
         source=movement.getDestination(),
         destination=movement.getDestination(),
@@ -15,22 +15,39 @@ def newPackingList(movement, causality):
         price_currency=movement.getPriceCurrency(),
         causality=causality)
 
+  delivery.confirm(message)
+  return delivery
+
+consumption_specialise_uid_list = [q.getUid() for q in portal.portal_catalog(
+  specialise_uid=portal.business_process_module.slapos_consumption_business_process.getUid(),
+  portal_type='Sale Trade Condition')]
+
 for movement in movement_list:
   person = movement.getDestinationValue()
   causality = movement.getCausality(portal_type="Subscription Request")
+  specialise_uid = movement.getSpecialiseUid()
   try:
-    delivery = person_delivery_mapping["%s---%s" % (person.getUid(), causality)]
+    delivery = person_delivery_mapping["%s---%s---%s" % (person.getUid(), causality, specialise_uid)]
+    if delivery is None:
+      raise KeyError
   except KeyError:
     delivery = person.Person_getAggregatedDelivery()
     if causality is None and (delivery is None or delivery.getSimulationState() != 'confirmed'):
-      delivery = newPackingList(movement, causality)
-      delivery.confirm('New aggregated delivery.')
+      delivery = newPackingList(movement, causality, 'New aggregated delivery.')
       person.Person_setAggregatedDelivery(delivery)
-    elif causality is not None:
-      # If causality is not None, this is a delivery from a subscription request, so
-      # we create a separated Sale Packing List for it.
-      delivery = newPackingList(movement, causality)
-      delivery.confirm('New aggregated delivery for subscription')
 
-    person_delivery_mapping["%s---%s" % (person.getUid(), causality)] = delivery
+    elif causality is not None:
+      if specialise_uid in consumption_specialise_uid_list:
+        subscription_requestion = movement.getCausality(portal_type="Subscription Request")
+        delivery = subscription_requestion.SubscriptionRequest_getAggregatedConsumptionDelivery()
+        if delivery is None or delivery.getSimulationState() != 'confirmed':
+          delivery = newPackingList(movement, causality, 'New aggregated delivery for consumption')
+          subscription_requestion.SubscriptionRequest_setAggregatedConsumptionDelivery(delivery)
+      else:
+        # If causality is not None, this is a delivery from a subscription request, so
+        # we create a separated Sale Packing List for it.
+        delivery = newPackingList(movement, causality, 'New aggregated delivery for subscription')
+
+    person_delivery_mapping["%s---%s---%s" % (person.getUid(), causality, specialise_uid)] = delivery
+
 return person_delivery_mapping.values()

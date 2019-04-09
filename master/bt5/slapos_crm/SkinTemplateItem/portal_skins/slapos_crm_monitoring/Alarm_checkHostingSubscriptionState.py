12f26794306e93b9cc2cd4c2aa87cafe3cbcb2a0
Rafael Monnerat <rafael@nexedi.com>
2019-04-09 03:33:57 +0000

    slapos_crm: Fix query on the alarm

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkHostingSubscriptionState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkHostingSubscriptionState.py
@@ -12,7 +12,7 @@ from Products.ZSQLCatalog.SQLCatalog import Query, NegatedQuery
 portal.portal_catalog.searchAndActivate(
     portal_type='Hosting Subscription',
     validation_state='validated',
-    default_monitor_scope_uid = NegatedQuery(Query(monitor_disabled_category.getUid())),
+    default_monitor_scope_uid = NegatedQuery(Query(default_monitor_scope_uid=monitor_disabled_category.getUid())),
     method_id='HostingSubscription_checkSoftwareInstanceState',
     activate_kw = {'tag':tag}
   )

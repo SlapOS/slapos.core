c05fc1c9caa4973806b32d989e9b54dc2363e1ee
Rafael Monnerat <rafael@nexedi.com>
2019-04-12 04:22:26 +0000

    slapos_cloud: Fix indexation on slapos_item table

--- master/bt5/slapos_cloud/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_slapos_item_list.sql
+++ master/bt5/slapos_cloud/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_slapos_item_list.sql
@@ -1,11 +1,22 @@
-REPLACE INTO
+DELETE FROM
   slapos_item
-  (`uid`, `slap_state`)
+WHERE
+<dtml-in uid>
+  uid=<dtml-sqlvar sequence-item type="int"><dtml-if sequence-end><dtml-else> OR </dtml-if>
+</dtml-in>
+;
+
+<dtml-var "'\0'">
+
+
+INSERT INTO
+  slapos_item
 VALUES
 <dtml-in prefix="loop" expr="_.range(_.len(uid))">
 (
-  <dtml-sqlvar expr="uid[loop_item]" type="int">,  
+  <dtml-sqlvar expr="uid[loop_item]" type="int">,
   <dtml-sqlvar expr="getSlapState[loop_item]" type="string" optional>
 )
 <dtml-if sequence-end><dtml-else>,</dtml-if>
 </dtml-in>
+

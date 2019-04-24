c6fbdfb2f938f6a657d13f47644529b23793b17c
Rafael Monnerat <rafael@nexedi.com>
2019-04-24 16:25:03 +0000

    slapos_erp5: (administration) Add utility script to change id of webpages
    This makes renaming easy before put the web page on a business template.

---
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/WebPage_updateGadgetId.py
@@ -0,0 +1,16 @@
+# Update the document id to be commited
+
+if not context.getId().startswith("201"):
+  raise ValueError("already updated")
+
+if context.getValidationState() not in ["published", "published_alive" ]:
+  raise ValueError("Not published yet, please publish the document first")
+
+reference = context.getReference()
+if reference is None:
+  raise ValueError("Reference is None")
+
+new_id = "rjs_" + reference.replace(".", "_")
+context.setId(new_id)
+
+return context.getRelativeUrl()

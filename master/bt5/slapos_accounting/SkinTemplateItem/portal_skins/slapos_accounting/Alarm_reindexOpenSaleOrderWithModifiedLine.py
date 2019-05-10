e3c2f5efab8c38e03406a629471a7758abd779ea
Rafael Monnerat <rafael@nexedi.com>
2019-05-10 18:43:30 +0000

    slapos_accounting: Only launch activities for the objects that actually changed
    This prevent launch activities for all objects in the list.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Alarm_reindexOpenSaleOrderWithModifiedLine.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Alarm_reindexOpenSaleOrderWithModifiedLine.py
@@ -1,10 +1,10 @@
 portal = context.getPortalObject()
 
 portal.portal_catalog.searchAndActivate(
-  # XXX Filter directly the right open sale order
   method_id='OpenSaleOrder_reindexIfIndexedBeforeLine',
   portal_type="Open Sale Order",
   children_portal_type="Open Sale Order Line",
+  uid=[i.uid for i in portal.ERP5Site_zGetOpenOrderWithModifiedLineUid()],
   activate_kw={'tag': tag},
 )
 
---
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/ERP5Site_zGetOpenOrderWithModifiedLineUid.sql
@@ -0,0 +1,14 @@
+SELECT
+  catalog.uid, catalog.path
+FROM
+  catalog, catalog as open_sale_order_line_catalog
+WHERE
+  1=1
+  AND catalog.path != 'reserved'
+  AND open_sale_order_line_catalog.path != 'reserved'
+  AND catalog.portal_type = "Open Sale Order"
+  AND catalog.uid = open_sale_order_line_catalog.parent_uid
+  AND open_sale_order_line_catalog.portal_type = "Open Sale Order Line"
+  AND open_sale_order_line_catalog.indexation_timestamp > catalog.indexation_timestamp
+group by
+  catalog.uid

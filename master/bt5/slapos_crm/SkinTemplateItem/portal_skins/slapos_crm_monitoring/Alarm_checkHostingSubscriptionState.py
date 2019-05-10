1f18510b9ffa34dccd604ff41ad18d49596528e2
Rafael Monnerat <rafael@nexedi.com>
2019-05-10 01:26:11 +0000

    slapos_crm: Filter by non disabled don't work like this
    The amount of disable should be too small, so no need to worry so much here.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkHostingSubscriptionState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkHostingSubscriptionState.py
@@ -4,15 +4,9 @@ if portal.ERP5Site_isSupportRequestCreationClosed():
   # Stop verification if there are too much tickets
   return
 
-monitor_disabled_category = portal.restrictedTraverse(
-  "portal_categories/monitor_scope/disabled", None)
-
-from Products.ZSQLCatalog.SQLCatalog import Query, NegatedQuery
-
 portal.portal_catalog.searchAndActivate(
     portal_type='Hosting Subscription',
     validation_state='validated',
-    default_monitor_scope_uid = NegatedQuery(Query(default_monitor_scope_uid=monitor_disabled_category.getUid())),
     method_id='HostingSubscription_checkSoftwareInstanceState',
     activate_kw = {'tag':tag}
   )

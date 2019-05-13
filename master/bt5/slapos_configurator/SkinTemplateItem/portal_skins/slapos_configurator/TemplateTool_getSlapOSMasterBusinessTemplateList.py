88caf29ef0af7f78977e276f3583ac0e5f550f03
Thomas Gambier <thomas.gambier@nexedi.com>
2019-05-13 15:23:28 +0200

    don't upgrade broken BT and don't remove imt_slapos_erp5

--- master/bt5/slapos_configurator/SkinTemplateItem/portal_skins/slapos_configurator/TemplateTool_getSlapOSMasterBusinessTemplateList.py
+++ master/bt5/slapos_configurator/SkinTemplateItem/portal_skins/slapos_configurator/TemplateTool_getSlapOSMasterBusinessTemplateList.py
@@ -6,12 +6,14 @@ bt5_update_catalog_list = ('erp5_ingestion_mysql_innodb_catalog',
                            'slapos_cloud', 'erp5_accounting',
                            'erp5_movement_table_catalog')
 
-bt5_installation_list = bt5_update_catalog_list + ('slapos_configurator', 'slapos_erp5')
+bt5_installation_list = bt5_update_catalog_list + ('slapos_configurator', 'slapos_erp5', 'imt_slapos_erp5')
 
 keep_bt5_id_list = ['erp5_ui_test',
                     'erp5_ui_test_core',
                     'slapos_jio_ui_test',
                     'slapos_category',
+                    # XXX erp5_accounting_l10n_fr should be removed when bug is fixed
+                    'erp5_accounting_l10n_fr',
                     'erp5_secure_payment',
                     'slapos_hypermedia',
                     'vifib_datas']

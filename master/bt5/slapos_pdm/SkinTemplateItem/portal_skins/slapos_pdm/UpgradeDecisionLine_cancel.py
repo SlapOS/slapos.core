d074ba1c1fb500635ba401132d87ecb40bf6dd60
Thomas Gambier <thomas.gambier@nexedi.com>
2019-06-14 17:07:21 +0200

    [slapos_pdm]: fix UpgradeDecisionLine_cancel

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/UpgradeDecisionLine_cancel.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/UpgradeDecisionLine_cancel.py
@@ -5,7 +5,8 @@ software_release = context.getAggregateValue(portal_type="Software Release")
 upgrade_decision = context.getParentValue()
 
 if software_release.getValidationState() == "archived":
-  upgrade_decision.cancel(comment="Software Release is archived")
+  upgrade_decision.cancel(comment="Software Release is archived.")
+  return
 
 if hosting_subscription is not None:
   if hosting_subscription.getSlapState() == "destroy_requested":

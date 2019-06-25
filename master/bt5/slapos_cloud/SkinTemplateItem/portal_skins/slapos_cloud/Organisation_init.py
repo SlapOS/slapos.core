15ba91fceb1b2a91c77f8b27a1f83f6ad42612bb
Rafael Monnerat <rafael@nexedi.com>
2019-06-25 02:05:17 +0000

    slapos_cloud: Don't set reference on init script for organisations
    Let the interaction workflow handle it later

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Organisation_init.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Organisation_init.py
@@ -1,12 +1,1 @@
-portal = context.getPortalObject()
-
-if context.getRole() == "host":
-  reference_prefix = "SITE"
-else:
-  reference_prefix = "O"
-
-reference = "%s-%s" % (reference_prefix, portal.portal_ids.generateNewId(
-    id_group='slap_organisation_reference',
-    id_generator='uid'))
-
-context.edit(reference=reference)
+return

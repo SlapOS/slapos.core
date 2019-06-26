b08a0c36779c2b23ef8af930dd6b31b2bb2c2001
Łukasz Nowak <luke@nexedi.com>
2019-06-26 14:17:16 +0200

    Feature computer remove unused software
    /reviewed-on https://lab.nexedi.com/nexedi/slapos.core/merge_requests/121

---
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/SoftwareRelease_findAndDestroySoftwareInstallation.py
@@ -0,0 +1,24 @@
+if context.getValidationState() != 'archived':
+  return
+if context.getSimulationState() == 'cleaned':
+  return
+portal = context.getPortalObject()
+
+catalog_kw = dict(
+  portal_type='Software Installation',
+  validation_state='validated',
+  url_string=context.getUrlString()
+)
+
+count = portal.portal_catalog.countResults(
+  **catalog_kw
+)
+
+if count[0][0] == 0:
+  context.cleanup(comment='No more validated Software Installations found')
+else:
+  portal.portal_catalog.searchAndActivate(
+    method_id = 'SoftwareInstallation_destroyWithSoftwareReleaseArchived',
+    activate_kw = {'tag':tag},
+    **catalog_kw
+  )

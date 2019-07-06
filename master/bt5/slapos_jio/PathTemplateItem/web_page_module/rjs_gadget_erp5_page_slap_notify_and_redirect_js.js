d8627d41056065013a1ffe35c97eb20151223f68
Rafael Monnerat <rafael@nexedi.com>
2019-07-06 15:21:47 +0000

    slapos_jio: Make acceptInvitation renderjs-friendly
    Redirect to a renderjs page to display a message and return to the site

---
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slap_notify_and_redirect_js.js
@@ -0,0 +1,58 @@
+/*globals console, window, rJS, RSVP, loopEventListener, i18n, Handlebars, $*/
+/*jslint indent: 2, nomen: true, maxlen: 80*/
+
+(function (window, rJS, RSVP, Handlebars) {
+  "use strict";
+  var gadget_klass = rJS(window),
+    message_source = gadget_klass.__template_element
+                         .getElementById("message-template")
+                         .innerHTML,
+    message_template = Handlebars.compile(message_source);
+
+  gadget_klass
+    .declareAcquiredMethod("jio_get", "jio_get")
+    .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
+    .declareAcquiredMethod("translateHtml", "translateHtml")
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("updateHeader", "updateHeader")
+
+
+    .declareMethod("getContent", function () {
+      return {};
+    })
+    .declareMethod("render", function (options) {
+      var gadget = this;
+      return new RSVP.Queue()
+        .push(function () {
+          return RSVP.all([
+            gadget.getElement(),
+            gadget.getUrlFor({command: 'change',
+                     options: {jio_key: "/", page: "slapos"}})
+          ]);
+        })
+        .push(function (result) {
+          var redirect_url = result[1],
+              element = result[0],
+              message = options.portal_status_message, advice, page_title;
+          if (options.message_type === "success") {
+            page_title = "Success...";
+          } else if (options.message_type === "error") {
+            page_title = "Fail...";
+          } else {
+            throw new Error("Unknown action to take: " + options.result);
+          }
+          element.innerHTML = message_template({
+            message_to_acknowledge: message,
+            redirect_url: redirect_url
+          });
+          return page_title;
+        })
+        .push(function (page_title) {
+          var header_dict = {
+            page_title: page_title
+          };
+          return gadget.updateHeader(header_dict);
+        });
+    });
+}(window, rJS, RSVP, Handlebars));
--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Project_acceptInvitation.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Project_acceptInvitation.py
@@ -1,36 +1,51 @@
 from zExceptions import Unauthorized
 portal = context.getPortalObject()
 
+person = portal.portal_membership.getAuthenticatedMember().getUserValue()
+if person is None:
+  message_str = "Please login before access the invitation link."
+  return context.REQUEST.RESPONSE.redirect(
+    context.getWebSectionValue().absolute_url() + \
+    "/join_form?portal_status_message=" + \
+    context.Base_translateString(message_str))
+
+def redirect(message, message_type):
+  return context.REQUEST.RESPONSE.redirect(
+    context.getWebSiteValue().absolute_url() + \
+    "/#/?page=slap_notify_and_redirect&message_type=%s" % message_type + \
+    "&portal_status_message=%s" % context.Base_translateString(message))
+
 if invitation_token is None:
-  raise ValueError("Invitation Token is required")
+  message_str = "The Invitation Token is not present on the URL, please review the URL."
+  return redirect(message_str, "error")
 
 if context.getPortalType() != "Project":
-  raise Unauthorized("Context is not an Organisation")
-
-person = portal.portal_membership.getAuthenticatedMember().getUserValue()
-
-if person is None:
-  raise ValueError("You must be logged in!")
+  raise Unauthorized("Context is not an Project, please review your URL.")
 
 try:
   invitation_token = portal.invitation_token_module[invitation_token]
 except KeyError:
-  raise ValueError("Invitation Token is not found.")
+  message_str = "The Invitation Token can't be found, please review the URL."
+  return redirect(message_str, "error")
 
 if invitation_token.getPortalType() != "Invitation Token":
-  raise ValueError("Invitation Token is not found.")
+  message_str = "The Invitation Token can't be found, please review the URL."
+  return redirect(message_str, "error")
 
 if invitation_token.getValidationState() != "validated":
-  raise ValueError("Invitation Token was already used.")
+  message_str = "The Invitation Token was already used and it cannot be reused, please ask a new one."
+  return redirect(message_str, "error")
 
 if invitation_token.getSourceValue() == person:
-  raise ValueError("Invitation Token cannot be used by the same user that generated the token!")
+  message_str = "Invitation Token cannot be used by the same user that generated the token!"
+  return redirect(message_str, "error")
 
 for assignment in person.objectValues(portal_type="Assignment"):
-  if assignment.getSubordination() == context.getRelativeUrl():
+  if assignment.getDestinationProject() == context.getRelativeUrl():
     invitation_token.invalidate(comment="User already has assignment to the Person")
-    return "Already had stuff"
-
+    message_str = "You sucessfully join a new project: %s." % context.getTitle()
+    return redirect(message_str, "success")
+    
 person.newContent(
   title="Assigment for Project %s" % context.getTitle(),
   portal_type="Assignment",
@@ -38,4 +53,5 @@ person.newContent(
 
 invitation_token.invalidate()
 
-return 'Go charlie'
+message_str = "You sucessfully join a new project: %s." % context.getTitle()
+return redirect(message_str, "success")

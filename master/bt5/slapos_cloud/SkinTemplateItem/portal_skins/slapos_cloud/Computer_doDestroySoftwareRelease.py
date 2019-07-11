53c497eb75815773d94794f5654f95aff102bb39
Łukasz Nowak <luke@nexedi.com>
2019-07-11 09:56:34 +0200

    fixup! slapos_cloud: Implement dialog to remove unused SRs from the Computer

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Computer_doDestroySoftwareRelease.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Computer_doDestroySoftwareRelease.py
@@ -4,7 +4,7 @@ removed_amount = 0
 for line in listbox:
   if line['listbox_selected']:
     document = context.restrictedTraverse(line['listbox_key'])
+    document.requestDestroy()
     removed_amount += 1
-    context.requestSoftwareRelease(software_release_url=document.getUrlString(), state='destroyed')
 
 return context.Base_redirect(keep_items=dict(portal_status_message=translateString('Removed ${amount} Software Releases.', mapping={'amount': removed_amount})))

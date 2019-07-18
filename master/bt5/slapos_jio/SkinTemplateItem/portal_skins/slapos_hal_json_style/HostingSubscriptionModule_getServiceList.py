cde0ec751a8e2417fdc34c53863959adf286a591
Rafael Monnerat <rafael@nexedi.com>
2019-07-18 18:48:55 +0000

    slapos_jio: Remove old Hateoas API, the code was migrated to use Jio-based API
      The new API will unify web site and internal Slap API.
    
      Keep slapos_jio_backward_compatibility bt5 as a place to store the API while
      the dependent libraries (like erp5.util) isn't migrated.

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/HostingSubscriptionModule_getServiceList.py
+++
@@ -1,7 +0,0 @@
-from Products.ZSQLCatalog.SQLCatalog import Query, NegatedQuery
-portal = context.getPortalObject()
-return portal.portal_catalog(
-    portal_type="Hosting Subscription",
-    validation_state="validated",
-    query=NegatedQuery(Query(aggregate_related_portal_type="Subscription Request")),
-    **kw)

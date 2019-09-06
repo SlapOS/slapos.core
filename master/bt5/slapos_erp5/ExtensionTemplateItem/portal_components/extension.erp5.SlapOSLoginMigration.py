7faf27740f94eee3ff3d4784cafbb3f54f92d578
Rafael Monnerat <rafael@nexedi.com>
2019-09-06 01:42:42 +0000

    slapos_erp5: In case of migration create Certificate Login instead ERP5 Login

--- master/bt5/slapos_erp5/ExtensionTemplateItem/portal_components/extension.erp5.SlapOSLoginMigration.py
+++ master/bt5/slapos_erp5/ExtensionTemplateItem/portal_components/extension.erp5.SlapOSLoginMigration.py
@@ -1,7 +1,6 @@
 def migrateInstanceToERP5Login(self):
   assert self.getPortalType() in ( 'Computer', 'Software Instance')
 
-  login_portal_type = 'ERP5 Login'
   reference = self.getReference()
   if not reference:
     # no user id and no login is required
@@ -9,12 +8,13 @@ def migrateInstanceToERP5Login(self):
   if not (self.hasUserId() or self.getUserId() == reference):
     self.setUserId(reference)
 
-  if len(self.objectValues(portal_type=login_portal_type)):
+  if len(self.objectValues(
+      portal_type=["Certificate Login", "ERP5 Login"])):
     # already migrated
     return
 
   login = self.newContent(
-    portal_type=login_portal_type,
+    portal_type='Certificate Login',
     reference=reference,
   )
 

1bc9a26424776a87132d33d357b031e81f01d9f1
Rafael Monnerat <rafael@nexedi.com>
2019-09-26 12:28:48 -0300

    Revert "slapos_subscription_request: Add Proof-of-concept Wechat payment support"
    This reverts commit 042bd6bceef0a8f5be73b5c3cdddc98d6f2edf9f.

--- master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/Base_generateWechatQRCodeFromCodeURL.py
+++
@@ -1,3 +0,0 @@
-# inspired by Pack_generateCode128BarcodeImage in sanef-evl project
-code_url = code_url + "&trade_no=" + trade_no
-return context.Base_generateBarcodeImage('qrcode', code_url)
--- master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/SubscriptionRequestModule_requestSubscriptionProxy.py
+++ master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/SubscriptionRequestModule_requestSubscriptionProxy.py
@@ -7,16 +7,15 @@ if REQUEST is not None:
 person, person_is_new = context.SubscriptionRequest_createUser(email, user_input_dict['name'])
 
 web_site = context.getWebSiteValue()
-
-# if payment mode not selected, ask user to confirm and select one.
-if not payment_mode:
+# Check if user is already exist, otherwise redirect to ask confirmation
+if confirmation_required and not person_is_new:
   base_url = web_site.absolute_url()
 
   return context.REQUEST.RESPONSE.redirect(
     "%s/#order_confirmation?name=%s&email=%s&amount=%s&subscription_reference=%s" % (
        base_url,
        person.getTitle(),
-       email,
+       person.getDefaultEmailText(),
        user_input_dict["amount"],
        subscription_reference))
 
@@ -28,28 +27,20 @@ subscription_request = context.subscription_request_module.newContent(
 
 subscription_request.setDefaultEmailText(email)
 
-def wrapWithShadow(subscription_request, amount, subscription_reference, payment_mode):
+def wrapWithShadow(subscription_request, amount, subscription_reference):
   subscription_request.activate(tag="subscription_condition_%s" % subscription_request.getId()
                              ).SubscriptionRequest_applyCondition(subscription_reference)
 
   return subscription_request.SubscriptionRequest_requestPaymentTransaction(amount=amount,
-                                                tag="subscription_%s" % subscription_request.getId(), payment_mode=payment_mode)
+                                                tag="subscription_%s" % subscription_request.getId())
 
 payment = person.Person_restrictMethodAsShadowUser(
   shadow_document=person,
   callable_object=wrapWithShadow,
-  argument_list=[subscription_request, user_input_dict["amount"], subscription_reference, payment_mode])
+  argument_list=[subscription_request, user_input_dict["amount"], subscription_reference])
 
 if batch_mode:
   return {'subscription' : subscription_request.getRelativeUrl(), 'payment': payment.getRelativeUrl() }
-
-if payment_mode == "wechat":
-  portal = context.getPortalObject()
-  code_url = portal.Base_getWechatCodeURL(subscription_request.getId(), payment.PaymentTransaction_getTotalPayablePrice(), user_input_dict["amount"])
-  web_site = context.getWebSiteValue()
-  base_url = web_site.absolute_url()
-  return context.REQUEST.RESPONSE.redirect(
-    "%s/#wechat_payment?amount=%s&trade_no=%s&code_url=%s" % (base_url, user_input_dict["amount"], subscription_request.getId(), code_url))
 
 def wrapRedirectWithShadow(payment_transaction, web_site):
   return payment_transaction.PaymentTransaction_redirectToManualPayzenPayment(web_site)

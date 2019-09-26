ed7c9e3ce8569c7787082dfa98e40ff066b094ec
Lukasz Nowak <luke@nexedi.com>
2019-09-26 08:26:15 +0200

    slapos: Drop slapos.slap.util for slapos.util

--- slapos/util.py
+++ slapos/util.py
@@ -36,8 +36,10 @@ import sqlite3
 from xml_marshaller.xml_marshaller import dumps, loads
 from lxml import etree
 import six
+from six.moves.urllib import parse
 import hashlib
 import collections
+import netaddr
 
 
 def mkdir_p(path, mode=0o700):
@@ -191,3 +193,25 @@ def calculate_dict_hash(d):
         )
       )
     ))).hexdigest()
+
+
+def _addIpv6Brackets(url):
+  # if master_url contains an ipv6 without bracket, add it
+  # Note that this is mostly to limit specific issues with
+  # backward compatiblity, not to ensure generic detection.
+  api_scheme, api_netloc, api_path, api_query, api_fragment = parse.urlsplit(url)
+  try:
+    ip = netaddr.IPAddress(api_netloc)
+    port = None
+  except netaddr.AddrFormatError:
+    try:
+      ip = netaddr.IPAddress(':'.join(api_netloc.split(':')[:-1]))
+      port = api_netloc.split(':')[-1]
+    except netaddr.AddrFormatError:
+      ip = port = None
+  if ip and ip.version == 6:
+    api_netloc = '[%s]' % ip
+    if port:
+      api_netloc = '%s:%s' % (api_netloc, port)
+    url = parse.urlunsplit((api_scheme, api_netloc, api_path, api_query, api_fragment))
+  return url

c1799cde010b93081eb144134e43f8b2bfd88347
Boxiang Sun <boxiang.sun@nexedi.com>
2019-09-27 10:23:35 +0000

    slapos_wechat: Add Wechat payment support

---
+++ master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/ERP5Site_receiveWechatPaymentCallback.py
@@ -0,0 +1,16 @@
+# Example code:
+
+# Import a standard function, and get the HTML request and response objects.
+from Products.PythonScripts.standard import html_quote
+request = container.REQUEST
+response =  request.response
+
+raise Exception(request)
+# Return a string identifying this script.
+print "This is the", script.meta_type, '"%s"' % script.getId(),
+if script.title:
+  print "(%s)" % html_quote(script.title),
+print "in", container.absolute_url()
+print response
+
+return printed

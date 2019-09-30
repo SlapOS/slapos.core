c54dfddcca979788f4f6d4dd6e9a1cb475108105
Boxiang Sun <boxiang.sun@nexedi.com>
2019-09-30 15:02:18 +0000

    slapos_wechat: Wechat payment improvement

---
+++ master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/Alarm_updateWechatStartedPaymentTransaction.py
@@ -0,0 +1,11 @@
+portal = context.getPortalObject()
+portal.portal_catalog.searchAndActivate(
+      portal_type="Payment Transaction",
+      simulation_state=["started"],
+      causality_state=["draft"],
+      payment_mode_uid=portal.portal_categories.payment_mode.wechat.getUid(),
+      method_id='PaymentTransaction_updateWechatPaymentStatus',
+      packet_size=1, # just one to minimise errors
+      activate_kw={'tag': tag}
+      )
+context.activate(after_tag=tag).getId()
---
+++ master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/PaymentTransaction_updateWechatPaymentStatus.py
@@ -0,0 +1,13 @@
+from DateTime import DateTime
+portal = context.getPortalObject()
+
+state = context.getSimulationState()
+if (state != 'started') or (context.getPaymentMode() != 'wechat'):
+  return
+else:
+  # ???
+  transaction_date, transaction_id = context.PaymentTransaction_getPayzenId()
+
+  if transaction_id is not None:
+    # so the payment is registered in payzen
+    context.PaymentTransaction_createWechatEvent().updateStatus()

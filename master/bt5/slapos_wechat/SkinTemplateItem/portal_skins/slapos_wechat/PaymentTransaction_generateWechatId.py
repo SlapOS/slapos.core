5e47b6d3af0e9745a4f40a4cf72729d859c21672
Thomas Gambier <thomas.gambier@nexedi.com>
2019-11-05 16:48:43 +0100

    slapos_wechat: add module to pay with Wechat
    mostly copied from slapos_payzen

---
+++ master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/PaymentTransaction_generateWechatId.py
@@ -0,0 +1,26 @@
+from zExceptions import Unauthorized
+if REQUEST is not None:
+  raise Unauthorized
+
+portal = context.getPortalObject()
+integration_site = portal.restrictedTraverse(portal.portal_preferences.getPreferredWechatIntegrationSite())
+
+transaction_date, transaction_id = context.PaymentTransaction_getWechatId()
+if transaction_id is not None:
+  # XXX raise?
+  return None, None
+
+#### Use the same ID as Payment
+#### we keep the mapping just to be more like Payzen Payment
+mapping_id = context.getId()
+
+try:
+  mapping = integration_site.getCategoryFromMapping(
+  'Causality/%s' % context.getId().replace('-', '_'),
+  create_mapping_line=True,
+  create_mapping=True)
+except ValueError:
+  pass
+integration_site.Causality[context.getId().replace('-', '_')].setDestinationReference(mapping_id)
+
+return context.PaymentTransaction_getWechatId()

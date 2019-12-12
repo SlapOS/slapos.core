982fbf1014d758bf8d04596a33b16aaae2d1229d
Rafael Monnerat <rafael@nexedi.com>
2019-12-12 03:54:04 +0000

    slapos_payzen: Use wechat if the currency is CNY
    This isn't the most suitable script for this anymore or some extra configurations can be used.

--- master/bt5/slapos_payzen/SkinTemplateItem/portal_skins/slapos_payzen/Currency_getIntegrationMapping.py
+++ master/bt5/slapos_payzen/SkinTemplateItem/portal_skins/slapos_payzen/Currency_getIntegrationMapping.py
@@ -1,7 +1,12 @@
 portal = context.getPortalObject()
-integration_site = portal.restrictedTraverse(portal.portal_preferences.getPreferredPayzenIntegrationSite())
-
 relative_url = context.getRelativeUrl()
+
+if relative_url == 'currency_module/CNY':
+  integration_site = portal.restrictedTraverse(portal.portal_preferences.getPreferredWechatIntegrationSite())
+else:
+  integration_site = portal.restrictedTraverse(portal.portal_preferences.getPreferredPayzenIntegrationSite())
+
+
 # Only EUR is supported for now
-assert relative_url == 'currency_module/EUR'
+assert relative_url in ('currency_module/EUR', 'currency_module/CNY')
 return integration_site.getMappingFromCategory('resource/%s' % relative_url).split('/', 1)[-1]

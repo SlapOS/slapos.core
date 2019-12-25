8ad5ba4d01450c8a236d9aea3b71c70ad525fada
Jérome Perrin <jerome@nexedi.com>
2019-12-26 00:42:38 +0100

    testing/testcase: cleanups
    Fix long lines, incorrect type annotations and adjust a bit to make mypy
    happy.

--- slapos/testing/testcase.py
+++ slapos/testing/testcase.py
@@ -36,7 +36,7 @@ from six.moves.urllib.parse import urlparse
 try:
   import subprocess32 as subprocess
 except ImportError:
-  import subprocess
+  import subprocess  # type: ignore
   subprocess  # pyflakes
 
 from .utils import getPortFromPath
@@ -48,7 +48,7 @@ from ..grid.utils import md5digest
 from ..util import mkdir_p
 
 try:
-  from typing import Iterable, Tuple, Callable, Type
+  from typing import Iterable, Tuple, Callable, Type, Dict, List, Optional
 except ImportError:
   pass
 
@@ -62,9 +62,9 @@ def makeModuleSetUpAndTestCaseClass(
     verbose=bool(int(os.environ.get('SLAPOS_TEST_VERBOSE', 0))),
     shared_part_list=os.environ.get('SLAPOS_TEST_SHARED_PART_LIST',
                                     '').split(os.pathsep),
-    snapshot_directory=os.environ.get('SLAPOS_TEST_LOG_DIRECTORY')
+    snapshot_directory=os.environ.get('SLAPOS_TEST_LOG_DIRECTORY'),
 ):
-  # type: (str, str, str, str, bool, bool, List[str]) -> Tuple[Callable[[], None], Type[SlapOSInstanceTestCase]]
+  # type: (str, str, str, str, bool, bool, Iterable[str], Optional[str]) -> Tuple[Callable[[], None], Type[SlapOSInstanceTestCase]]
   """Create a setup module function and a testcase for testing `software_url`.
 
   This function returns a tuple of two arguments:
@@ -138,7 +138,9 @@ def makeModuleSetUpAndTestCaseClass(
           '_test_file_snapshot_directory': snapshot_directory
       })
 
-  class SlapOSInstanceTestCase_(cls, SlapOSInstanceTestCase):
+  class SlapOSInstanceTestCase_(
+      cls,  # type: ignore # https://github.com/python/mypy/issues/2813
+      SlapOSInstanceTestCase):
     # useless intermediate class so that editors provide completion anyway.
     pass
 
@@ -503,11 +505,12 @@ class SlapOSInstanceTestCase(unittest.TestCase):
       cls._storeSystemSnapshot("{}._cleanup leaked_partitions".format(name))
       for cp in leaked_partitions:
         try:
+          # XXX is this really the reference ?
+          partition_reference = cp.getInstanceParameterDict()['instance_title']
           cls.slap.request(
               software_release=cp.getSoftwareRelease().getURI(),
               # software_type=cp.getType(), # TODO
-              # XXX is this really the reference ?
-              partition_reference=cp.getInstanceParameterDict()['instance_title'],
+              partition_reference=partition_reference,
               state="destroyed")
         except:
           cls.logger.exception(
@@ -527,10 +530,14 @@ class SlapOSInstanceTestCase(unittest.TestCase):
       cls.logger.exception("Error during stop")
       cls._storeSystemSnapshot("{}._cleanup stop".format(name))
     leaked_supervisor_configs = glob.glob(
-        os.path.join(cls.slap.instance_directory, 'etc', 'supervisord.conf.d', '*.conf'))
+        os.path.join(
+            cls.slap.instance_directory, 'etc', 'supervisord.conf.d', '*.conf'))
     if leaked_supervisor_configs:
-      [os.unlink(config) for config in leaked_supervisor_configs]
-      raise AssertionError("Test leaked supervisor configurations: %s" % leaked_supervisor_configs)
+      for config in leaked_supervisor_configs:
+        os.unlink(config)
+      raise AssertionError(
+          "Test leaked supervisor configurations: %s" %
+          leaked_supervisor_configs)
 
   @classmethod
   def requestDefaultInstance(cls, state='started'):

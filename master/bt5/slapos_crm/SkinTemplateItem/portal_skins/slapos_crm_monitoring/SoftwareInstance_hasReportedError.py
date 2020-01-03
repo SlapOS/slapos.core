9024402b2d7ac8bf8818ec266fd5636c0aabdcb6
Rafael Monnerat <rafael@nexedi.com>
2020-01-03 22:53:56 +0000

    slapos_crm: Extent SoftwareInstance_hasReportedError to make it more flexible
    Allow it report the message and creation date, which can be usable on other contexts.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/SoftwareInstance_hasReportedError.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/SoftwareInstance_hasReportedError.py
@@ -8,19 +8,23 @@ if context.getAggregateValue(portal_type="Computer Partition") is not None:
   except KeyError:
     if include_message:
       return "Not possible to connect"
-    return  
+    return
 
   d = json.loads(d)
   result = d['text']
-  
+  last_contact = DateTime(d.get('created_at'))
+
   # Optimise by checking memcache information first.
   if result.startswith('#error '):
+    if include_created_at:
+      return result, last_contact
     return result
 
-  #last_contact = DateTime(d.get('created_at'))
   # XXX time limit of 48 hours for run at least once.
+  if include_message and include_created_at:
+    return result, last_contact
 
-  if include_message:
+  if include_message and not include_created_at:
     return result
 
 return None

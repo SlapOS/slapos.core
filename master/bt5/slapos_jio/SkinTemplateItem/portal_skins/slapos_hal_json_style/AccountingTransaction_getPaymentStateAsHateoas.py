9bd2ad5a9b64c7b44cfa104eae3c7d37e3324ffd
Thomas Gambier <thomas.gambier@nexedi.com>
2020-01-07 14:37:58 +0100

    slapos_jio: fix AccountingTransaction_getPaymentStateAsHateoas after f512ad84c587702066c6a981885f039b16c31875

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/AccountingTransaction_getPaymentStateAsHateoas.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/AccountingTransaction_getPaymentStateAsHateoas.py
@@ -3,7 +3,7 @@ import json
 state = context.AccountingTransaction_getPaymentState()
 payment_transaction = None
 if state == "Pay now":
-  payment_transaction = context.SaleInvoiceTransaction_getPayzenPaymentRelatedValue().getRelativeUrl()
+  payment_transaction = context.SaleInvoiceTransaction_getSlapOSPaymentRelatedValue().getRelativeUrl()
 
 return json.dumps({"state": state,
                    "payment_transaction": payment_transaction})

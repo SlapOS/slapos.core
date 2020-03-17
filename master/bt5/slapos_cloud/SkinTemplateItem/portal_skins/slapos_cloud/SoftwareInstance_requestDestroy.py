5e4851c2464376a01e563d4c0064ede4570d196b
Rafael Monnerat <rafael@nexedi.com>
2020-03-17 20:09:56 +0000

    slapos_cloud: Also destroy all Slaves allocated at the instance
    This is helpfull to allow instances be re-requested and allocated by their respective Slave Instances

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/SoftwareInstance_requestDestroy.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/SoftwareInstance_requestDestroy.py
@@ -5,8 +5,8 @@ portal = context.getPortalObject()
 if portal.portal_membership.isAnonymousUser():
   raise Unauthorized("You cannot invoke this API as Annonymous")
 
-#if context.SoftwareInstance_isUserAllowedToDestroy():
-#  raise Unauthorized("You cannot destroy this %s." % context.getPortalType())
+if not context.SoftwareInstance_isUserAllowedToDestroy():
+  raise Unauthorized("You cannot destroy this %s." % context.getPortalType())
 
 if context.getSlapState() not in ["start_requested", "stop_requested"]:
   return context.Base_redirect(keep_items={"portal_status_message": "This %s is on %s state and cannot be destroyed." \
@@ -15,5 +15,14 @@ if context.getSlapState() not in ["start_requested", "stop_requested"]:
 # It is used a external method as portal_slap._storeLastData cannot be invoked
 # from a python script
 context.SoftwareInstance_renameAndRequestDestroy()
+
+# Request Destroy on all Slaves allocated on the same partition
+computer_partition = context.getAggregateValue(
+  portal_type="Computer Partition")
+
+if computer_partition is not None:
+  for slave in computer_partition.getAggregateRelatedValueList(
+    portal_type="Slave Instance"):
+    slave.SoftwareInstance_renameAndRequestDestroy()
 
 return context.Base_redirect(keep_items={"portal_status_message": "Destroy requested."})

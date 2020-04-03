ee87fef44c2cd16543a16cc6207cf69a7e75203c
Łukasz Nowak <luke@nexedi.com>
2020-04-03 10:05:40 +0200

    fixup! slapgrid: Support [manager:x] sections

--- slapos/grid/slapgrid.py
+++ slapos/grid/slapgrid.py
@@ -208,11 +208,11 @@ def merged_options(args, configp):
             "reload_config_cmd",
             "slapos node restart firewall")
 
-  key = 'manager:'
+  key = 'manager'
   options[key] = {}
   for section in configp.sections():
     if section.startswith(key):
-      manager = section[len(key):]
+      manager = section[len(key)+1:]
       options[key][manager] = dict(configp.items(section))
   return options
 

5f2aa3feaf26e826be5beb8f9117f7d783d62c8d
lu.xu <lu.xu@nexedi.com>
2020-04-09 22:58:54 +0000

    slapos_jio: page_slap_controller translate portal_type inproperly

--- master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slap_controller_js.js
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slap_controller_js.js
@@ -71,7 +71,7 @@
             child_gadget_url = "gadget_erp5_page_slap_site_view.html";
           } else if (result.portal_type !== undefined) {
             child_gadget_url = 'gadget_erp5_page_slap_' +
-              result.portal_type.replace(/ /g, '_').toLowerCase() +
+              result.parent_relative_url.replace(/_module/g, '').toLowerCase() +
               '_view.html';
           } else {
             throw new Error('Can not display document: ' + options.jio_key);

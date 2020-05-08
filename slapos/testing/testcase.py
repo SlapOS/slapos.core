0889395fd012567d178b329240bc43a674fbc2b0
Jérome Perrin <jerome@nexedi.com>
2020-05-08 15:35:07 +0900

    Revert "WIP log"
    This reverts commit 3160c589daf8a85e465020cb86e2c4840be407c6.
    
    pushed by mistake. This is unfinished but it was wrong because we copy
    this log file to snapshot directory before we are done writing to it.

--- slapos/testing/testcase.py
+++ slapos/testing/testcase.py
@@ -152,14 +152,7 @@ def makeModuleSetUpAndTestCaseClass(
     if debug:
       unittest.installHandler()
     logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        filename=os.path.join(slap._base_directory, 'var', 'log', 'test.log'))
-    logger = logging.getLogger()
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(
-        logging.DEBUG if (verbose or debug) else logging.WARNING)
-    logger.addHandler(console_handler)
+        level=logging.DEBUG if (verbose or debug) else logging.WARNING)
     installSoftwareUrlList(cls, [software_url], debug=debug)
 
   return setUpModule, SlapOSInstanceTestCase_
@@ -369,9 +362,7 @@ def installSoftwareUrlList(cls, software_url_list, max_retry=2, debug=False):
     cls.slap.waitForSoftware(max_retry=max_retry, debug=debug)
     _storeSoftwareSnapshot('setupModule')
     for software_url in software_url_list:
-      cls.logger.debug("Checking software %s", software_url)
       checkSoftware(cls.slap, software_url)
-      cls.logger.debug("Done checking software %s", software_url)
   except BaseException as e:
     if not debug:
       cls.logger.exception("Error building software, removing")

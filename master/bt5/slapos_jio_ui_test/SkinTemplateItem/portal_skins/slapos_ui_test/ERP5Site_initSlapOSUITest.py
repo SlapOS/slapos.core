e26a9adddb63a523b37856826539c379b2639257
Rafael Monnerat <rafael@nexedi.com>
2020-05-28 19:20:22 +0000

    slapos_jio_ui_test: Ensure Authentication Policy is enabled

--- master/bt5/slapos_jio_ui_test/SkinTemplateItem/portal_skins/slapos_ui_test/ERP5Site_initSlapOSUITest.py
+++ master/bt5/slapos_jio_ui_test/SkinTemplateItem/portal_skins/slapos_ui_test/ERP5Site_initSlapOSUITest.py
@@ -7,7 +7,8 @@ preference = portal.portal_preferences.getActiveSystemPreference()
 preference.edit(
   preferred_credential_alarm_automatic_call=1,
   preferred_credential_recovery_automatic_approval=1,
-  preferred_credential_request_automatic_approval=1
+  preferred_credential_request_automatic_approval=1,
+  preferred_authentication_policy_enabled=1
 )
 
 try:

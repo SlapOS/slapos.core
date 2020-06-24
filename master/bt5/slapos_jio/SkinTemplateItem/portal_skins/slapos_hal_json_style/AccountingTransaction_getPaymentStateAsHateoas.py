5a601e00e03d6a20043c06b3d5248f2321679bb9
Rafael Monnerat <rafael@nexedi.com>
2020-06-24 23:40:05 +0000

    slapos_jio:  Display translated state on  "State" columns for invoices
        Also inform payment_mode to let the script handle the display of information

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/AccountingTransaction_getPaymentStateAsHateoas.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/AccountingTransaction_getPaymentStateAsHateoas.py
@@ -2,8 +2,12 @@ import json
 
 state = context.AccountingTransaction_getPaymentState()
 payment_transaction = None
+payment_mode = None
 if state == "Pay now":
-  payment_transaction = context.SaleInvoiceTransaction_getSlapOSPaymentRelatedValue().getRelativeUrl()
+  payment_transaction_value = context.SaleInvoiceTransaction_getSlapOSPaymentRelatedValue()
+  payment_transaction = payment_transaction_value.getRelativeUrl()
+  payment_mode = payment_transaction_value.getPaymentMode()
 
-return json.dumps({"state": state,
+return json.dumps({"state": context.Base_translateString(state),
+                   "payment_mode": payment_mode,
                    "payment_transaction": payment_transaction})

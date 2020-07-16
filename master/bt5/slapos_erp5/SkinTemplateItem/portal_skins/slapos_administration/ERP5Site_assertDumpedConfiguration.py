f1bd3cfcdf1715823ffce38d139a4bba0402e031
Rafael Monnerat <rafael@nexedi.com>
2020-07-16 23:32:28 +0000

    slapos_erp5: Update to define more ignored items

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/ERP5Site_assertDumpedConfiguration.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/ERP5Site_assertDumpedConfiguration.py
@@ -26,7 +26,12 @@ if show_all or configuration == "builder":
 if show_all or configuration == "bt":
   expected_file = portal.expected_business_template_dumped_configuration
   found = portal.ERP5Site_dumpInstalledBusinessTemplateList(
-    ignore_business_template_list=["rapid_space_web_site"])
+    ignore_business_template_list=["rapid_space_web_site",
+                                   'erp5_ui_test_core',
+                                   'slapos_vifib',
+                                   'slapos_jio_zh_ui_test',
+                                   'slapos_jio_ui_test',
+                                   'rapid_space_ui_test'])
   print resolveDiff("Business Template Configuration Diff", expected_file, found)
 
 if show_all or configuration == "order_builder":
@@ -36,12 +41,38 @@ if show_all or configuration == "order_builder":
 
 if show_all or configuration == "property_sheet":
   expected_file = portal.expected_property_sheet_dumped_configuration
-  found = portal.ERP5Site_dumpPropertySheetList()
+  found = portal.ERP5Site_dumpPropertySheetList(
+    ignore_property_sheet_list=["Foo"]
+  )
   print resolveDiff("Property Sheet Configuration Diff", expected_file, found)
 
 if show_all or configuration == "skins":
   expected_file = portal.expected_portal_skins_dumped_configuration
-  found = portal.ERP5Site_dumpPortalSkinsContent()
+  found = portal.ERP5Site_dumpPortalSkinsContent(
+    ignore_folder_list=[
+      "rapid_space",
+      "slapos_vifib",
+      "rapid_space_ui_test",
+      "slapos_ui_test",
+      "slapos_zh_ui_test",
+      "erp5_web_renderjs_ui_test_core"
+    ],
+    ignore_skin_list=[
+      "expected_alarm_tool_dumped_configuration",
+      "expected_builder_dumped_configuration",
+      "expected_business_template_dumped_configuration",
+      "expected_order_builder_dumped_configuration",
+      "expected_portal_skins_dumped_configuration",
+      "expected_portal_type_dumped_configuration",
+      "expected_property_sheet_dumped_configuration",
+      "expected_role_dumped_configuration",
+      "expected_rule_dumped_configuration",
+      "expected_skin_property_dumped_configuration",
+      "expected_type_actions_dumped_configuration",
+      "expected_web_page_module_configuration",
+      "expected_workflow_dumped_configuration",
+    ]
+  )
   print resolveDiff("Skins Configuration Diff", expected_file, found)
 
 if show_all or configuration == "web_content":
@@ -72,7 +103,16 @@ if show_all or configuration == "rule":
 
 if show_all or configuration == "skin_property":
   expected_file = portal.expected_skin_property_dumped_configuration
-  found = portal.ERP5Site_dumpSkinProperty()
+  found = portal.ERP5Site_dumpSkinProperty(
+    ignore_skin_folder_list=[
+      "rapid_space",
+      "slapos_vifib",
+      "rapid_space_ui_test",
+      "slapos_ui_test",
+      "slapos_zh_ui_test",
+      "erp5_web_renderjs_ui_test_core"
+    ]
+  )
   print resolveDiff("Skin Property Configuration Diff", expected_file, found)
 
 if show_all or configuration == "workflow":

ec5df04cc29e31a23bcc01bf6f3e10e033468892
Rafael Monnerat <rafael@nexedi.com>
2020-07-31 02:57:26 +0000

    slapos_erp5: Add testSlapOSCodingStyle for SlapOS

---
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSCodingStyle.py
@@ -0,0 +1,68 @@
+# -*- coding: utf8 -*-
+##############################################################################
+#
+# Copyright (c) 2020 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+##############################################################################
+
+import unittest
+from Products.ERP5Type.tests.CodingStyleTestCase import CodingStyleTestCase
+
+from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin
+
+def makeTestSlapOSCodingStyleTestCase(tested_business_template):
+  class TestSlapOSCodingStyle(CodingStyleTestCase, SlapOSTestCaseMixin):
+    """Runs CodingStyleTestCase checks on slapos business templates
+    """
+    def afterSetUp(self):
+      CodingStyleTestCase.afterSetUp(self)
+      SlapOSTestCaseMixin.afterSetUp(self)
+
+    def getBusinessTemplateList(self):
+      # include administration for test_PythonSourceCode
+      # This method is not used to install business templates in live test, but
+      # we define it for CodingStyleTestCase.test_PythonSourceCode
+      return ('erp5_administration', )
+
+  return type("TestSlapOSCodingStyle.%s" % tested_business_template,
+              (TestSlapOSCodingStyle,),
+              {"getTestedBusinessTemplateList": lambda self: (tested_business_template, )})
+
+
+def test_suite():
+  """Generate test to check statically each business template
+  """
+  suite = unittest.TestSuite()
+
+  from Products.ERP5 import ERP5Site
+  portal_templates = ERP5Site.getSite().portal_templates
+  bt5 = portal_templates.getInstalledBusinessTemplate("slapos_erp5")
+  dependency_list = set(bt5.getTestDependencyList() + bt5.getDependencyList())
+
+  for _, bt in portal_templates.resolveBusinessTemplateListDependency(dependency_list):
+    if bt.startswith('slapos_'):
+      suite.addTest(unittest.makeSuite(makeTestSlapOSCodingStyleTestCase(bt)))
+
+  suite.addTest(unittest.makeSuite(makeTestSlapOSCodingStyleTestCase('slapos_erp5')))
+  return suite

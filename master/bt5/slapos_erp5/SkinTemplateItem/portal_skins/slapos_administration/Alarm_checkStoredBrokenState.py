f36816f667a8115d425e77128b7a94998e736705
Rafael Monnerat <rafael@nexedi.com>
2020-08-12 21:25:34 +0000

    slapos_erp5: Add alarm to search for objects with Broken_state

---
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/Alarm_checkStoredBrokenState.py
@@ -0,0 +1,3 @@
+context.portal_catalog.searchAndActivate(
+  method_id="Base_checkStoredBrokenState",
+  activate_kw={"tag": tag})
---
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/Base_checkStoredBrokenState.py
@@ -0,0 +1,2 @@
+assert '__Broken_state__' not in context.showDict()
+assert '__Broken_newargs__' not in context.showDict()

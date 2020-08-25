f55ffdc731c07d98e542a7072c09695bbc32b461
Rafael Monnerat <rafael@nexedi.com>
2020-08-25 20:32:32 +0000

    slapos_accounting: Introduce a simplified version for calculate user's debt to pay
       This is a faster version to later accomodate custom implementation from Cloud Contracts

---
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Entity_statOutstandingAmount.py
@@ -0,0 +1,19 @@
+from Products.ZSQLCatalog.SQLCatalog import SimpleQuery, ComplexQuery
+portal = context.getPortalObject()
+params = {"node_category_strict_membership": ['account_type/asset/receivable',
+                                               'account_type/liability/payable']}
+from DateTime import DateTime
+if at_date:
+  params['at_date'] = at_date
+  params['grouping_query'] = ComplexQuery(
+      SimpleQuery(grouping_reference=None),
+      SimpleQuery(grouping_date=at_date, comparison_operator=">"),
+      logical_operator="OR")
+else:
+  params['grouping_reference'] = None
+
+return portal.portal_simulation.getInventoryAssetPrice(
+              mirror_section_uid = context.getUid(),
+              simulation_state=('stopped', 'delivered'),
+              portal_type=portal.getPortalAccountingMovementTypeList(),
+              **params)

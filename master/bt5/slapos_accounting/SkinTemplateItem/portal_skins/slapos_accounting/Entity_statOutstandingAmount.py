61b64044a77430141b85e861bfdebb9d16e54ff0
Rafael Monnerat <rafael@nexedi.com>
2020-08-27 16:07:59 +0000

    slapos_accounting: Calculate User's "Outstanding" Amounts on SlapOS
        This consider Maximum Delay and Maximum Credit a user can have

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Entity_statOutstandingAmount.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Entity_statOutstandingAmount.py
@@ -2,7 +2,6 @@ from Products.ZSQLCatalog.SQLCatalog import SimpleQuery, ComplexQuery
 portal = context.getPortalObject()
 params = {"node_category_strict_membership": ['account_type/asset/receivable',
                                                'account_type/liability/payable']}
-from DateTime import DateTime
 if at_date:
   params['at_date'] = at_date
   params['grouping_query'] = ComplexQuery(
@@ -11,6 +10,9 @@ if at_date:
       logical_operator="OR")
 else:
   params['grouping_reference'] = None
+
+if resource_uid is not None:
+  params["resource_uid"] = resource_uid
 
 return portal.portal_simulation.getInventoryAssetPrice(
               mirror_section_uid = context.getUid(),

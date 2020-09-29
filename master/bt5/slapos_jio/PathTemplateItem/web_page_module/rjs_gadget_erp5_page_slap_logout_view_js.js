14387e2d29af39c22cc3d1dba72eb4bff09c5932
lu.xu <lu.xu@nexedi.com>
2020-09-29 13:19:40 +0000

    slapos_jio: add Language page

---
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slap_logout_view_js.js
@@ -0,0 +1,67 @@
+/*global window, rJS, UriTemplate */
+/*jslint indent: 2, maxerr: 3, nomen: true */
+(function (window, rJS, UriTemplate) {
+  "use strict";
+
+  rJS(window)
+    /////////////////////////////////////////////////////////////////
+    // handle acquisition
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("redirect", "redirect")
+    .declareAcquiredMethod("getUrlFor", "getUrlFor")
+    .declareAcquiredMethod("getUrlForList", "getUrlForList")
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
+    .declareAcquiredMethod("translate", "translate")
+    .declareAcquiredMethod("updateHeader", "updateHeader")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+    .declareMethod("render", function () {
+      var gadget = this;
+
+      return gadget.getUrlFor([
+        // Back url
+        {command: 'display'}
+      ])
+        .push(function (url) {
+          return gadget.updateHeader({
+            page_title: 'Logout',
+            page_icon: 'power-off',
+            front_url: url
+          });
+        })
+        .push(function () {
+          return gadget.translate('Confirm');
+        })
+        .push(function (translated_text) {
+          gadget.element.querySelector('input').value = translated_text;
+        });
+    })
+
+    .onEvent('submit', function () {
+      var gadget = this,
+        logout_url_template;
+
+      return gadget.jio_getAttachment('acl_users', 'links')
+        .push(function (links) {
+          logout_url_template = links._links.logout.href;
+          return gadget.getUrlFor({
+            command: 'display',
+            absolute_url: true,
+            options: {}
+          });
+        })
+        .push(function (came_from) {
+          return gadget.redirect({
+            command: 'raw',
+            options: {
+              url: UriTemplate.parse(logout_url_template).expand({came_from: came_from})
+            }
+          });
+        });
+    })
+    .declareMethod("triggerSubmit", function () {
+      return;
+    });
+}(window, rJS, UriTemplate));

ca2d43242911b7b34598b664546e654ce7ca996a
Rafael Monnerat <rafael@nexedi.com>
2020-12-31 21:52:53 +0000

    slapos_crm: Minor update on a search from the instance

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/SupportRequest_getInstanceMonitorUrl.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/SupportRequest_getInstanceMonitorUrl.py
@@ -4,8 +4,12 @@ hosting_subscription = support_request.getAggregateValue(portal_type="Hosting Su
 if hosting_subscription is None:
   return
 
-instance = hosting_subscription.getPredecessorValue()
-if instance is None or instance.getSlapState() == 'destroy_requested':
+instance = None
+for possible_instance in hosting_subscription.getPredecessorValueList():
+  if possible_instance.getSlapState() != 'destroy_requested':
+    instance = possible_instance
+    break
+if instance is None:
   return
 
 parameter_dict = instance.getConnectionXmlAsDict()

dc3afa4fab7af517be7a9f3a60f0515a11d87398
Rafael Monnerat <rafael@nexedi.com>
2021-02-25 21:17:15 +0000

    slapos_accounting: Do not stop Invoice w/o Sale Invoice Transaction Lines
        This forces to wait for builders before stop

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Delivery_stopConfirmedAggregatedSaleInvoiceTransaction.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Delivery_stopConfirmedAggregatedSaleInvoiceTransaction.py
@@ -5,6 +5,7 @@ isTransitionPossible = portal.portal_workflow.isTransitionPossible
 if context.getSimulationState() == 'confirmed'\
   and len(context.checkConsistency()) == 0\
   and context.getCausalityState() == 'solved'\
+  and len(context.objectValues(portal_type="Sale Invoice Transaction Line"))\
   and context.getSpecialise() in [portal.portal_preferences.getPreferredAggregatedSaleTradeCondition(),
                                   portal.portal_preferences.getPreferredAggregatedSubscriptionSaleTradeCondition()]:
   comment = 'Stopped by alarm as all actions in confirmed state are ready.'

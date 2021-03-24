3a8e001f87a39475507d20ae8c331543daafc873
Rafael Monnerat <rafael@nexedi.com>
2021-03-24 16:01:41 +0000

    slapos_rss_style: Removed unsused RSS form

--- master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/WebSection_getWebSectionPublicationList.py
+++
@@ -1,8 +0,0 @@
-portal = context.getPortalObject()
-kw['portal_type'] = ["External RSS Item"]
-
-kw['validation_state'] = ["published", "published_alive"]
-kw['sort_on'] = (('modification_date', 'DESC'),)
-kw['limit'] = 50
-
-return portal.portal_catalog(**kw)

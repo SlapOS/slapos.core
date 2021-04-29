91ae216bf28521cc2c43283da9d72944fe927552
Rafael Monnerat <rafael@nexedi.com>
2021-04-29 18:24:49 +0000

    slapos_jio:    Filter list to not display closed/destroy/invalidated ones.
       Filter Computer, Hosting Subscription and Computer Network to not display closed/destroy/invalidated ones.

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Project_getComputerTrackingList.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Project_getComputerTrackingList.py
@@ -5,5 +5,17 @@ from DateTime import DateTime
 kw = {"project_uid": context.getUid(),
       "at_date": DateTime()}
 
+def filter_per_portal_type(document):
+  if document.getPortalType() == "Computer" \
+      and document.getAllocationScope() != "close/forever":
+    return document
+  elif document.getPortalType() == "Hosting Subscription" \
+      and document.getSlapState() != "destroy_requested":
+    return document
+  elif document.getPortalType() == "Computer Network" \
+      and document.getValidationState() == "validated":
+    return document
+
 return [ i.getObject()
-           for i in context.portal_simulation.getCurrentTrackingList(**kw)]
+           for i in context.portal_simulation.getCurrentTrackingList(**kw)
+             if filter_per_portal_type(i.getObject())]

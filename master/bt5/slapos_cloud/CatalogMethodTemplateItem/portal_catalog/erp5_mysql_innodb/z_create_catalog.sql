f7b15b095e472e4b5ff6785c0f7fe5268e8dc21e
Romain Courteaud <romain@nexedi.com>
2021-05-26 13:53:51 +0000

    slapos_cloud: add indexation_timestamp SQL index

--- master/bt5/slapos_cloud/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_catalog.sql
+++ master/bt5/slapos_cloud/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_catalog.sql
@@ -65,5 +65,6 @@ CREATE TABLE `catalog` (
   KEY `causality_state_portal_type` (`causality_state`, `portal_type`),
   KEY `invoice_state` (`invoice_state`),
   KEY `payment_state` (`payment_state`),
-  KEY `event_state` (`event_state`)
+  KEY `event_state` (`event_state`),
+  KEY `indexation_timestamp` (`indexation_timestamp`)
 ) ENGINE=InnoDB;

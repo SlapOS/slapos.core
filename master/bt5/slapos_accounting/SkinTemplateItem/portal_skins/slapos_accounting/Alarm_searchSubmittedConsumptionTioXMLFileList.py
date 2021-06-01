aa2045d9e0c6ab011e0402079dee9eaed652db0b
Rafael Monnerat <rafael@nexedi.com>
2021-06-01 18:29:45 +0000

    slapos_accounting: Process more at a time
       If we limit the processing from this alarm the production execeeds the processing, and it never reaches the end.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Alarm_searchSubmittedConsumptionTioXMLFileList.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Alarm_searchSubmittedConsumptionTioXMLFileList.py
@@ -4,8 +4,8 @@ portal.portal_catalog.searchAndActivate(
   portal_type="Computer Consumption TioXML File",
   validation_state="submitted",
   method_id='ComputerConsumptionTioXMLFile_solveInvoicingGeneration',
-  activity_count=1,
-  packet_size=1,
+  activity_count=100,
+  packet_size=100,
   activate_kw={'tag': tag, 'priority': 5}
 )
 

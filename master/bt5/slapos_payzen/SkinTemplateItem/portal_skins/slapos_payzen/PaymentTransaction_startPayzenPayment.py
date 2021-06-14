3eb3f76c386cacb8080346aafa9dc852da95127f
Rafael Monnerat <rafael@nexedi.com>
2021-06-14 19:17:09 +0000

    slapos_payzen&wechat: Change payment_mode if invoice/payment price is zero
       It isn't possible to pay on payzen zero amount invoices, so just update payment_mode to skip the payment from the search (to prevent bloat activities by relaunching activities that will never change the state of the object).

--- master/bt5/slapos_payzen/SkinTemplateItem/portal_skins/slapos_payzen/PaymentTransaction_startPayzenPayment.py
+++ master/bt5/slapos_payzen/SkinTemplateItem/portal_skins/slapos_payzen/PaymentTransaction_startPayzenPayment.py
@@ -3,20 +3,11 @@ from DateTime import DateTime
 state = context.getSimulationState()
 transaction_amount = int(round((context.PaymentTransaction_getTotalPayablePrice() * -100), 2))
 if (state != 'confirmed') or (context.getPaymentMode() != 'payzen') or (transaction_amount == 0):
+  if (transaction_amount == 0):
+    invoice = context.getCausalityValue(portal_types="Sale Invoice Transaction")
+    if invoice is not None and round(invoice.getTotalPrice(), 2) == 0:
+      context.edit(payment_mode="wire_transfer")
   return
 else:
   # Request manual payment
   context.start(comment='Requested manual payment')
-
-#   raise NotImplementedError
-#   if context.PaymentTransaction_getPreviousPayzenId() is not None:
-#     # there is previous payment
-#     context.setStartDate(DateTime())
-#     context.updateCausalityState()
-#     portal.system_event_module.newContent(
-#        title='Transaction %s Payzen registration' % context.getTitle(),
-#        portal_type='Payzen Event',
-#        source_value=service,
-#        destination_value=context).registerPayzen()
-#     comment='Automatically duplicated in payzen.'
-#   else:
--- master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/PaymentTransaction_startWechatPayment.py
+++ master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/PaymentTransaction_startWechatPayment.py
@@ -3,6 +3,10 @@ from DateTime import DateTime
 state = context.getSimulationState()
 transaction_amount = int(round((context.PaymentTransaction_getTotalPayablePrice() * -100), 2))
 if (state != 'confirmed') or (context.getPaymentMode() != 'wechat') or (transaction_amount == 0):
+  if (transaction_amount == 0):
+    invoice = context.getCausalityValue(portal_types="Sale Invoice Transaction")
+    if invoice is not None and round(invoice.getTotalPrice(), 2) == 0:
+      context.edit(payment_mode="wire_transfer")
   return
 else:
   # Request manual payment

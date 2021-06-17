58a14014e8abde913d867bd73cc5be04c89a6bc7
Romain Courteaud <romain@nexedi.com>
2021-06-17 12:20:01 +0000

    slapos_cloud: invalidated instances are supposed to be destroyed
    Optimize the query execution time.

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_searchComputerPartitionAndMarkFree.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_searchComputerPartitionAndMarkFree.py
@@ -3,7 +3,6 @@ portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
   aggregate__uid="%",
   validation_state="invalidated",
-  slap_state="destroy_requested",
   parent_uid=context.getPortalObject().software_instance_module.getUid(),
   method_id='SoftwareInstance_tryToUnallocatePartition',
   activate_kw={'tag': tag}

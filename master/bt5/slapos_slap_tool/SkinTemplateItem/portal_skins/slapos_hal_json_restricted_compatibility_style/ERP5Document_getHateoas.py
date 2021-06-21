24584990c2e9162e49da98046f1492c5f025db74
Romain Courteaud <romain@nexedi.com>
2021-06-21 08:19:45 +0000

    slapos_slap_tool: add compatibility with old clients
    Old slapgrid version will use the "Hosting Subscription" term during
    search.
    Add a new Skin Selection to rewrite the search query on the fly.
    
    Add a new web site to not rely on the generic one, as it prevent specific customization

---
+++ master/bt5/slapos_slap_tool/SkinTemplateItem/portal_skins/slapos_hal_json_restricted_compatibility_style/ERP5Document_getHateoas.py
@@ -0,0 +1,36 @@
+portal = context.getPortalObject()
+
+if (mode == 'search') and (query is not None):
+  # Rewrite the queries containing the Hosting Subscription string
+  query = query.replace('"Hosting Subscription"', '"Instance Tree"')
+
+new_skin_name = "HalRestricted"
+portal.portal_skins.changeSkin(new_skin_name)
+if REQUEST is None:
+  REQUEST = context.REQUEST
+REQUEST.set('portal_skin', new_skin_name)
+
+return context.ERP5Document_getHateoas(
+  REQUEST=REQUEST,
+  response=response,
+  view=view,
+  mode=mode,
+  query=query,
+  select_list=select_list,
+  limit=limit,
+  form=form,
+  form_data=form_data,
+  relative_url=relative_url,
+  list_method=list_method,
+  default_param_json=default_param_json,
+  form_relative_url=form_relative_url,
+  bulk_list=bulk_list,
+  group_by=group_by,
+  sort_on=sort_on,
+  local_roles=local_roles,
+  selection_domain=selection_domain,
+  extra_param_json=extra_param_json,
+  portal_status_message=portal_status_message,
+  portal_status_level=portal_status_level,
+  keep_items=keep_items
+)

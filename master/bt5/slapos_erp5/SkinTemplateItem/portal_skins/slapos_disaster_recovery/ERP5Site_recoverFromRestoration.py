468b503bcadd191f01fac2f6ccde3ce5e0fb3bc6
Rafael Monnerat <rafael@nexedi.com>
2021-07-09 20:42:26 +0000

    slapos_erp5: [Data Recovery] Skip full reindexation if isn't needed
       If the mariadb is more recent (and consitent) them the ZODB, it isn't needed to full reindex since all
       changed objects are visible in the catalog. So it is safe enough only partially reindex.

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_disaster_recovery/ERP5Site_recoverFromRestoration.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_disaster_recovery/ERP5Site_recoverFromRestoration.py
@@ -5,10 +5,15 @@ before = now - int(7)
 
 strfstring = '%Y-%m-%d %H:%M:%S'
 
-portal.ERP5Site_checkLatestModifiedDocumentList(0, 1000, 100, before.strftime(strfstring), now.strftime(strfstring))
+portal.ERP5Site_checkLatestModifiedDocumentList(0, 1000, 100,
+  before.strftime(strfstring), now.strftime(strfstring))
 
 # Force reindexation of recently created document
 # This expect module to use HBTree
+
+# The following category could be consider as "non-optimistic", but
+# it is minimal compared to a whole reindex and it is safe to prevent 
+# Minor inconsistencies.
 for module_id in portal.objectIds(("ERP5 Folder",)):
   if module_id.endswith("_module"):
     portal[module_id].recurseCallMethod(
@@ -22,23 +27,31 @@ for module_id in portal.objectIds(("ERP5 Folder",)):
 
 portal.ERP5Site_checkDeletedDocumentList(0, 1000, 100)
 
-for module_id in [
-  'portal_preferences',
-  'portal_categories',
-  'portal_alarms',
-  'portal_simulation']+portal.objectIds(("ERP5 Folder",)):
-  portal[module_id].recurseCallMethod(
-    'immediateReindexObject',
-    min_depth=1,
-    max_depth=10000,
-    activate_kw=dict(
-      group_method_id='portal_catalog/catalogObjectList',
-      alternate_method_id='alternateReindexObject',
-      group_method_cost=1,
-      priority=6,
-    ),
-    max_retry=0,
-    activity_count=100,
-  )
+
+# Whenever we trust that the catalog is consistent and more recent them the 
+# ZODB, it isn't required to trigger the whole reindexation of the site. 
+
+# If the Mariadb is eventually inconsitent or older them the current ZODB,
+# It's required reindex the whole site.
+
+if not optimistic:
+  for module_id in [
+    'portal_preferences',
+    'portal_categories',
+    'portal_alarms',
+    'portal_simulation']+portal.objectIds(("ERP5 Folder",)):
+    portal[module_id].recurseCallMethod(
+      'immediateReindexObject',
+      min_depth=1,
+      max_depth=10000,
+      activate_kw=dict(
+        group_method_id='portal_catalog/catalogObjectList',
+        alternate_method_id='alternateReindexObject',
+        group_method_cost=1,
+        priority=6,
+      ),
+      max_retry=0,
+      activity_count=100,
+    )
 
 return "OK"

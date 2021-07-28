5ae9008671d4587b67c4c4f0ac78f9b036de8d79
Jérome Perrin <jerome@nexedi.com>
2021-07-28 09:21:30 +0200

    cli/request: support yaml format for --parameters-file option

--- slapos/tests/test_cli.py
+++ slapos/tests/test_cli.py
@@ -739,6 +739,19 @@ class TestCliRequestParametersFileJson(CliMixin):
     )
 
 
+class TestCliRequestParametersFileYaml(TestCliRequestParametersFileJson):
+  def _makeParameterFile(self):
+    f = tempfile.NamedTemporaryFile(suffix='.yaml', mode='w', delete=False)
+    f.write(textwrap.dedent('''\
+    {
+      "foo": ["bar"]
+    }
+    '''))
+    f.flush()
+    self.addCleanup(os.unlink, f.name)
+    return f.name
+
+
 class TestCliRequestParametersFileXml(TestCliRequestParametersFileJson):
   expected_partition_parameter_kw = {'foo': 'bar'}
   def _makeParameterFile(self):

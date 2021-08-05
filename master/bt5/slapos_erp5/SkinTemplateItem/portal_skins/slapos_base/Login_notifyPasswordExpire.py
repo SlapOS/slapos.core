e1915cb860974cc9a87b0323c42420667e5c02e0
Rafael Monnerat <rafael@nexedi.com>
2021-08-05 21:07:44 +0000

    slapos_erp5: Fixup when restart from a website
      The suffixes present on the come_from are incompatible with the addition done by portal_password

---
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_base/Login_notifyPasswordExpire.py
@@ -0,0 +1,43 @@
+"""
+  File a password expire event.
+"""
+from DateTime import DateTime
+from Products.ZSQLCatalog.SQLCatalog import Query
+from erp5.component.module.DateUtils import addToDate
+
+portal = context.getPortalObject()
+portal_preferences = portal.portal_preferences
+
+if not portal_preferences.isAuthenticationPolicyEnabled() or \
+   not portal_preferences.isPreferredSystemRecoverExpiredPassword():
+  # no policy, no sense to file expire at all or symply system do not configured to
+  return
+
+user = context.getParentValue()
+username = context.getReference()
+
+# Prevent creating new recovery if one was recently created
+recovery_list = portal.portal_catalog(
+  portal_type="Credential Recovery",
+  reference=username,
+  default_destination_decision_uid=user.getUid(),
+  creation_date=Query(range="min", creation_date=addToDate(DateTime(), {'day': -1})),
+  limit=1)
+if recovery_list:
+  return
+tag = 'credential_recovery_%s' %context.getReference()
+if portal.portal_activities.countMessageWithTag(tag):
+  return
+
+if context.getWebSectionValue() is not None:
+  context.REQUEST.set('came_from', context.getWebSectionValue().absolute_url())
+
+module = portal.getDefaultModule(portal_type='Credential Recovery')
+credential_recovery = module.newContent(
+    portal_type="Credential Recovery",
+    reference=username,
+    destination_decision_value=user,
+    language=portal.Localizer.get_selected_language(),
+    activate_kw={'tag': tag})
+context.serialize()
+credential_recovery.submit()

7adf49d439ca8ca8087de833ee426fa9ae586cbd
Romain Courteaud <romain@nexedi.com>
2021-08-26 13:05:12 +0000

    slapos_cloud: migrate Computer to Compute Node
    Also migrate Computer Partition to Compute Partition

---
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/AlarmTool_checkComputeNodeMigrationConsistency.py
@@ -0,0 +1,23 @@
+portal = context.getPortalObject()
+result_list = []
+
+migration_kw = {
+  'portal_type': 'Computer',
+  'slap_state': '%'
+}
+
+non_migrated_instance = portal.portal_catalog(limit=1, **migration_kw)
+
+if len(non_migrated_instance) == 1:
+  if fixit:
+    portal.portal_catalog.searchAndActivate(
+      activate_kw=dict(priority=5,
+                       tag=script.getId(),
+                       after_method_id=('immediateReindexObject',
+                                        'recursiveImmediateReindexObject')),
+      method_id='fixConsistency',
+      **migration_kw)
+  else:
+    result_list.append("all X needs updates %s" % non_migrated_instance[0].getRelativeUrl())
+
+return result_list

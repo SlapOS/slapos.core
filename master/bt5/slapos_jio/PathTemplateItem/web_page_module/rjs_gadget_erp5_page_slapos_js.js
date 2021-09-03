d486d0ab91980d8634b2243376536d3a1d11ced5
Rafael Monnerat <rafael@nexedi.com>
2021-09-03 03:33:21 +0000

    slapos_jio: Display something on the Map by default
      Even if the Map had no markers, display some location meaningful instead a point into the occean

--- master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slapos_js.js
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_erp5_page_slapos_js.js
@@ -383,10 +383,16 @@
               });
             })
             .push(function (map_gadget) {
-              return map_gadget.render({
+              var map_options = {
                 zoom : 40,
                 marker_list : marker_list
-              });
+              };
+              if (marker_list.length === 0) {
+                map_options.latitude = 48.858370;
+                map_options.longitude = 2.294481;
+                map_options.zoom = 0;
+              }
+              return map_gadget.render(map_options);
             });
         });
     });

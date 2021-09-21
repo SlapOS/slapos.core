1b95f815128309c70d2f4639538d77db31803433
Rafael Monnerat <rafael@nexedi.com>
2021-09-21 19:16:26 +0000

    slapos_pdm: Remove "Software Catalog" feature since it was unused
       This is basically dead code, and untested.

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_manageSoftwareCatalog.py
+++
@@ -1,9 +0,0 @@
-portal = context.getPortalObject()
-portal.portal_catalog.searchAndActivate(
-  portal_type='Software Product',
-  validation_state='published',
-  method_id='SoftwareProduct_manageSoftwareCatalog',
-  activate_kw={'tag': tag}
-)
-
-context.activate(after_tag=tag).getId()

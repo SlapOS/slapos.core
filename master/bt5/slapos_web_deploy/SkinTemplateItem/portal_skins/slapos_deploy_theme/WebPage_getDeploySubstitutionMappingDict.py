6a22ec0b9df86501767b30c908a777b3469ce4f0
Rafael Monnerat <rafael@nexedi.com>
2021-09-28 19:02:56 +0000

    slapos_web_deploy: Also search for published alive pages
       This was done by Thomas, I'm just pushing it into git

--- master/bt5/slapos_web_deploy/SkinTemplateItem/portal_skins/slapos_deploy_theme/WebPage_getDeploySubstitutionMappingDict.py
+++ master/bt5/slapos_web_deploy/SkinTemplateItem/portal_skins/slapos_deploy_theme/WebPage_getDeploySubstitutionMappingDict.py
@@ -7,15 +7,10 @@ url_map = {"deploy-Function.Common" : "function_common_content",
        "deploy-Unstable.Channel": "slapos_unstable_content"}
 
 portal = context.getPortalObject()
-
-from DateTime import DateTime
-i = DateTime()
-
 for doc in portal.portal_catalog(reference=url_map.keys(),
                                     portal_type="Web Page",
-                                    validation_state="published"):
+                                    validation_state=["published", "published_alive"]):
 
   mapping_dict[url_map[doc.getReference()]] = doc.getTextContent()
 
-context.log((DateTime() - i)*3600*24)
 return mapping_dict

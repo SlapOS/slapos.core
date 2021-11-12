06f72fdf4bf774009af4ea415c1bd0b1fda014fe
Rafael Monnerat <rafael@nexedi.com>
2021-11-12 23:12:50 +0000

    slapos_erp5: Implement quick password expiration
        It add a script to be custom on project site, if you have short term users, that requires short password expiration timing.

---
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_base/Login_getFastExpirationReferenceList.py
@@ -0,0 +1,7 @@
+"""
+  Returns the list of logins that will expire in 24 hours.
+
+  This is used for mantain superusers accounts in auto expiration mode.
+"""
+
+return  []
--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_base/Login_isPasswordExpired.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_base/Login_isPasswordExpired.py
@@ -1,16 +1,30 @@
 """
   Returns if user account is Person's password is expired.
   Start password recovery process for expired password (if configured).
+
+  This script was introduce to expire superusers passwords faster, 
+  requiring them to reset it for using it. This allow us keep them 
+  with a reasonably easy access w/o require to reccur to zope password
+  while debug or update code on production.
+
+  The second reason is to overcome certain security restrictions when
+  search Password events.
+
 """
 portal = context.getPortalObject()
 is_password_expired = False
 expire_date_warning = 0
 password_event_list = context.Login_unrestrictedSearchPasswordEvent()
+quick_expiration_login_list = context.Login_getFastExpirationReferenceList()
 
 if password_event_list:
   ONE_HOUR = 1 / 24.0
   portal_preferences = portal.portal_preferences
-  expire_date = password_event_list[0].creation_date + portal_preferences.getPreferredMaxPasswordLifetimeDuration() * ONE_HOUR
+  if context.getReference() in quick_expiration_login_list:
+    # Expire the superusers every 24 hours maximum
+    expire_date = password_event_list[0].creation_date + 24 * ONE_HOUR
+  else:
+    expire_date = password_event_list[0].creation_date + portal_preferences.getPreferredMaxPasswordLifetimeDuration() * ONE_HOUR
   now = DateTime()
   if expire_date < now:
     # password is expired

af100fdebede7319c4758832a45db5fecfd033a5
Rafael Monnerat <rafael@nexedi.com>
2021-11-25 00:39:50 +0000

    slapos_cloud: Extent *_slap_interface_workflow for projects
       - Include person.requestProject API on person_slap_interface_workflow
       - Include project_slap_interface_workflow on Projects

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Project_closeRelatedAssignment.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Project_closeRelatedAssignment.py
@@ -1,6 +1,1 @@
-person = context.getPortalObject().portal_membership.getAuthenticatedMember().getUserValue()
-
-for assignment in person.objectValues(portal_type="Assignment"):
-  if assignment.getDestinationProject() == context.getRelativeUrl():
-    assignment.close()
-    return
+return context.leaveProject()

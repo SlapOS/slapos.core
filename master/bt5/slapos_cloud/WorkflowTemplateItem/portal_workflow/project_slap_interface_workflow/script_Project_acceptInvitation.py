c9d3665bcbc4bc94b7db505e6d092ff6d4412c8b
Rafael Monnerat <rafael@nexedi.com>
2021-11-25 00:39:50 +0000

    slapos_cloud: Add slap_interface_workflow for projects

---
+++ master/bt5/slapos_cloud/WorkflowTemplateItem/portal_workflow/project_slap_interface_workflow/script_Project_acceptInvitation.py
@@ -0,0 +1,49 @@
+project = state_change['object']
+portal = person.getPortalObject()
+person = portal.portal_membership.getAuthenticatedMember().getUserValue()
+
+# Get required arguments
+kwargs = state_change.kwargs
+
+# Required args
+# Raise TypeError if all parameters are not provided
+try:
+  token_id = kwargs['invitation_token']
+except KeyError:
+  raise TypeError("Project_requestAssignment takes exactly 1 argument")
+
+try:
+  invitation_token = portal.invitation_token_module[token_id]
+except KeyError:
+  raise ValueError("The Invitation Token can't be found, please review the URL.")
+
+if person is None:
+  message_str = "Please login before access the invitation link."
+  raise ValueError(message_str)
+
+if invitation_token.getPortalType() != "Invitation Token":
+  message_str = "The Invitation Token can't be found, please review the URL."
+  raise ValueError(message_str)
+
+if invitation_token.getValidationState() != "validated":
+  message_str = "The Invitation Token was already used and it cannot be reused, please ask a new one."
+  raise ValueError(message_str)
+
+if invitation_token.getSourceValue() == person:
+  message_str = "Invitation Token cannot be used by the same user that generated the token!"
+  raise ValueError(message_str)
+
+for assignment in person.objectValues(portal_type="Assignment"):
+  if assignment.getDestinationProject() == context.getRelativeUrl():
+    invitation_token.invalidate(comment="User already has assignment to the Person")
+
+if invitation_token.getValidationState() == "validated":
+  person.newContent(
+    title="Assigment for Project %s" % context.getTitle(),
+    portal_type="Assignment",
+    destination_project_value=context).open()
+
+  invitation_token.invalidate()
+
+message_str = "You sucessfully join a new project: %s." % context.getTitle()
+context.REQUEST.set("project_message", compute_node.getRelativeUrl())
---
+++ master/bt5/slapos_cloud/WorkflowTemplateItem/portal_workflow/project_slap_interface_workflow/script_Project_requestToken.py
@@ -0,0 +1,26 @@
+project = state_change['object']
+portal = person.getPortalObject()
+
+portal = context.getPortalObject()
+person = portal.portal_membership.getAuthenticatedMember().getUserValue()
+
+request_method = "POST"
+script_name = "Project_acceptInvitation"
+
+web_site = context.getWebSiteValue()
+if web_site is None:
+  web_site = portal
+
+request_url = "%s/%s/%s" % (web_site.absolute_url(), project.getRelativeUrl(), script_name)
+
+# Maybe it would be better to use another portal_type 
+token = portal.invitation_token_module.newContent(
+  portal_type="Invitation Token",
+  source_value=person,
+  url_string=request_url,
+  url_method=request_method
+)
+token.validate()
+
+context.REQUEST.set('request_token_id', token.getId())
+context.REQUEST.set('request_token_url_string', request_url)

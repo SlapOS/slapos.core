19057b25662f43efbbd86100820b5aa457f8a7d0
Rafael Monnerat <rafael@nexedi.com>
2021-12-01 20:47:08 +0000

    slapos_jio: Now add project via Request Project API

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Person_requestProject.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Person_requestProject.py
@@ -10,9 +10,10 @@ if person is None:
 else:
   request_kw = dict(project_title=title)
   person.requestProject(**request_kw)
-  project = context.restrictedTraverse(context.REQUEST.get('project'))
+  project_relative_url = context.REQUEST.get('project_relative_url')
+  project_reference = context.REQUEST.get('project_reference')
 
   return json.dumps({
-    "reference": project.getReference(),
-    "relative_url": project.getRelativeUrl()
+    "reference": project_reference,
+    "relative_url": project_relative_url
   })

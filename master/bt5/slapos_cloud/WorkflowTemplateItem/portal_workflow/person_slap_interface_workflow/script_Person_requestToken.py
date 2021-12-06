0b935fc8947f22925bbc7bdb9b55061be9a19942
Rafael Monnerat <rafael@nexedi.com>
2021-12-06 20:44:48 +0000

    slapos_cloud&jio: Add Person.requestToken API
      This API aims to handle token generation for person, rather them
      keep ad-hoc API on scripts for specific usage

---
+++ master/bt5/slapos_cloud/WorkflowTemplateItem/portal_workflow/person_slap_interface_workflow/script_Person_requestToken.py
@@ -0,0 +1,23 @@
+person = state_change['object']
+portal = person.getPortalObject()
+# Get required arguments
+kwargs = state_change.kwargs
+
+# Required args
+# Raise TypeError if all parameters are not provided
+try:
+  request_url = kwargs['request_url']
+except KeyError:
+  raise TypeError, "Person_requestToken takes exactly 1 argument"
+
+request_method = "POST"
+access_token = portal.access_token_module.newContent(
+  portal_type="One Time Restricted Access Token",
+  agent_value=person,
+  url_string=request_url,
+  url_method=request_method
+)
+access_token_id = access_token.getId()
+access_token.validate()
+
+context.REQUEST.set("token", access_token_id)

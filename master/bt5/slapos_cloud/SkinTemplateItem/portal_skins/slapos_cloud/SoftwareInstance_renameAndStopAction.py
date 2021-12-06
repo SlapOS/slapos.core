7a42cbc732c3cb28b5707cc7e1bc12ccafab3f89
Łukasz Nowak <luke@nexedi.com>
2021-12-06 15:15:24 +0100

    XXX slapos_cloud: Skeletonise safe action for instance reallocation

---
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/SoftwareInstance_renameAndStopAction.py
@@ -0,0 +1,16 @@
+from zExceptions import Unauthorized
+from DateTime import DateTime
+
+portal = context.getPortalObject()
+if portal.portal_membership.isAnonymousUser():
+  raise Unauthorized("You cannot invoke this API as Annonymous")
+
+if context.getSlapState() not in ["start_requested", "stop_requested"]:
+  return context.Base_redirect(keep_items={"portal_status_message": "This %s is on %s state and cannot be destroyed." \
+    % (context.getPortalType(), context.getSlapState())})
+
+# It is used a external method as portal_slap._storeLastData cannot be invoked
+# from a python script
+context.SoftwareInstance_renameAndStop()
+
+return context.Base_redirect(keep_items={"portal_status_message": "Renamed and stopped."})

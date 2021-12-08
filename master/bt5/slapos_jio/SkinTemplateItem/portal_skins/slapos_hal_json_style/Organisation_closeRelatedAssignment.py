fbf0046aaaba98f8774cc12b07111ccb45eed318
Rafael Monnerat <rafael@nexedi.com>
2021-12-08 01:17:50 +0000

    slapos_cloud: Bootstrap organisation slap interface workflow

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Organisation_closeRelatedAssignment.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Organisation_closeRelatedAssignment.py
@@ -1,6 +1,1 @@
-person = context.getPortalObject().portal_membership.getAuthenticatedMember().getUserValue()
-
-for assignment in person.objectValues(portal_type="Assignment"):
-  if assignment.getDestination() == context.getRelativeUrl():
-    assignment.close()
-    return
+context.leaveOrganisation()

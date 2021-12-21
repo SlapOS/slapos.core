a685044d53785def2a94fc4e216cae7949d16202
Rafael Monnerat <rafael@nexedi.com>
2021-12-21 21:37:28 +0000

    slapos_jio: Set Proper Content-Type on the response

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Person_requestSite.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Person_requestSite.py
@@ -10,9 +10,10 @@ if person is None:
 else:
   request_kw = dict(organisation_title=title)
   person.requestSite(**request_kw)
-  organisation_relative_url = context.REQUEST.get('organisation_relative_url')
-  organisation_reference = context.REQUEST.get('organisation_reference')
+  organisation_relative_url = request.get('organisation_relative_url')
+  organisation_reference = request.get('organisation_reference')
 
+  response.setHeader('Content-Type', "application/json")
   return json.dumps({
     "reference": organisation_reference,
     "relative_url": organisation_relative_url

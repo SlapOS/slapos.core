c7220955b34f86f7de19b1924fe41820683581d1
Rafael Monnerat <rafael@nexedi.com>
2021-12-22 21:44:16 +0100

    slapos_erp5: simulate isn't required anymore

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5InteractionWorkflow.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5InteractionWorkflow.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin, simulate
+from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin
 
 class TestSlapOSERP5InteractionWorkflowComputeNodeSetAllocationScope(
     SlapOSTestCaseMixin):

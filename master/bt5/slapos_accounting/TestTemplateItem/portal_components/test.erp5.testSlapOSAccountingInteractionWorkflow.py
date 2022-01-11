26e57b41bd0f2433a58078c99b7f481d092f97ff
Romain Courteaud <romain@nexedi.com>
2022-01-11 10:05:48 +0100

    slapos_accounting: stop generating a single open sale order per user
    Instead of having a giant versionned Open Sale Order, containing all user
    subscriptions, create one Open Sale Order per subscription.
    The Open Order state can now be used to mark the subscription status.
    
    This reduce the simulation tree size, as a such tree will not be
    updated anymore when a subscription ends.
    
    The Open Order stop date is set when closing the Open Order.
    
    Create a new 'automated' ledger category, to explicitely mark the
    deliveries managed by the alarms / scripts.
    Drop the explicit reference of a specific business process / trade condition,
    and allow any Trade Condition to be used, based on its predicate/trade_condition_type configuration.
    
    Ensure the items are propagated in the simulation movements and delivery lines.
    
    Aggregate Sale Invoices, based on their Trade Condition and profile values.
    A user will have multiple invoices if he is using multiple virtual master's services.
    
    Implement a VAT calculation for France only for now.
    Delivery Line with a taxable business_contribution must trigger the
    tax calculation script (which depends on many factors like type of sold items, customer address, ...)
    and explicitely mark on the line the VAT rate.
    Customer address can be modified in futur, without impacting the delivery
    vat calculation.
    
    Also in this commit:
    * drop template usages
    * use interaction workflow to trigger alarms
    * do not generate invoice with a 0 total price
    * drop hardcoded reference of hardcoded currencies
    * add contraints on the simulation logic, to prevent generating wrong movements
    * configure Sale Supply to allow more complex price definitions
    * allow using any kind of resource on the deliveries
    * disable consumption delivery generation (until better specified)
    * stop forcing/hardcoding payment mode in advance
    * support for variated resources
    * allow to manually pay multiple invoices with a single payment
    * drop the cloud contract tricks
    * destroy items as soon as the Open Order is closed
    * add a discount service to reduce the invoice price if wanted
    * add a deposit service to support invoice prepayment

--- master/bt5/slapos_accounting/TestTemplateItem/portal_components/test.erp5.testSlapOSAccountingInteractionWorkflow.py
+++ master/bt5/slapos_accounting/TestTemplateItem/portal_components/test.erp5.testSlapOSAccountingInteractionWorkflow.py
@@ -1,113 +1,19 @@
 # Copyright (c) 2012 Nexedi SA and Contributors. All Rights Reserved.
-from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin
+from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin, simulate
 
 import transaction
-from Products.ERP5Type.tests.utils import createZODBPythonScript
-from DateTime import DateTime
+
 
 class TestSlapOSAccountingInteractionWorkflow(SlapOSTestCaseMixin):
   def beforeTearDown(self):
     transaction.abort()
 
-  def _simulateInstanceTree_calculateSubscriptionStartDate(self, date):
-    script_name = 'InstanceTree_calculateSubscriptionStartDate'
-    if script_name in self.portal.portal_skins.custom.objectIds():
-      raise ValueError('Precondition failed: %s exists in custom' % script_name)
-    createZODBPythonScript(self.portal.portal_skins.custom,
-                        script_name,
-                        '*args, **kwargs',
-                        '# Script body\n'
-"""from DateTime import DateTime
-return DateTime('%s') """ % date.ISO())
-    transaction.commit()
-
-  def _dropInstanceTree_calculateSubscriptionStartDate(self):
-    script_name = 'InstanceTree_calculateSubscriptionStartDate'
-    if script_name in self.portal.portal_skins.custom.objectIds():
-      self.portal.portal_skins.custom.manage_delObjects(script_name)
-    transaction.commit()
-
-  def test_InstanceTree_fixConsistency(self,
-        date=DateTime('2012/01/15'), day=15):
-    new_id = self.generateNewId()
-    item = self.portal.instance_tree_module.newContent(
-      portal_type='Instance Tree',
-      title="Subscription %s" % new_id,
-      reference="TESTSUB-%s" % new_id,
-      periodicity_hour_list=None,
-      periodicity_minute_list=None,
-      periodicity_month_day=None,
-    )
-
-    self.assertEqual(item.getPeriodicityHour(), None)
-    self.assertEqual(item.getPeriodicityMinute(), None)
-    self.assertEqual(item.getPeriodicityMonthDay(), None)
-
-    self._simulateInstanceTree_calculateSubscriptionStartDate(date)
-    try:
-      item.fixConsistency()
-    finally:
-      self._dropInstanceTree_calculateSubscriptionStartDate()
-
-    self.assertEqual(item.getPeriodicityHourList(), [0])
-    self.assertEqual(item.getPeriodicityMinuteList(), [0])
-    self.assertEqual(item.getPeriodicityMonthDay(), day)
-
-  def test_InstanceTree_fixConsistency_today_after_28(self):
-    self.test_InstanceTree_fixConsistency(DateTime('2012/01/29'), 28)
-
-  def test_InstanceTree_manageAfter(self):
-    class DummyTestException(Exception):
-      pass
-
-    def verify_fixConsistency_call(self):
-      # Check that fixConsistency is called on instance tree
-      if self.getRelativeUrl().startswith('instance_tree_module/'):
-        raise DummyTestException
-      else:
-        return self.fixConsistency_call()
-
-    # Replace serialize by a dummy method
-    InstanceTreeClass = self.portal.portal_types.getPortalTypeClass(
-        'Instance Tree')
-    InstanceTreeClass.fixConsistency_call = InstanceTreeClass.\
-        fixConsistency
-    InstanceTreeClass.fixConsistency = verify_fixConsistency_call
-
-    try:
-      # manage_afterAdd
-      self.assertRaises(
-        DummyTestException, 
-        self.portal.instance_tree_module.newContent,
-        portal_type='Instance Tree')
-      # manage_afterClone
-      self.assertRaises(
-        DummyTestException, 
-        self.portal.instance_tree_module.\
-          template_instance_tree.Base_createCloneDocument,
-        batch_mode=1)
-    finally:
-      self.portal.portal_types.resetDynamicDocumentsOnceAtTransactionBoundary()
-      transaction.commit()
-
-  def _simulateDelivery_calculate(self):
-    script_name = 'Delivery_calculate'
-    if script_name in self.portal.portal_skins.custom.objectIds():
-      raise ValueError('Precondition failed: %s exists in custom' % script_name)
-    createZODBPythonScript(self.portal.portal_skins.custom,
-                        script_name,
-                        '*args, **kwargs',
-                        '# Script body\n'
-"""portal_workflow = context.portal_workflow
-portal_workflow.doActionFor(context, action='edit_action', comment='Visited by Delivery_calculate') """)
-    transaction.commit()
-
-  def _dropDelivery_calculate(self):
-    script_name = 'Delivery_calculate'
-    if script_name in self.portal.portal_skins.custom.objectIds():
-      self.portal.portal_skins.custom.manage_delObjects(script_name)
-    transaction.commit()
-
+  @simulate('Delivery_calculate',
+            '*args, **kwargs',
+            """# Script body
+portal_workflow = context.portal_workflow
+portal_workflow.doActionFor(context, action='edit_action', comment='Visited by Delivery_calculate')
+            """)
   def _test_calculate(self, new_id, newContent, **new_kw):
     cancel_spl = newContent(**new_kw)
     close_spl = newContent(**new_kw)
@@ -125,20 +31,17 @@ portal_workflow.doActionFor(context, action='edit_action', comment='Visited by D
     stop_spl.confirm()
     submit_spl = newContent(**new_kw)
 
-    self._simulateDelivery_calculate()
-    try:
-      cancel_spl.cancel()
-      close_spl.close()
-      confirm_spl.confirm()
-      deliver_spl.deliver()
-      order_spl.order()
-      plan_spl.plan()
-      setReady_spl.setReady()
-      start_spl.start()
-      stop_spl.stop()
-      submit_spl.submit()
-    finally:
-      self._dropDelivery_calculate()
+    cancel_spl.cancel()
+    close_spl.close()
+    confirm_spl.confirm()
+    deliver_spl.deliver()
+    order_spl.order()
+    plan_spl.plan()
+    setReady_spl.setReady()
+    start_spl.start()
+    stop_spl.stop()
+    submit_spl.submit()
+
     self.assertEqual(
       cancel_spl.workflow_history['edit_workflow'][-1]['comment'],
       'Visited by Delivery_calculate')
@@ -182,52 +85,3 @@ portal_workflow.doActionFor(context, action='edit_action', comment='Visited by D
     portal_type = "Sale Invoice Transaction"
     self._test_calculate(new_id, newContent, portal_type=portal_type,
         start_date='2011/01/01')
-
-  def test_InstanceTree_changePromise(self):
-    new_id = self.generateNewId()
-    subscription = self.portal.instance_tree_module.newContent(
-      portal_type='Instance Tree',
-      title="Subscription %s" % new_id,
-      reference="TESTSUB-%s" % new_id,
-      )
-    subscription.validate()
-
-    self.assertEqual(subscription.getCausalityState(), 'diverged')
-
-    request_kw = dict(
-      software_release='http://example.org',
-      software_type='http://example.org',
-      instance_xml=self.generateSafeXml(),
-      sla_xml=self.generateSafeXml(),
-      shared=False,
-    )
-
-    subscription.requestStart(**request_kw)
-    subscription.converge()
-    self.assertEqual(subscription.getCausalityState(), 'solved')
-    subscription.requestDestroy(**request_kw)
-    self.assertEqual(subscription.getCausalityState(), 'diverged')
-
-  def test_InstanceTree_changePromiseInDivergedState(self):
-    new_id = self.generateNewId()
-    subscription = self.portal.instance_tree_module.newContent(
-      portal_type='Instance Tree',
-      title="Subscription %s" % new_id,
-      reference="TESTSUB-%s" % new_id,
-      )
-    subscription.validate()
-
-
-    request_kw = dict(
-      software_release='http://example.org',
-      software_type='http://example.org',
-      instance_xml=self.generateSafeXml(),
-      sla_xml=self.generateSafeXml(),
-      shared=False,
-    )
-
-    subscription.requestStart(**request_kw)
-    self.assertEqual(subscription.getCausalityState(), 'diverged')
-
-    subscription.requestDestroy(**request_kw)
-    self.assertEqual(subscription.getCausalityState(), 'diverged')

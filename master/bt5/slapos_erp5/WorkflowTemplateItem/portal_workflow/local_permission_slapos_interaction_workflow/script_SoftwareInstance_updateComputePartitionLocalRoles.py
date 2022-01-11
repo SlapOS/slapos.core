7380b57e0017d9b19b2943205411177534111279
Romain Courteaud <romain@nexedi.com>
2022-01-11 10:05:48 +0100

    slapos_erp5: support for the virtual master logic
    There are 4 types of assigment family:
    * The accounting team has assignment like: accounting/manager or accounting/agent
      They can see all invoices, but can not modify the automated ledger
    * The sale team has assignment like: sale/manager or sale/agent
    * A virtual master customer has an assignment like:
        function/customer
        destination_project/THE_PREFERED_DEFAULT_PROJECT
      He can request instance trees.
      He can see all the nodes on the virtual master.
    * The virtual master manager has assignment like:
        function/production/manager
        destination_project/THE_PROJECT
        or
        function/production/agent
        destination_project/THE_PROJECT
      He is responsible to create the compute node and ensure the quality
      of the service provided on it (so, he manages the related crm).
    
    Worklist must be usable, to quickly allow any user to know what is
    expected from him in the system.
    
    Also in this commit:
    * change the module id generator to _generatePerDayNodeNumberId
      to reduce conflicts on object creation
    * change bt5 dependencies to use the new panel UI

--- master/bt5/slapos_erp5/WorkflowTemplateItem/portal_workflow/local_permission_slapos_interaction_workflow/script_SoftwareInstance_updateComputePartitionLocalRoles.py
+++
@@ -1,5 +0,0 @@
-portal_type_list = ['Compute Partition']
-software_instance = state_change['object']
-after_tag = (software_instance.getPath(), ('immediateReindexObject', 'recursiveImmediateReindexObject'))
-for object_ in software_instance.getAggregateValueList(portal_type=portal_type_list):
-  object_.activate(after_path_and_method_id=after_tag).updateLocalRolesOnSecurityGroups()

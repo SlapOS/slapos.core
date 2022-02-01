d248fdf1fd9f33b65d7ae0c79eb28564eb2cab31
Ophelie <ophelie.gagnard@nexedi.com>
2022-02-01 15:11:49 +0000

    slapos_metadata_transform_agent: prevent to process ongoing data array

--- master/bt5/slapos_metadata_transform_agent/PathTemplateItem/portal_callables/MetadataIngestionPolicy_parseTag.py
+++ master/bt5/slapos_metadata_transform_agent/PathTemplateItem/portal_callables/MetadataIngestionPolicy_parseTag.py
@@ -1,10 +1,3 @@
-"""
-  This script is used in Ingestion Policy objects to parse provided by fluentd / ebulk reference.
-  In this case it expects a reference in this format for example:
-
-
-"""
-context.log(reference)
 if not reference:
   raise ValueError('reference is None')
 
--- master/bt5/slapos_metadata_transform_agent/SkinTemplateItem/portal_skins/slapos_metadata_transform_agent/Alarm_checkNodeStatus.py
+++ master/bt5/slapos_metadata_transform_agent/SkinTemplateItem/portal_skins/slapos_metadata_transform_agent/Alarm_checkNodeStatus.py
@@ -5,7 +5,7 @@ portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
   portal_type="Data Array",
   publication_section_relative_url="publication_section/file_system_image/node_image",
-  validation_state="draft",
+  simulation_state="converted",
   method_id='DataArray_generateDiffND',
   activate_kw={'tag': tag},
 )

b5580573366ea406e3358135312811b40ceccf49
Jérome Perrin <perrinjerome@gmail.com>
2022-02-15 22:32:19 +0900

    runpromises: support software releases older than slapos 1.0.118
    Before slapos commit a184cca8b (Try to reuse existing file to avoid
    excessive IO on update + other minor optimisations, 2019-06-17), which
    started to be used in profiles in 65acdca0d (Release slapos.cookbook
    (1.0.118), 2019-08-13), the promise plugins did not contain
    
        import sys
        sys.path[0:0] = (..., )
    
    but there was an extra import statement:
    
        import sys
        import json
        sys.path[0:0] = (..., )
    
    slapos.core >= 1.7 was not able to process such promises, because the
    code introspecting these promises assumed that the second node of the
    AST was the assignment, which is incorrect in the old promises because
    it was another import.
    
    To accomodate both cases, use the value from the first assignment.
    
    We also change to use ast.literal_eval for safety.

--- slapos/grid/promise/runpromises.py
+++ slapos/grid/promise/runpromises.py
@@ -36,8 +36,10 @@ promise_file = next(
 with open(os.path.join(promise_folder, promise_file)) as f:
   promise_content = f.read()
 tree = ast.parse(promise_content, mode='exec')
-
-sys.path[0:0] = eval(compile(ast.Expression(tree.body[1].value), '', 'eval'))
+assign_node = next(e for e in tree.body if isinstance(e, ast.Assign))
+if sys.version_info >= (3, 9):
+  assert ast.unparse(assign_node.targets[0]) == 'sys.path[0:0]'
+sys.path[0:0] = ast.literal_eval(assign_node.value)
 
 from slapos.grid.promise import PromiseLauncher, PromiseError
 from slapos.cli.entry import SlapOSApp

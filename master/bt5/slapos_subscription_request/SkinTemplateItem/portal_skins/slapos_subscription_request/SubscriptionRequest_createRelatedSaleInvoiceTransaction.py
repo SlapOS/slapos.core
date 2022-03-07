a85782d3df3692d4d7d3ff42e56d2e1524c8bc6c
Romain Courteaud <romain@nexedi.com>
2022-03-07 16:53:14 +0000

    slapos_subscription_request: XXX create line

--- master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/SubscriptionRequest_createRelatedSaleInvoiceTransaction.py
+++ master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/SubscriptionRequest_createRelatedSaleInvoiceTransaction.py
@@ -14,10 +14,18 @@ if current_invoice is None:
   current_invoice = invoice_template.Base_createCloneDocument(batch_mode=1)
   assert current_invoice is not None
   current_invoice.manage_delObjects([x for x in
-                                     current_invoice.contentIds()])
+                                     current_invoice.contentIds() if x != "1"])
   current_invoice.edit(
     categories=[],
     title=None
+  )
+  current_invoice["1"].manage_delObjects([x for x in
+                                          current_invoice["1"].contentIds()])
+  current_invoice["1"].edit(
+    categories=[],
+    title=None,
+    quantity=None,
+    price=None
   )
 
   """
@@ -31,16 +39,17 @@ if current_invoice is None:
   context.edit(causality_value=current_invoice)
 
   payment_transaction = portal.restrictedTraverse(payment)
-  """
   current_invoice.edit(
-        title="Reservation Fee",
-        destination_value=context.getDestinationSection(),
-        destination_section_value=context.getDestinationSection(),
-        destination_decision_value=context.getDestinationSection(),
-        start_date=payment_transaction.getStartDate(),
-        stop_date=payment_transaction.getStopDate(),
-      )
+    title="Reservation Fee",
+    destination_value=context.getDestinationSection(),
+    destination_section_value=context.getDestinationSection(),
+    destination_decision_value=context.getDestinationSection(),
+    start_date=payment_transaction.getStartDate(),
+    stop_date=payment_transaction.getStopDate(),
 
+    specialise=portal.portal_preferences.getPreferredAggregatedSubscriptionSaleTradeCondition(),
+  )
+  """
   current_invoice["1"].edit(
         start_date=payment_transaction.getStartDate(),
         stop_date=payment_transaction.getStopDate())
@@ -54,7 +63,25 @@ if current_invoice is None:
     quantity=context.getQuantity()
   )
   cell.setPrice(price)
+"""
 
+  current_invoice["1"].edit(
+    start_date=payment_transaction.getStartDate(),
+    stop_date=payment_transaction.getStopDate(),
+
+    use="trade/sale",
+    resource="service_module/slapos_reservation_fee",
+    quantity_unit="unit/piece",
+    base_contribution_list=[
+      "base_amount/invoicing/discounted",
+      "base_amount/invoicing/taxable"
+    ],
+    # variation/service_module/slapos_reservation_fee/default
+
+    quantity=context.getQuantity(),
+    price=price
+  )
+  """
   # Test to see if the user has specific trade condition for aggregation.
   person = context.getDestinationSectionValue()
   trade_condition = person.Person_getAggregatedSubscriptionSaleTradeConditionValue(

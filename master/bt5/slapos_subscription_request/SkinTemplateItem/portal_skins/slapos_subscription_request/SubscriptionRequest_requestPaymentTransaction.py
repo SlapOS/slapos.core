a00e7c222cab9e31f9693ad10e43c42727357cb6
Romain Courteaud <romain@nexedi.com>
2022-03-11 16:08:24 +0000

    slapos_subscription_request: coding style

--- master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/SubscriptionRequest_requestPaymentTransaction.py
+++ master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/SubscriptionRequest_requestPaymentTransaction.py
@@ -9,15 +9,6 @@ current_payment = None
 service_variation = None
 
 if current_invoice is None:
-  target_language = context.getLanguage()
-  """
-  if target_language == "zh": # Wechat payment, reservation fee is 188 CNY
-    payment_template = portal.restrictedTraverse(portal.portal_preferences.getPreferredZhPrePaymentTemplate())
-    invoice_template = portal.restrictedTraverse(portal.portal_preferences.getPreferredZhPrePaymentSubscriptionInvoiceTemplate())
-  else: # Payzen payment, reservation fee is 25 EUR
-    payment_template = portal.restrictedTraverse(portal.portal_preferences.getPreferredDefaultPrePaymentTemplate())
-    invoice_template = portal.restrictedTraverse(portal.portal_preferences.getPreferredDefaultPrePaymentSubscriptionInvoiceTemplate())
-  """
 
   # PaymentTransaction_init is guarded by the owner role
   # but SubscriptionRequest_requestPaymentTransaction is called with a shadow user
@@ -117,13 +108,13 @@ if current_invoice is None:
   amount = context.getQuantity()
   total = round((int(amount) * price)+(int(amount) * price*tax), 2)
 
-  payable_line = current_payment.newContent(
+  current_payment.newContent(
     portal_type="Accounting Transaction Line",
     quantity=total,
     destination="account_module/payable",
     source="account_module/receivable",
   )
-  encash_line = current_payment.newContent(
+  current_payment.newContent(
     portal_type="Accounting Transaction Line",
     quantity=-total,
     # XXX why source/destination are identical?
@@ -131,62 +122,6 @@ if current_invoice is None:
     source="account_module/payment_to_encash",
   )
 
-  """
-  amount = context.getQuantity()
-  if 0:#context.SubscriptionRequest_testSkippedReservationFree(contract):
-    # Reservation is Free
-    price = 0
-    tax = 0
-  # else:
-  elif 0:
-    invoice_line = invoice_template["1"].asContext(
-      destination_section=context.getDestinationSection(),
-      start_date=DateTime(),
-      stop_date=DateTime()
-    )
-
-    subscription_trade_condition = portal.portal_preferences.getPreferredAggregatedSubscriptionSaleTradeCondition()
-    # XXX drop using Person_getAggregatedSubscriptionSaleTradeConditionValue
-    # Instead, put the info on the selected trade condition
-    # If user need a specific one, he must select it explicitely
-    user_trade_condition = context.getDestinationSectionValue().\
-       Person_getAggregatedSubscriptionSaleTradeConditionValue(subscription_trade_condition)
-
-    if user_trade_condition:
-      invoice_line.edit(
-        specialise=user_trade_condition,
-        destination_section=context.getDestinationSection())
-  
-    resource = invoice_line.getResourceValue()
-    if variation_reference is not None:
-      for variation in resource.objectValues(portal_type="Service Individual Variation"):
-        if variation.getReference() == variation_reference:
-          service_variation = variation.getRelativeUrl()
-          invoice_line.setVariation(service_variation)
-
-    price = invoice_line.getResourceValue().getPrice(
-                          context=invoice_line)
-    
-    # We need to provide Price to pay right the way, so we need to include
-    # taxation at this point it is most liketly to quickly forecast price 
-    # with taxes, but for now it is hardcoded.
-    tax = 0
-    if 'base_amount/invoicing/taxable' in invoice_line.getBaseContributionList():
-      specialise = invoice_line.getSpecialiseValue(portal_type='Sale Trade Condition')
-      if specialise is not None:
-        for trade_model_line in specialise.getAggregatedAmountList(invoice_line):
-          tax = trade_model_line.getPrice()
-          # For simplification consider tax is a single value.
-          break
-
-  for line in current_payment.contentValues():
-    if line.getSource() == "account_module/payment_to_encash":
-      total = round((-int(amount) * price)+(-int(amount) * price*tax), 2)
-      line.setQuantity(total)
-    elif line.getSource() == "account_module/receivable":
-      total = round((int(amount) * price)+(int(amount) * price*tax), 2)
-      line.setQuantity(total)
-"""
   # Accelarate job of alarms before proceed to payment.
   comment = "Validation payment for subscription request %s" % context.getRelativeUrl()
   current_payment.confirm(comment=comment)

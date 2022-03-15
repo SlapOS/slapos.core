b19a3da950c6bfd13173fc68ff6364bda964f986
Romain Courteaud <romain@nexedi.com>
2022-03-15 15:23:09 +0000

    slapos_accounting: sale trade condition are versionned

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Base_testSlapOSValidTradeCondition.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Base_testSlapOSValidTradeCondition.py
@@ -1,19 +1,25 @@
-root_trade_condition_list = ["sale_trade_condition_module/slapos_aggregated_trade_condition",
-                             "sale_trade_condition_module/slapos_aggregated_subscription_trade_condition",
-                             
-                             # Valid trade condition for payments
-                             "sale_trade_condition_module/slapos_manual_accounting_trade_condition"]
-
-if context.getSpecialise() in root_trade_condition_list:
-  return True
-
 portal = context.getPortalObject()
-specialise_uid = [
-  portal.restrictedTraverse(i).getUid() for i in root_trade_condition_list
+
+root_trade_condition_reference_list = [
+  "slapos_aggregated_trade_condition",
+  "slapos_aggregated_subscription_trade_condition",
+  # Valid trade condition for payments
+  "slapos_manual_accounting_trade_condition"
 ]
+
+root_trade_condition_list = portal.portal_catalog(
+  portal_type="Sale Trade Condition",
+  reference=root_trade_condition_reference_list,
+  validation_state="validated",
+)
+
+specialise_uid_list = [x.getUid() for x in root_trade_condition_list]
+
+if context.getSpecialiseUid() in specialise_uid_list:
+  return True
 
 return context.getSpecialiseUid() in [
     i.uid for i in portal.ERP5Site_searchRelatedInheritedSpecialiseList(
     portal_type="Sale Trade Condition",
-    specialise_uid=specialise_uid,
+    specialise_uid=specialise_uid_list,
     validation_state="validated")]

2f3fe85bc557a1650a92c9e42f78870587399ea4
Rafael Monnerat <rafael@nexedi.com>
2022-03-18 19:35:41 +0100

    x

--- master/bt5/slapos_slap_tool/ToolComponentTemplateItem/portal_components/tool.erp5.SlapTool.py
+++ master/bt5/slapos_slap_tool/ToolComponentTemplateItem/portal_components/tool.erp5.SlapTool.py
@@ -605,7 +605,6 @@ class SlapTool(BaseTool):
     Fire up bung on Compute Node
     """
     compute_node = self._getComputeNodeDocument(compute_node_id) 
-    compute_node.setErrorStatus('bang')
     return compute_node.reportComputeNodeBang(comment=message)
 
   security.declareProtected(Permissions.AccessContentsInformation,

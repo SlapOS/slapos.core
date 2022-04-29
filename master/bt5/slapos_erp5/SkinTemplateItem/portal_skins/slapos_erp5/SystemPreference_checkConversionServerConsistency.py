538ae8505279075370acfdad39d14323f2a8c0a1
Rafael Monnerat <rafael@nexedi.com>
2022-04-29 15:05:50 +0000

    slapos_erp5: Call other Constraint Script due precedence
       Since the ID is used, it is important to "enableGlobal" before set conversion server configuration.

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/SystemPreference_checkConversionServerConsistency.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_erp5/SystemPreference_checkConversionServerConsistency.py
@@ -1,5 +1,10 @@
-if context.getPortalType() not in [ "System Preference"]:
+error_log = []
+
+if context.getPortalType() not in ["System Preference"]:
   return []
+
+if context.getId() == "slapos_default_system_preference" and context.getPreferenceState() != "global":
+  error_log.extend(context.SystemPreference_checkSystemPreferenceConsistency(fixit=fixit, **kw))
 
 if context.getPreferenceState() != "global":
   return []
@@ -14,7 +19,8 @@ if expected_url != url:
   if fixit:
     system_preference.setPreferredDocumentConversionServerUrl(expected_url)
     fixing = ' (fixed)'
-  return ["Conversion Server not configured as expected%s: %s" %
-    (fixing, "Expect %s\nGot %s" % (expected_url, url))]
-else:
-  return []
+  
+  error_log.append("Conversion Server not configured as expected%s: %s" %
+    (fixing, "Expect %s\nGot %s" % (expected_url, url)))
+
+return error_log

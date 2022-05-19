60ef493453ce611c121bd9189670cb9030438464
Romain Courteaud <romain@nexedi.com>
2022-05-19 08:27:50 +0000

    slapos_jio:
    * drop open/personal allocation_scope
    * allocation_scope/open/public and friend do not exist anymore
    * drop slap_add_compute_node page
    * drop slap_project_list page
    * change status gadget to support parameter configured on the gadget field
    * drop slap_project_view page
    * drop slap_transfer_compute_node
    * drop slap_compute_node_view page
    * drop slap_compute_node_revoke_certificate page
    * drop compute_node_request_certificate page
    * drop not implemented update_allocation_scope action on compute node
    * drop not implemented update_category action on compute node
    * drop slap_compute_node_list page
    * drop slap_ticket_list page
    * drop slap_add_ticket page
    * use parameters as if they were configured on the gadget field
    * drop support request view page
    * drop web message view
    * drop slap_add_related_event page
    * drop service_list page
    * drop Software Release view
    * drop Software Product view
    * drop software installation creation
    * move SoftwareInstallation_getSoftwareReleaseInformation
    * drop slap_select_software_product/release pages
    * drop slap_software_installation_view page
    * drop slap_destroy_software_installation page
    * instance tree actions moved to slapos_panel
    * drop slap_add_instance_tree page
    * accept parameters as provided by GadgetField
    * drop instance tree view action
    * only check accessible instances
    * drop gadget_erp5_page_slap_instance_tree_view
    * drop slap_software_instance_view page
    * drop person.requestOrganisation
    * do not delete all categories on Credential Request and set destination_project
    * revert: restore WebSection_newCredentialRequest as previously
    * drop close ticket
    * drop SoftwareProduct_getSoftwareReleaseAsHateoas
    * drop Upgrade Decision view
    * drop Computer Network views
    * drop Computer Network scripts
    * drop add_organisation page
    * update renderjs_oss modification date
    * fixup rebase
    * drop Compute Node Token previous implementation
    * erp5_web_renderjs_ui | RJS is managed by erp5_web_renderjs_ui bt5
    * move most stuff to slapos_jio_before_deletion
      Keep slapos_jio minimal to simplify the rebase on the files to keep
    * keep status gadget
    * forgot to drop actions
    * restore label_listbox_field
    * restore status gadget first rendering
      onLoop must be fixed probably
    * status gadget portal _type is provided by the python script
    * do not mess with cursor used by browser
      it only confuses users
    * do not display links without href
    * disable gadget status onLoop
      Instead of jio.get, the paramers must provide the url to query,
      otherwise, it is not possible to use this gadget in the full ERP5JS
    * reintroduce all stuff from slapos_jio_before_deletion
      my branch is not ready to drop everything
    * fixup rebase
    * drop files moved to portal_skins
    * move to slapos_panel

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Organisation_getNewsDict.py
+++
@@ -1,6 +0,0 @@
-from zExceptions import Unauthorized
-if REQUEST is not None:
-  raise Unauthorized
-
-return context.Base_getNewsDictFromComputeNodeList(
-  context.Organisation_getComputeNodeTrackingList())

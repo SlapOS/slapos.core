8a2cc912ebb9184520e2248cfdc0506f25d9e03b
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2022-06-01 15:34:56 +0000

    slapos_jio_api_style: use SlapOSCatalogToolCacheMixin to get Compute Node

--- master/bt5/slapos_jio_api_style/SkinTemplateItem/portal_skins/slapos_jio_api/jIOWebSection_getObjectFromData.py
+++ master/bt5/slapos_jio_api_style/SkinTemplateItem/portal_skins/slapos_jio_api/jIOWebSection_getObjectFromData.py
@@ -1,31 +1,12 @@
 portal_type = data_dict["portal_type"]
 
 portal = context.getPortalObject()
-### Start of Duplicated code for getting documents the SlapOS way"
-from Products.ERP5Type.Cache import CachingMethod
-
-def _getNonCachedComputeNodeDocumentUrl(compute_node_reference):
-  return context.Base_getUnrestrictedDocumentUrl(
-      portal_type='Compute Node',
-      # XXX Hardcoded validation state
-      validation_state="validated",
-      reference=compute_node_reference)
-
-def _getComputeNodeDocument(compute_node_reference):
-  """
-  Get the validated compute_node with this reference.
-  """
-  result = CachingMethod(_getNonCachedComputeNodeDocumentUrl,
-      id='_getComputeNodeDocument',
-      cache_factory='slap_cache_factory')(compute_node_reference)
-  if result:
-    return portal.restrictedTraverse(result)
-  return None
-
-
 if portal_type == "Software Installation":
   if "compute_node_id" in data_dict and "software_release_uri" in data_dict:
-    compute_node = _getComputeNodeDocument(data_dict["compute_node_id"])
+    compute_node = portal.portal_catalog.getComputeNodeObject(
+      data_dict["compute_node_id"],
+      raise_not_found=False,
+    )
     if compute_node:
       return compute_node.getSoftwareInstallationFromUrl(data_dict["software_release_uri"])
 

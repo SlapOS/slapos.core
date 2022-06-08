92da569ee243c24fbd79f634dd10f1fa4fcfeadd
Rafael Monnerat <rafael@nexedi.com>
2022-06-08 08:48:37 +0000

    slapos_jio: Drop Handlebars
      This follow up renderjs general directive to not use it.

--- master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_slapos_panel_js.js
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_slapos_panel_js.js
@@ -1,42 +1,25 @@
-/*jslint nomen: true, indent: 2, maxerr: 3 */
-/*global window, document, rJS, Handlebars, RSVP, Node, loopEventListener */
-(function (window, document, rJS, Handlebars, RSVP, Node, loopEventListener) {
+/*jslint nomen: true, indent: 2, maxerr: 3, unparam: true */
+/*global window, document, rJS, RSVP, Node, asBoolean , ensureArray,
+         mergeGlobalActionWithRawActionList, domsugar*/
+(function (window, document, rJS, RSVP, Node, domsugar) {
   "use strict";
 
-  /////////////////////////////////////////////////////////////////
-  // temlates
-  /////////////////////////////////////////////////////////////////
-  // Precompile templates while loading the first gadget instance
-  var gadget_klass = rJS(window),
-    template_element = gadget_klass.__template_element,
-    panel_template_header = Handlebars.compile(template_element
-                         .getElementById("panel-template-header")
-                         .innerHTML),
-    panel_template_body = Handlebars.compile(template_element
-                         .getElementById("panel-template-body")
-                         .innerHTML),
-    panel_template_warning_link = Handlebars.compile(template_element
-                         .getElementById("panel-template-warning-link")
-                         .innerHTML),
-    panel_template_contextual_help = Handlebars.compile(template_element
-                         .getElementById("panel-template-contextual-help")
-                         .innerHTML);
-
-  gadget_klass
+  rJS(window)
     .setState({
-      visible: false,
-      desktop: false
+      visible: false
     })
     //////////////////////////////////////////////
     // acquired method
     //////////////////////////////////////////////
-    .declareAcquiredMethod("getUrlFor", "getUrlFor")
-    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
-    .declareAcquiredMethod("translateHtml", "translateHtml")
-    .declareAcquiredMethod("translate", "translate")
-    .declareAcquiredMethod("renderEditorPanel", "renderEditorPanel")
-    .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("getUrlForList", "getUrlForList")
+    .declareAcquiredMethod("getTranslationList", "getTranslationList")
+    .declareAcquiredMethod("getTranslationDict", "getTranslationDict")
     .declareAcquiredMethod("redirect", "redirect")
+    .declareAcquiredMethod("getSetting", "getSetting")
+    .declareAcquiredMethod("jio_getAttachment", "jio_getAttachment")
+    .declareAcquiredMethod("getUrlParameter", "getUrlParameter")
+    .declareAcquiredMethod("renderEditorPanel", "renderEditorPanel")
+
 
     /////////////////////////////////////////////////////////////////
     // declared methods
@@ -59,13 +42,25 @@
         jio_key: options.jio_key
       });
     })
-
-    .onStateChange(function (modification_dict) {
-      var context = this,
+    .onStateChange(function onStateChange(modification_dict) {
+      var i,
+        context = this,
         gadget = this,
-        queue = new RSVP.Queue(),
         jio_key = modification_dict.jio_key,
-        tmp_element;
+        translation_index_list = [
+          'Services',
+          'Dashboard',
+          'Login Account',
+          'Tickets',
+          'Sites',
+          'Projects',
+          'Invoices',
+          'Servers',
+          'Networks',
+          'Language',
+          'Logout'
+        ],
+        queue = new RSVP.Queue();
 
       if (modification_dict.hasOwnProperty("visible")) {
         if (this.state.visible) {
@@ -78,61 +73,58 @@
           }
         }
       }
-
-      if (modification_dict.hasOwnProperty("global")) {
+      if (modification_dict.hasOwnProperty("editable")) {
         queue
+          // Update the global links
           .push(function () {
-            return RSVP.all([
-              context.getUrlFor({command: 'display', options: {page: "logout"}}),
-              context.getUrlFor({command: 'display', options: {page: "search", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_site_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_ticket_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_invoice_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_service_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_compute_node_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_network_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_project_list", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_person_view", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slapos", editable: true}}),
-              context.getUrlFor({command: 'display', options: {page: "slap_language_view"}})
-            ]);
-          })
-          .push(function (result_list) {
-            // XXX: Customize panel header!
-            return context.translateHtml(
-              panel_template_header() +
-                panel_template_body({
-                "logout_href": result_list[0],
-                "search_href": result_list[1],
-                "organisation_href": result_list[2],
-                "support_request_href": result_list[3],
-                "accounting_href": result_list[4],
-                "instance_tree_href": result_list[5],
-                "compute_node_href": result_list[6],
-                "computer_network_href": result_list[7],
-                "project_href": result_list[8],
-                "person_href": result_list[9],
-                "dashboard_href": result_list[10],
-                "language_href": result_list[11]
-              })
-            );
-          })
-          .push(function (my_translated_or_plain_html) {
-            tmp_element = document.createElement('div');
-            tmp_element.innerHTML = my_translated_or_plain_html;
-            return context.declareGadget('gadget_erp5_panel_shortcut.html', {
-              scope: "gadget_erp5_panel_shortcut",
-              element: tmp_element.querySelector('[data-gadget-scope="erp5_panel_shortcut"]')
+            return RSVP.hash({
+              url_list: gadget.getUrlForList([
+                {command: 'display', options: {page: "slap_service_list", editable: true}},
+                {command: 'display', options: {page: "slapos", editable: true}},
+                {command: 'display', options: {page: "slap_person_view", editable: true}},
+                {command: 'display', options: {page: "slap_ticket_list", editable: true}},
+                {command: 'display', options: {page: "slap_site_list", editable: true}},
+                {command: 'display', options: {page: "slap_project_list", editable: true}},
+                {command: 'display', options: {page: "slap_invoice_list", editable: true}},
+                {command: 'display', options: {page: "slap_compute_node_list", editable: true}},
+                {command: 'display', options: {page: "slap_network_list", editable: true}},
+                {command: 'display', options: {page: "slap_language_view"}},
+                {command: 'display', options: {page: "logout"}}
+              ]),
+              translation_list: gadget.getTranslationList(translation_index_list)
             });
           })
-          .push(function (panel_shortcut) {
-            return panel_shortcut.render({
-              focus: false
-            });
-          })
-          .push(function () {
-            context.element.querySelector("div").appendChild(tmp_element);
-            return context.listenResize();
+          .push(function (result_dict) {
+            var element_list = [],
+              icon_and_key_list = [
+                'home', 'l',
+                'gears', 'h',
+                'user', 'p',
+                'comments', 't',
+                'map-marker', 'k',
+                'cubes', 'w',
+                'credit-card', 'i',
+                'database', 'c',
+                'globe', 'n',
+                'language', 'a',
+                'power-off', 'o'
+              ];
+
+            for (i = 0; i < result_dict.url_list.length; i += 1) {
+              // <li><a href="URL" class="ui-btn-icon-left ui-icon-ICON" data-i18n="TITLE" accesskey="KEY"></a></li>
+              element_list.push(domsugar('li', [
+                domsugar('a', {
+                  href: result_dict.url_list[i],
+                  'class': 'ui-btn-icon-left ui-icon-' + icon_and_key_list[2 * i],
+                  accesskey: icon_and_key_list[2 * i + 1],
+                  text: result_dict.translation_list[i],
+                  'data-i18n': translation_index_list[i]
+                })
+              ]));
+            }
+
+            domsugar(gadget.element.querySelector("ul"),
+                     [domsugar(null, element_list)]);
           });
       }
       // Check for Alerts to pop
@@ -145,7 +137,6 @@
             return context.calculateMyAttentionPointList(jio_key, false);
           });
       }
-
       return queue;
     })
 
@@ -157,45 +148,35 @@
           (evt.target.tagName === 'BUTTON')) {
         return this.toggle();
       }
-      if (this.element.querySelector('a[id="attention-point-link"]') === evt.target) {
+      if (this.element.querySelector('a.attention-point-link') === evt.target) {
         evt.preventDefault();
         return this.calculateMyAttentionPointList(this.state.jio_key, true);
       }
     }, false, false)
 
-    .declareJob('listenResize', function () {
-      // resize should be only trigger after the render method
-      // as displaying the panel rely on external gadget (for translation for example)
-      var result,
-        event,
-        context = this;
-      function extractSizeAndDispatch() {
-        if (window.matchMedia("(min-width: 90em)").matches) {
-          return context.changeState({
-            desktop: true
-          });
-        }
-        return context.changeState({
-          desktop: false
-        });
-      }
-      result = loopEventListener(window, 'resize', false,
-                                 extractSizeAndDispatch);
-      event = document.createEvent("Event");
-      event.initEvent('resize', true, true);
-      window.dispatchEvent(event);
-      return result;
-    })
-
-    .allowPublicAcquisition('notifyChange', function () {
+    .allowPublicAcquisition('notifyChange', function notifyChange() {
       // Typing a search query should not modify the header status
       return;
     })
 
+    .allowPublicAcquisition("notifyFocus", function notifyFocus() {
+      // All html5 fields in ERP5JS triggers this method when focus
+      // is triggered. This is usefull to display error text.
+      // But, in the case of panel, we don't need to handle anything.
+      return;
+    })
+    .allowPublicAcquisition("notifyBlur", function notifyBlur() {
+      // All html5 fields in ERP5JS triggers this method when blur
+      // is triggered now. This is usefull to display error text.
+      // But, in the case of panel, we don't need to handle anything.
+      return;
+    })
+
+
     .declareJob("calculateContextualHelpList", function (jio_key) {
       var context = this,
         queue = new RSVP.Queue(),
-        contextual_help_dl = document.querySelector('dl.dl-contextual-help');
+        contextual_help_dl = context.element.querySelector("dl.dl-contextual-help");
       return queue
         .push(function () {
           return context.getSetting('hateoas_url');
@@ -210,45 +191,72 @@
           );
         })
         .push(function (contextual_help_list) {
+          var i,
+            contextual_help_element_list = [];
           if (contextual_help_list.length > 0) {
-            if (!Boolean(document.querySelector('#contextual-help-link'))) {
-              contextual_help_dl.innerHTML = panel_template_contextual_help({
-                contextual_help_list: contextual_help_list
-              })
+            if (document.querySelector('.contextual-help-link') === null) {
+              contextual_help_element_list.push(domsugar("dt",
+                {
+                  "class": "ui-btn-icon-left ui-icon-question",
+                  "data-i18n": "Help",
+                  "text": "Help"
+                })
+                );
+              for (i = 0; i < contextual_help_list.length; i += 1) {
+                contextual_help_element_list.push(
+                  domsugar("dd", {"class": "document-listview"},
+                    [
+                      domsugar("a", {
+                        "class": "help",
+                        "target": "_blank",
+                        "href": contextual_help_list[i].href,
+                        "text": contextual_help_list[i].title
+                      })])
+                );
               }
+            }
           }
+          return domsugar(contextual_help_dl,
+                {"class": "dl-contextual-help"},
+                contextual_help_element_list);
         });
-     })
+    })
     .declareJob("calculateMyAttentionPointList", function (jio_key, force_open) {
       var context = this,
-        queue = new RSVP.Queue(),
-        attention_point_ul = document.querySelector('ul.ul-attention-point'),
+        attention_point_ul = context.element.querySelector('ul.ul-attention-point'),
         seen_attention_point_dict = JSON.parse(window.sessionStorage.getItem('seen_attention_point_dict') || "{}");
-      return queue
-        .push(function () {
-          return context.getSetting('hateoas_url');
-        })
+      return context.getSetting('hateoas_url')
         .push(function (hateoas_url) {
           if (jio_key === false || jio_key === undefined || jio_key === null) {
             return [[], 'No Alert'];
           }
-          return RSVP.all([context.jio_getAttachment(
-            jio_key,
-            hateoas_url + jio_key + '/Base_getAttentionPointList'
-          ),
-          context.translate('No Alert!')
+          return RSVP.all([
+            context.jio_getAttachment(
+              jio_key,
+              hateoas_url + jio_key + '/Base_getAttentionPointList'
+            ),
+            context.getTranslationList([
+              'No Alert!',
+              'Warnings'])
           ]);
         })
         .push(function (result_list) {
           var attention_point_list = result_list[0],
-            no_alert_caption = result_list[1];
+            no_alert_caption = result_list[1][0];
           if (attention_point_list.length > 0) {
-            if (!Boolean(document.querySelector('#attention-point-link'))) {
-              attention_point_ul.innerHTML = panel_template_warning_link({
-                amount: attention_point_list.length
-              });
-            }
-            if (force_open || (JSON.stringify(seen_attention_point_dict[jio_key]) != JSON.stringify(attention_point_list))) {
+            domsugar(attention_point_ul, {class: 'ul-attention-point'}, [
+              domsugar(
+                "li",
+                {},
+                [
+                  domsugar("a",
+                    {
+                      "class": "ui-btn-icon-notext ui-icon-warning attention-point-link",
+                      "text": result_list[1][1] + " (" + attention_point_list.length + ")"
+                    })
+                ]
+              )]);
+            if (force_open || (JSON.stringify(seen_attention_point_dict[jio_key]) !== JSON.stringify(attention_point_list))) {
               seen_attention_point_dict[jio_key] = attention_point_list;
               window.sessionStorage.setItem('seen_attention_point_dict', JSON.stringify(seen_attention_point_dict));
               return context.renderEditorPanel("gadget_erp5_attention_point.html", {
@@ -256,7 +264,10 @@
               });
             }
           } else {
-            attention_point_ul.innerHTML = "<li></li>";
+            domsugar(attention_point_ul,
+              {class: 'ul-attention-point'},
+              [domsugar("li")]);
+
             if (force_open) {
               return context.renderEditorPanel("gadget_erp5_attention_point.html", {
                 attention_point_list: [[no_alert_caption, 'no-alert']]
@@ -266,4 +277,4 @@
         });
     });
 
-}(window, document, rJS, Handlebars, RSVP, Node, loopEventListener));
+}(window, document, rJS, RSVP, Node, domsugar));

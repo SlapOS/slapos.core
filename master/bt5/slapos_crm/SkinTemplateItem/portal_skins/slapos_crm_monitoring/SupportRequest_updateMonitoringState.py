c9b2e93241300bd39da1b9a9121e451c3d89fe4f
Romain Courteaud <romain@nexedi.com>
2022-06-15 09:38:57 +0000

    slapos_crm: support for the virtual master logic
    Unify Ticket and Event creation, to ensure consistency on the different categories.
    Use causality category instead of aggregate, to link the Ticket to the context document (instance, node, ...).
    Aggregate must be used to define the item of the movement resource.
    
    Monitoring: the tickets are managed by the virtual master admin,
    who is responsible to provide a good service quality.
    Admin will use their worklist to see the ongoing tickets.
    
    Regularisation Request: this ticket is handle by the accounting team.
    Unpaid invoice can be related to any kind of service (instance, node), which
    have only 2 states from the ticket point of view: running or destroyed.
    Only check the automated ledger, do not be impacted by other kind of invoices.
    
    Prevent allocation if a customer has an ongoing Regularisation Request.
    
    TODO: handle case of unpaid invoices for professional customers
    
    Also in this commit:
    * drop templates
    * use interaction workflow to trigger alarms

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/SupportRequest_updateMonitoringState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/SupportRequest_updateMonitoringState.py
@@ -1,10 +1,38 @@
+""" Close Support Request which are related to a Destroy Requested Instance. """
+from Products.ERP5Type.Errors import UnsupportedWorkflowMethod
+portal = context.getPortalObject()
+
 if context.getSimulationState() == "invalidated":
   return
 
-document = context.getAggregateValue(portal_type="Instance Tree")
+document = context.getCausalityValue(portal_type="Instance Tree")
 if document is None:
-  return 
+  return
 
-if document.getPortalType() == "Instance Tree":
-  if document.getSlapState() == "destroy_requested":
-    return context.SupportRequest_updateMonitoringDestroyRequestedState()
+if document.getSlapState() == "destroy_requested":
+
+  # Send Notification message
+  message = """ Closing this ticket as the Instance Tree was destroyed by the user.
+  """
+
+  ticket_title = "Instance Tree was destroyed was destroyed by the user"
+  event = context.SupportRequest_getLastEvent(ticket_title)
+  if event is None:
+    context.Ticket_createProjectEvent(
+      ticket_title, 'outgoing', 'Web Message',
+      portal.service_module.slapos_crm_information.getRelativeUrl(),
+      text_content=message,
+      content_type='text/plain',
+      notification_message="slapos-crm-support-request-close-destroyed-notification",
+      #language=XXX,
+      substitution_method_parameter_dict={
+        'instance_tree_title': document.getTitle()
+      }
+    )
+
+  try:
+    context.validate()
+  except UnsupportedWorkflowMethod:
+    pass
+  context.invalidate()
+  return event

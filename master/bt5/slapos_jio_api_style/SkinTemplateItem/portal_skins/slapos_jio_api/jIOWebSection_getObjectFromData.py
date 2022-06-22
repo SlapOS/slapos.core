530ff9c79e55671c53477f83a40e3103614f2a53
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2022-06-22 17:32:05 +0200

    slapos_jio_api_style: Add dedicated JSON form to get Software Instance
    This is needed as we get software instances by their reference and not
    their relative urls

--- master/bt5/slapos_jio_api_style/SkinTemplateItem/portal_skins/slapos_jio_api/jIOWebSection_getObjectFromData.py
+++ master/bt5/slapos_jio_api_style/SkinTemplateItem/portal_skins/slapos_jio_api/jIOWebSection_getObjectFromData.py
@@ -9,4 +9,10 @@ if portal_type == "Software Installation":
     if compute_node:
       return compute_node.getSoftwareInstallationFromUrl(data_dict["software_release_uri"])
 
+elif portal_type == "Software Instance":
+  if "reference" in data_dict:
+    software_instance = portal.portal_catalog.getSoftwareInstanceObject(data_dict["reference"])
+    if software_instance:
+      return software_instance
+
 return None

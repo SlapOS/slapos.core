cf4abc44d5e0019e517448636afe1ade6dceed68
Romain Courteaud <romain@nexedi.com>
2022-07-05 09:43:06 +0000

    slapos_jio_before_deletion:
    * restore status gadget first rendering
      onLoop must be fixed probably
    * drop organisation/site pages
    * drop project creation/deletion pages
    * drop gadget_erp5_page_slap_cloud_contract_view_js
    * drop template_software_installation
    * drop template_instance_tree
    * drop template_software_instance
    * move login page templates
    * moved to slapos_panel
    * moved from slapos_jio
    * drop pages

---
+++ master/bt5/slapos_jio_before_deletion/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
@@ -0,0 +1,20 @@
+# TODO how to avoid hardcode here? from InstanceTree_getConnectionParameterList?
+base_url = 'https://monitor.app.officejs.com/#/?page=ojsm_landing'
+
+try:
+  instance_tree = context
+  if context.getPortalType() in ["Software Instance", "Slave Instance"]:
+    instance_tree = context.getSpecialise()
+  connection_parameter_dict = instance_tree.InstanceTree_getMonitorParameterDict()
+  connection_url = '&url=%s'% connection_parameter_dict['url'] + '&username=%s'% connection_parameter_dict['username'] + '&password=%s'% connection_parameter_dict['password']
+except (AttributeError, TypeError) as e:
+  connection_url = ''
+
+if context.getPortalType() == "Instance Tree":
+  for connection_parameter in context.InstanceTree_getConnectionParameterList(raw=True):
+    if connection_parameter['connection_key'] == "monitor-setup-url":
+      return connection_parameter['connection_value']
+  return base_url + '&query=portal_type:"Instance Tree" AND title:(%s)' % context.getTitle() + connection_url
+
+if context.getPortalType() in ["Software Instance", "Slave Instance"]:
+  return base_url + '&query=portal_type:"Software Instance" AND title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle() + connection_url

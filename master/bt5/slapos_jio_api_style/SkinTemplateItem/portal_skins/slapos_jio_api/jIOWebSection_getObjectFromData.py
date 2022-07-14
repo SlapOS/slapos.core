5e10a8f3089d8adf9f54ace5422db5844c161086
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2022-07-14 09:56:54 +0000

    slapos_jio_api_style: Add Compute Node in get Object From Data

--- master/bt5/slapos_jio_api_style/SkinTemplateItem/portal_skins/slapos_jio_api/jIOWebSection_getObjectFromData.py
+++ master/bt5/slapos_jio_api_style/SkinTemplateItem/portal_skins/slapos_jio_api/jIOWebSection_getObjectFromData.py
@@ -9,6 +9,18 @@ if portal_type == "Software Installation":
     if compute_node:
       return compute_node.getSoftwareInstallationFromUrl(data_dict["software_release_uri"])
 
+elif portal_type == "Compute Node":
+  if "compute_node_id" in data_dict:
+    compute_node_id = data_dict["compute_node_id"]
+    user = portal.portal_membership.getAuthenticatedMember().getUserName()
+    if str(user) == compute_node_id:
+      compute_node = portal.portal_membership.getAuthenticatedMember().getUserValue()
+      compute_node.setAccessStatus(compute_node_id)
+    else:
+      compute_node = portal.portal_catalog.getComputeNodeObject(compute_node_id)
+    if compute_node:
+      return compute_node
+
 elif portal_type == "Software Instance":
   if "reference" in data_dict:
     software_instance = portal.portal_catalog.getSoftwareInstanceObject(

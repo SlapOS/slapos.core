f52598e953e99eb2cae1022f22dde59ad1b7cc9f
Romain Courteaud <romain@nexedi.com>
2022-08-10 14:20:23 +0000

    slapos_pdm:
    * add SlapOSVirtualMasterSourceProjectConstraint on Upgrade Decision
    * software product must be linked to a project
    * create Software Type portal type
      It is a variation of a Software Product
    * rename Software Type to Software Product Type Variation
    * add Software Product Release Variation portal type
    * force default use/base_contribution on Software Product
      Didn't find another way to do it (init script requires data migration)
    * require Software Product to have at least one Type and one Release
    * store url_string on Release Variation
    * use edit_workflow on Variation portal types
    * use the jump action to access Software Release
    * add dedicated form to view Release
    * add Software Type view
    * set SoftwareProduct default quantity unit
    * support document without categories property
    * add constraint to ensure quantity unit of a Software Product is defined
    * drop Software Release configuration. Drop hardcoded template only used in tests
    * fixup
      See 1173ea0edf89154719e92acb79c8867c702560c6
    * drop Compute Node upgrade
      This is now handled manually by updating the Allocation Supply
    * stop allowing Upgrade Decision Line creation
    * Upgrade Decision is a ticket
    * wip upgrade decision redesign with allocation supply
    * not commited script
    * really compare url strings
    * update upgrade decision view
    * only create upgrade decision for SOftware Instance for now
    * set the compute node on the test allocation supply
    * drop not used scripts
    * remote tests from dropped scripts
    * mark test to check
    * test alarm_instance_tree_create_upgrade_decision
    * drop upgrade_scope
    * test upgrade decision creation for not allocated instance tree
    * test Upgrade for allocated Software Instance
    * drop deprecated tests
    * root shared instance can not be upgraded by Allocation Supply on the same tree
    * upgrade not allocated or allocated on remote slave instances
    * update not allocated in there is instance node supply
    * test upgrade Instance Node
    * drop unused InstanceTree_getUpgradeSubscriptionRelatedValue
    * drop UpgradeDecisionLine_cancel
    * drop Upgrade Decision Line view
    * drop SoftwareProduct_getSortedSoftwareReleaseList
    * fix upgrade decision list view
    * drop InstanceTree_isUpgradePossible
      No way to know which software release url on instance will request.
    * drop UpgradeDecision_processUpgrade
    * drop Base_acceptUpgradeDecision
    * drop Base_rejectUpgradeDecision
    * drop reviewRegistration
    * check created upgrade decision
    * do not propose again to upgrade if decision has been rejected
    * no need to check all rejected decisions
    * do not put newly create decision in started
      Ask review from manager
    * drop UpgradeDecision_notify
    * drop UpgradeDecision_notifyDelivered
    * drop reviewUpgrade
    * drop UpgradeDecision_getAggregateValue
    * drop approveRegistration
    * drop upgrade_slap_interface_workflow
    * drop UpgradeDecision_requestChangeState
    * drop slapos_pdm_auto_cancel_upgrade_decision
    * use UpgradeDecision_processUpgrade
    * alarm to destroy software installation
    * drop unused template
    * configure Upgrade Decision worklist
    * drop not needed script arguments
    * add Upgrade Decision constraints
    * method moved to global mixin
    * do not upgrade if there is no instance yet
    * create upgrade decision as soon as Allocation Supply is validated
    * workaround to prevent breaking the instance when classes are live reloaded
    * test: disable alarm when creating an allocation supply
    * test: disable alarm
    * software_release is an optional variation
      We can not update the Open Order every time one Instance is upgraded
    * sort SoftwareProduct getVariationBaseCategoryList's result
    * update xml format
    * optional_variation_base_category_list are not set in variation_base_category_list
    * force software change from Upgrade Decision
    * drop template_instance_tree
    * allow to force upgrade to a specific release
    * rely on self instead of one a global
    * drop view_related_upgrade_decision on Compute Node
      Deprecated for a long time

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/InstanceTree_createUpgradeDecision.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/InstanceTree_createUpgradeDecision.py
@@ -2,64 +2,122 @@ from DateTime import DateTime
 portal = context.getPortalObject()
 
 instance_tree = context
-upgrade_scope = context.getUpgradeScope("ask_confirmation")
-if upgrade_scope in ["never", "disabled"]:
-  return
-
-root_instance_list = [
-  q for q in instance_tree.getSuccessorValueList(portal_type=["Software Instance", "Slave Instance"])
-  if q.getSlapState() != 'destroy_requested']
-if len(root_instance_list) == 0:
-  return
-root_instance = root_instance_list[0]
-
-slave_upgrade = False
-if root_instance.getPortalType() == 'Slave Instance':
-  slave_upgrade = True
-  upgrade_scope = "auto"
 
 if instance_tree.getSlapState() == "destroy_requested":
   return
 
+if (target_software_release is not None) or (target_software_type is not None):
+  # Those parameters will be use to force an upgrade decision
+  # to a specific release / type
+  # used by Remote Node for example
+  assert target_software_release is not None
+  assert target_software_type is not None
+
+if portal.portal_catalog.getResultValue(
+  portal_type='Upgrade Decision',
+  aggregate__uid=instance_tree.getUid(),
+  simulation_state=['started', 'stopped', 'planned', 'confirmed']
+) is not None:
+  # There is already a upgrade decision, do nothing
+  return
+
+# Check if UpgradeDecision_approveRegistration is running
+# XXX we should instead look at the current upgrade decision state
 tag = "%s_requestUpgradeDecisionCreation_inProgress" % instance_tree.getUid()
 if portal.portal_activities.countMessageWithTag(tag) > 0:
-  # nothing to do
+  # previous run not finished, skip
   return
 
-partition = root_instance.getAggregateValue(portal_type="Compute Partition")
-if partition is None:
+software_product, software_release, software_type = instance_tree.InstanceTree_getSoftwareProduct()
+if software_product is None:
+  # No way to upgrade, if we can find which Software Product to upgrade
+  return
+assert software_release.getUrlString() == instance_tree.getUrlString()
+
+compute_node = None
+root_instance = None
+root_instance_list = [
+  q for q in instance_tree.getSuccessorValueList(portal_type=["Software Instance", "Slave Instance"])
+  if q.getSlapState() != 'destroy_requested']
+if len(root_instance_list) != 0:
+  root_instance = root_instance_list[0]
+  partition = root_instance.getAggregateValue()
+  if partition is not None:
+    compute_node = partition.getParentValue()
+
+    if (root_instance.getPortalType() == 'Slave Instance'):
+      if (compute_node.getPortalType() == 'Compute Node'):
+        # Search the instance node linked to this partition
+        soft_instance = partition.getAggregateRelatedValue(portal_type='Software Instance')
+        if soft_instance is None:
+          return
+        instance_node = soft_instance.getSpecialiseRelatedValue(portal_type='Instance Node')
+        if instance_node is None:
+          return
+        compute_node = instance_node
+      elif (compute_node.getPortalType() != 'Remote Node'):
+        return
+
+person = context.getDestinationSectionValue()
+if person is None:
   return
 
-decision_title = 'A new upgrade is available for %s' % instance_tree.getTitle()
-newer_release = None
-
-if slave_upgrade:
-  software_instance = partition.getAggregateRelatedValue(portal_type='Software Instance')
-  if software_instance:
-    url_string = software_instance.getUrlString()
-    if url_string != instance_tree.getUrlString():
-      newer_release = context.portal_catalog.getResultValue(portal_type='Software Release', url_string=url_string)
-else:
-  newer_release = instance_tree.\
-                  InstanceTree_getUpgradableSoftwareRelease()
-if newer_release is None:
-  return
-
-decision_in_progress = newer_release.\
-    SoftwareRelease_getUpgradeDecisionInProgress(instance_tree.getUid())
-
-if decision_in_progress:
-  decision_in_progress.reviewRegistration(
-    software_release_url=newer_release.getUrlString())
-  if decision_in_progress.getSimulationState() != "cancelled":
-    return
-
-upgrade_decision = newer_release.SoftwareRelease_createUpgradeDecision(
-  source_url=instance_tree.getRelativeUrl(),
-  title=decision_title
+# Search if the product with the same type
+# XXX search only for the main node
+allocation_cell_list = software_product.getFollowUpValue().Project_getSoftwareProductPredicateList(
+  software_product=software_product,
+  software_product_type=target_software_type or software_type,
+  software_product_release=target_software_release,
+  destination_value=person,
+  node_value=compute_node,
+  predicate_portal_type='Allocation Supply Cell'
 )
 
-upgrade_decision.approveRegistration(
-  upgrade_scope=upgrade_scope)
+if (compute_node is None) and (root_instance is not None):
+  # Do not upgrade if there is no instance yet
+  allocation_cell_node_list = [(x, [y.getPortalType() for y in x.getParentValue().getParentValue().getAggregateValueList()]) for x in allocation_cell_list]
+  if (root_instance.getPortalType() == 'Slave Instance'):
+    allocation_cell_list = [x for x, y in allocation_cell_node_list if ("Remote Node" in y) or ("Instance Node" in y)]
+  elif (root_instance.getPortalType() == 'Software Instance'):
+    allocation_cell_list = [x for x, y in allocation_cell_node_list if ("Remote Node" in y) or ("Compute Node" in y)]
 
-return upgrade_decision
+# Only upgrade if there is no doubt (ie, only 1 url is allowed)
+if len(allocation_cell_list) == 1:
+  if (allocation_cell_list[0].getSoftwareReleaseValue().getUrlString() != instance_tree.getUrlString()):
+    # XXX Upgrade
+
+    if compute_node is not None:
+      assert compute_node.getRelativeUrl() in allocation_cell_list[0].getParentValue().getParentValue().getAggregateList()
+
+    if portal.portal_catalog.getResultValue(
+      portal_type='Upgrade Decision',
+      aggregate__uid=instance_tree.getUid(),
+      simulation_state=['rejected'],
+      software_release__uid=allocation_cell_list[0].getSoftwareReleaseUid()
+    ):
+      # If same upgrade decision has been rejected, do nothing
+      return
+
+    decision_title = 'A new upgrade is available for %s' % instance_tree.getTitle()
+    upgrade_decision = portal.upgrade_decision_module.newContent(
+      portal_type='Upgrade Decision',
+      title=decision_title,
+      destination_section_value=person,
+      destination_decision_value=person,
+      destination_project_value=instance_tree.getFollowUpValue(),
+      resource_value=software_product,
+      variation_category_list=allocation_cell_list[0].getVariationCategoryList(),
+      aggregate_value=instance_tree,
+    )
+
+    with upgrade_decision.defaultActivateParameterDict({'tag': tag}):
+      if upgrade_decision.getSimulationState() == "draft":
+        upgrade_decision.plan()
+      upgrade_decision.setStartDate(DateTime())
+      if upgrade_decision.getSimulationState() == "planned":
+        upgrade_decision.confirm()
+
+    # Prevent concurrent transaction to create 2 upgrade decision for the same instance_tree
+    instance_tree.serialize()
+
+    return upgrade_decision

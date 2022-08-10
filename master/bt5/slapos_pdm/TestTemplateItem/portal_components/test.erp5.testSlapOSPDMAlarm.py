8846c82efb62409623e2a72fdb3d1079d31830af
Romain Courteaud <romain@nexedi.com>
2022-08-10 14:20:23 +0000

    slapos_pdm:
    * add SlapOSVirtualMasterSourceProjectConstraint on Upgrade Decision
    * software product must be linked to a project
    * create Software Type portal type
      It is a variation of a Software Product
    * rename Software Type to Software Product Type Variation
    * add Software Product Release Variation portal type
    * force default use/base_contribution on Software Product
      Didn't find another way to do it (init script requires data migration)
    * require Software Product to have at least one Type and one Release
    * store url_string on Release Variation
    * use edit_workflow on Variation portal types
    * use the jump action to access Software Release
    * add dedicated form to view Release
    * add Software Type view
    * set SoftwareProduct default quantity unit
    * support document without categories property
    * add constraint to ensure quantity unit of a Software Product is defined
    * drop Software Release configuration. Drop hardcoded template only used in tests
    * fixup
      See 1173ea0edf89154719e92acb79c8867c702560c6
    * drop Compute Node upgrade
      This is now handled manually by updating the Allocation Supply
    * stop allowing Upgrade Decision Line creation
    * Upgrade Decision is a ticket
    * wip upgrade decision redesign with allocation supply
    * not commited script
    * really compare url strings
    * update upgrade decision view
    * only create upgrade decision for SOftware Instance for now
    * set the compute node on the test allocation supply
    * drop not used scripts
    * remote tests from dropped scripts
    * mark test to check
    * test alarm_instance_tree_create_upgrade_decision
    * drop upgrade_scope
    * test upgrade decision creation for not allocated instance tree
    * test Upgrade for allocated Software Instance
    * drop deprecated tests
    * root shared instance can not be upgraded by Allocation Supply on the same tree
    * upgrade not allocated or allocated on remote slave instances
    * update not allocated in there is instance node supply
    * test upgrade Instance Node
    * drop unused InstanceTree_getUpgradeSubscriptionRelatedValue
    * drop UpgradeDecisionLine_cancel
    * drop Upgrade Decision Line view
    * drop SoftwareProduct_getSortedSoftwareReleaseList
    * fix upgrade decision list view
    * drop InstanceTree_isUpgradePossible
      No way to know which software release url on instance will request.
    * drop UpgradeDecision_processUpgrade
    * drop Base_acceptUpgradeDecision
    * drop Base_rejectUpgradeDecision
    * drop reviewRegistration
    * check created upgrade decision
    * do not propose again to upgrade if decision has been rejected
    * no need to check all rejected decisions
    * do not put newly create decision in started
      Ask review from manager
    * drop UpgradeDecision_notify
    * drop UpgradeDecision_notifyDelivered
    * drop reviewUpgrade
    * drop UpgradeDecision_getAggregateValue
    * drop approveRegistration
    * drop upgrade_slap_interface_workflow
    * drop UpgradeDecision_requestChangeState
    * drop slapos_pdm_auto_cancel_upgrade_decision
    * use UpgradeDecision_processUpgrade
    * alarm to destroy software installation
    * drop unused template
    * configure Upgrade Decision worklist
    * drop not needed script arguments
    * add Upgrade Decision constraints
    * method moved to global mixin
    * do not upgrade if there is no instance yet
    * create upgrade decision as soon as Allocation Supply is validated
    * workaround to prevent breaking the instance when classes are live reloaded
    * test: disable alarm when creating an allocation supply
    * test: disable alarm
    * software_release is an optional variation
      We can not update the Open Order every time one Instance is upgraded
    * sort SoftwareProduct getVariationBaseCategoryList's result
    * update xml format
    * optional_variation_base_category_list are not set in variation_base_category_list
    * force software change from Upgrade Decision
    * drop template_instance_tree
    * allow to force upgrade to a specific release
    * rely on self instead of one a global
    * drop view_related_upgrade_decision on Compute Node
      Deprecated for a long time

--- master/bt5/slapos_pdm/TestTemplateItem/portal_components/test.erp5.testSlapOSPDMAlarm.py
+++ master/bt5/slapos_pdm/TestTemplateItem/portal_components/test.erp5.testSlapOSPDMAlarm.py
@@ -1,47 +1,26 @@
-# -*- coding:utf-8 -*-
-##############################################################################
-#
-# Copyright (c) 2002-2013 Nexedi SA and Contributors. All Rights Reserved.
-#
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
-#
-##############################################################################
-from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin
+# Copyright (c) 2013 Nexedi SA and Contributors. All Rights Reserved.
+from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin, TemporaryAlarmScript
 
 class TestSlapOSUpgradeDecisionProcess(SlapOSTestCaseMixin):
-  def afterSetUp(self):
-    SlapOSTestCaseMixin.afterSetUp(self)
-    self.new_id = self.generateNewId()
 
   def _makeUpgradeDecision(self, confirm=True):
     upgrade_decision = self.portal.\
        upgrade_decision_module.newContent(
          portal_type="Upgrade Decision",
-         title="TESTUPDE-%s" % self.new_id)
+         title="TESTUPDE-%s" % self.generateNewId())
     if confirm:
       upgrade_decision.confirm()
     return upgrade_decision
 
   def _makeInstanceTree(self, slap_state="start_requested"):
     instance_tree = self.portal\
-      .instance_tree_module.template_instance_tree\
-      .Base_createCloneDocument(batch_mode=1)
+      .instance_tree_module\
+      .newContent(portal_type="Instance Tree")
     instance_tree.validate()
+    new_id = self.generateNewId()
     instance_tree.edit(
-        title= "Test hosting sub start %s" % self.new_id,
-        reference="TESTHSS-%s" % self.new_id,
+        title= "Test hosting sub start %s" % new_id,
+        reference="TESTHSS-%s" % new_id
     )
     self.portal.portal_workflow._jumpToStateFor(instance_tree, slap_state)
 
@@ -51,84 +30,54 @@ class TestSlapOSUpgradeDecisionProcess(SlapOSTestCaseMixin):
     upgrade_decision = self._makeUpgradeDecision()
     upgrade_decision.start()
     self.tic()
-    self._test_alarm(
-      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_started,
-      upgrade_decision,
-      'UpgradeDecision_processUpgrade')
 
-  def test_alarm_upgrade_decision_process_planned(self):
-    upgrade_decision = self._makeUpgradeDecision(confirm=0)
-    upgrade_decision.plan()
-    self._test_alarm(
-      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_planned,
-      upgrade_decision,
-      'UpgradeDecision_notify')
+    with TemporaryAlarmScript(self.portal, 'UpgradeDecision_processUpgrade'):
+      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_started.\
+        activeSense()
+      self.tic()
 
-  def test_alarm_upgrade_decision_process_stopped(self):
-    upgrade_decision = self._makeUpgradeDecision()
-    upgrade_decision.start()
-    upgrade_decision.stop()
-    self.tic()
-    self._test_alarm(
-      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_stopped,
-      upgrade_decision,
-      'UpgradeDecision_notifyDelivered')
-
-  def _test_alarm_compute_node_create_upgrade_decision(self, allocation_scope, upgrade_scope):
-    compute_node = self._makeComputeNode(allocation_scope=allocation_scope)[0]
-    compute_node.setUpgradeScope(upgrade_scope)
-    self._test_alarm(
-      self.portal.portal_alarms.slapos_pdm_compute_node_create_upgrade_decision,
-      compute_node,
-      'ComputeNode_checkAndCreateUpgradeDecision')
-
-  def _test_alarm_compute_node_create_upgrade_decision_not_visited(self, allocation_scope, upgrade_scope):
-    compute_node = self._makeComputeNode(allocation_scope=allocation_scope)[0]
-    compute_node.setUpgradeScope(upgrade_scope)
-    self._test_alarm_not_visited(
-      self.portal.portal_alarms.slapos_pdm_compute_node_create_upgrade_decision,
-      compute_node,
-      'ComputeNode_checkAndCreateUpgradeDecision')
-
-
-  def test_alarm_compute_node_create_upgrade_decision_auto(self):
-    self._test_alarm_compute_node_create_upgrade_decision('open/public', 'auto')
-    self._test_alarm_compute_node_create_upgrade_decision('open/personal', 'auto')
-    self._test_alarm_compute_node_create_upgrade_decision('open/subscription', 'auto')
-    self._test_alarm_compute_node_create_upgrade_decision('close/outdated', 'auto')
-    self._test_alarm_compute_node_create_upgrade_decision('close/maintanance', 'auto')
-    self._test_alarm_compute_node_create_upgrade_decision('close/termination', 'auto')
-    self._test_alarm_compute_node_create_upgrade_decision('close/noallocation', 'auto')
-
-  def test_alarm_compute_node_create_upgrade_decision_ask_confirmation(self):
-    self._test_alarm_compute_node_create_upgrade_decision('open/public', 'confirmation')
-    self._test_alarm_compute_node_create_upgrade_decision('open/personal', 'confirmation')
-    self._test_alarm_compute_node_create_upgrade_decision('open/subscription', 'confirmation')
-    self._test_alarm_compute_node_create_upgrade_decision('close/outdated', 'confirmation')
-    self._test_alarm_compute_node_create_upgrade_decision('close/maintanance', 'confirmation')
-    self._test_alarm_compute_node_create_upgrade_decision('close/termination', 'confirmation')
-    self._test_alarm_compute_node_create_upgrade_decision('close/noallocation', 'confirmation')
-
-
-  def test_alarm_compute_node_create_upgrade_decision_never(self):
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('open/public', 'never')
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('open/personal', 'never')
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('open/subscription', 'never')
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('close/outdated', 'never')
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('close/maintanance', 'never')
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('close/termination', 'never')
-    self._test_alarm_compute_node_create_upgrade_decision_not_visited('close/noallocation', 'never')
+    self.assertEqual(
+        'Visited by UpgradeDecision_processUpgrade',
+        upgrade_decision.workflow_history['edit_workflow'][-1]['comment'])
 
   def test_alarm_instance_tree_create_upgrade_decision(self):
     instance_tree = self._makeInstanceTree()
-    self._test_alarm(
-      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision,
-      instance_tree,
-      'InstanceTree_createUpgradeDecision')
+    self.tic()
+
+    with TemporaryAlarmScript(self.portal, 'InstanceTree_createUpgradeDecision'):
+      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision.\
+        activeSense()
+      self.tic()
+
+    self.assertEqual('Visited by InstanceTree_createUpgradeDecision',
+      instance_tree.workflow_history['edit_workflow'][-1]['comment'])
 
   def test_alarm_create_upgrade_decision_destroyed_instance_tree(self):
     instance_tree = self._makeInstanceTree(slap_state="destroy_requested")
-    self._test_alarm_not_visited(
-      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision,
-      instance_tree,
-      'InstanceTree_createUpgradeDecision')
+    self.tic()
+
+    with TemporaryAlarmScript(self.portal, 'InstanceTree_createUpgradeDecision'):
+      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision.\
+        activeSense()
+      self.tic()
+
+    self.assertNotEqual('Visited by InstanceTree_createUpgradeDecision',
+      instance_tree.workflow_history['edit_workflow'][-1]['comment'])
+
+  def test_alarm_destroy_unused_software_installation(self):
+    software_installation = self.portal.software_installation_module.newContent(
+      portal_type='Software Installation'
+    )
+    self.portal.portal_workflow._jumpToStateFor(software_installation,
+                                                'start_requested')
+    self.portal.portal_workflow._jumpToStateFor(software_installation,
+                                                'validated')
+    self.tic()
+
+    with TemporaryAlarmScript(self.portal, 'SoftwareInstallation_destroyIfUnused'):
+      self.portal.portal_alarms.slapos_pdm_destroy_unused_software_installation.\
+        activeSense()
+      self.tic()
+
+    self.assertEqual('Visited by SoftwareInstallation_destroyIfUnused',
+      software_installation.workflow_history['edit_workflow'][-1]['comment'])

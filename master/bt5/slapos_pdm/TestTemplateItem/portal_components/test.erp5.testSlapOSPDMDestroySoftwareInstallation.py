b8b53d61163bf0f901cdd5a7c465aa12e7a348e5
Romain Courteaud <romain@nexedi.com>
2022-08-10 14:20:23 +0000

    slapos_pdm:
    * add SlapOSVirtualMasterSourceProjectConstraint on Upgrade Decision
    * software product must be linked to a project
    * create Software Type portal type
      It is a variation of a Software Product
    * rename Software Type to Software Product Type Variation
    * add Software Product Release Variation portal type
    * force default use/base_contribution on Software Product
      Didn't find another way to do it (init script requires data migration)
    * require Software Product to have at least one Type and one Release
    * store url_string on Release Variation
    * use edit_workflow on Variation portal types
    * use the jump action to access Software Release
    * add dedicated form to view Release
    * add Software Type view
    * set SoftwareProduct default quantity unit
    * support document without categories property
    * add constraint to ensure quantity unit of a Software Product is defined
    * drop Software Release configuration. Drop hardcoded template only used in tests
    * fixup
      See 1173ea0edf89154719e92acb79c8867c702560c6
    * drop Compute Node upgrade
      This is now handled manually by updating the Allocation Supply
    * stop allowing Upgrade Decision Line creation
    * Upgrade Decision is a ticket
    * wip upgrade decision redesign with allocation supply
    * not commited script
    * really compare url strings
    * update upgrade decision view
    * only create upgrade decision for SOftware Instance for now
    * set the compute node on the test allocation supply
    * drop not used scripts
    * remote tests from dropped scripts
    * mark test to check
    * test alarm_instance_tree_create_upgrade_decision
    * drop upgrade_scope
    * test upgrade decision creation for not allocated instance tree
    * test Upgrade for allocated Software Instance
    * drop deprecated tests
    * root shared instance can not be upgraded by Allocation Supply on the same tree
    * upgrade not allocated or allocated on remote slave instances
    * update not allocated in there is instance node supply
    * test upgrade Instance Node
    * drop unused InstanceTree_getUpgradeSubscriptionRelatedValue
    * drop UpgradeDecisionLine_cancel
    * drop Upgrade Decision Line view
    * drop SoftwareProduct_getSortedSoftwareReleaseList
    * fix upgrade decision list view
    * drop InstanceTree_isUpgradePossible
      No way to know which software release url on instance will request.
    * drop UpgradeDecision_processUpgrade
    * drop Base_acceptUpgradeDecision
    * drop Base_rejectUpgradeDecision
    * drop reviewRegistration
    * check created upgrade decision
    * do not propose again to upgrade if decision has been rejected
    * no need to check all rejected decisions
    * do not put newly create decision in started
      Ask review from manager
    * drop UpgradeDecision_notify
    * drop UpgradeDecision_notifyDelivered
    * drop reviewUpgrade
    * drop UpgradeDecision_getAggregateValue
    * drop approveRegistration
    * drop upgrade_slap_interface_workflow
    * drop UpgradeDecision_requestChangeState
    * drop slapos_pdm_auto_cancel_upgrade_decision
    * use UpgradeDecision_processUpgrade
    * alarm to destroy software installation
    * drop unused template
    * configure Upgrade Decision worklist
    * drop not needed script arguments
    * add Upgrade Decision constraints
    * method moved to global mixin
    * do not upgrade if there is no instance yet
    * create upgrade decision as soon as Allocation Supply is validated
    * workaround to prevent breaking the instance when classes are live reloaded
    * test: disable alarm when creating an allocation supply
    * test: disable alarm
    * software_release is an optional variation
      We can not update the Open Order every time one Instance is upgraded
    * sort SoftwareProduct getVariationBaseCategoryList's result
    * update xml format
    * optional_variation_base_category_list are not set in variation_base_category_list
    * force software change from Upgrade Decision
    * drop template_instance_tree
    * allow to force upgrade to a specific release
    * rely on self instead of one a global
    * drop view_related_upgrade_decision on Compute Node
      Deprecated for a long time

---
+++ master/bt5/slapos_pdm/TestTemplateItem/portal_components/test.erp5.testSlapOSPDMDestroySoftwareInstallation.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2019 Nexedi SA and Contributors. All Rights Reserved.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+#
+##############################################################################
+
+from erp5.component.test.SlapOSTestCaseMixin import SlapOSTestCaseMixin
+
+class TestSlapOSDestroySoftwareInstallation(SlapOSTestCaseMixin):
+
+  def bootstrapSoftwareInstallation(self, is_allocated=True, is_supplied=True):
+    if is_allocated:
+      allocation_state = 'allocated'
+    else:
+      allocation_state = 'possible'
+    software_product, release_variation, type_variation, compute_node, _, _ = \
+      self.bootstrapAllocableInstanceTree(allocation_state=allocation_state)
+
+    software_installation = self.portal.software_installation_module.newContent(
+      portal_type='Software Installation',
+      follow_up_value=compute_node.getFollowUpValue(),
+      url_string=release_variation.getUrlString(),
+      aggregate_value=compute_node
+    )
+
+    self.portal.portal_workflow._jumpToStateFor(software_installation,
+                                                'start_requested')
+    self.portal.portal_workflow._jumpToStateFor(software_installation,
+                                                'validated')
+
+    supply = None
+    if is_supplied:
+      supply = self.addAllocationSupply("for compute node", compute_node, software_product,
+                                        release_variation, type_variation)
+    self.tic()
+    return software_installation, supply
+
+  ########################################################
+  # Software Installation in expected state
+  ########################################################
+  def test_destroyIfUnused_toDestroy(self):
+    software_installation, _ = self.bootstrapSoftwareInstallation(is_allocated=False, is_supplied=False)
+
+    software_installation.SoftwareInstallation_destroyIfUnused()
+    self.assertEqual('destroy_requested', software_installation.getSlapState())
+    self.assertEqual('validated', software_installation.getValidationState())
+
+  ########################################################
+  # Software Installation not in expected state
+  ########################################################
+  def test_destroyIfUnused_notStarted(self):
+    software_installation, _ = self.bootstrapSoftwareInstallation()
+    self.portal.portal_workflow._jumpToStateFor(software_installation,
+                                                'draft',
+                                                wf_id='installation_slap_interface_workflow')
+
+    software_installation.SoftwareInstallation_destroyIfUnused()
+    self.assertEqual('draft', software_installation.getSlapState())
+    self.assertEqual('validated', software_installation.getValidationState())
+
+  def test_destroyIfUnused_notValidated(self):
+    software_installation, _ = self.bootstrapSoftwareInstallation()
+    self.portal.portal_workflow._jumpToStateFor(software_installation,
+                                                'draft',
+                                                wf_id='item_workflow')
+
+    software_installation.SoftwareInstallation_destroyIfUnused()
+    self.assertEqual('start_requested', software_installation.getSlapState())
+    self.assertEqual('draft', software_installation.getValidationState())
+
+  ########################################################
+  # Software Installation used
+  ########################################################
+  def test_destroyIfUnused_oneInstanceUsed(self):
+    software_installation, _ = self.bootstrapSoftwareInstallation(is_allocated=True, is_supplied=False)
+
+    software_installation.SoftwareInstallation_destroyIfUnused()
+    self.assertEqual('start_requested', software_installation.getSlapState())
+    self.assertEqual('validated', software_installation.getValidationState())
+
+  def test_destroyIfUnused_oneSupply(self):
+    software_installation, _ = self.bootstrapSoftwareInstallation(is_allocated=False, is_supplied=True)
+
+    software_installation.SoftwareInstallation_destroyIfUnused()
+    self.assertEqual('start_requested', software_installation.getSlapState())
+    self.assertEqual('validated', software_installation.getValidationState())
+
+  ########################################################
+  # Supply invalidated
+  ########################################################
+  def test_destroyIfUnused_oneInvalidatedSupply(self):
+    software_installation, supply = self.bootstrapSoftwareInstallation(is_allocated=False, is_supplied=True)
+    supply.invalidate()
+    self.tic()
+
+    software_installation.SoftwareInstallation_destroyIfUnused()
+    self.assertEqual('start_requested', software_installation.getSlapState())
+    self.assertEqual('validated', software_installation.getValidationState())

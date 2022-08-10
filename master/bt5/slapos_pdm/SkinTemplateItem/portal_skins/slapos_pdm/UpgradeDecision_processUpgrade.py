650a41d6f627b19bb5d777569dfcc4d4f6abcc77
Romain Courteaud <romain@nexedi.com>
2022-08-10 14:20:23 +0000

    slapos_pdm:
    * add SlapOSVirtualMasterSourceProjectConstraint on Upgrade Decision
    * software product must be linked to a project
    * create Software Type portal type
      It is a variation of a Software Product
    * rename Software Type to Software Product Type Variation
    * add Software Product Release Variation portal type
    * force default use/base_contribution on Software Product
      Didn't find another way to do it (init script requires data migration)
    * require Software Product to have at least one Type and one Release
    * store url_string on Release Variation
    * use edit_workflow on Variation portal types
    * use the jump action to access Software Release
    * add dedicated form to view Release
    * add Software Type view
    * set SoftwareProduct default quantity unit
    * support document without categories property
    * add constraint to ensure quantity unit of a Software Product is defined
    * drop Software Release configuration. Drop hardcoded template only used in tests
    * fixup
      See 1173ea0edf89154719e92acb79c8867c702560c6
    * drop Compute Node upgrade
      This is now handled manually by updating the Allocation Supply
    * stop allowing Upgrade Decision Line creation
    * Upgrade Decision is a ticket
    * wip upgrade decision redesign with allocation supply
    * not commited script
    * really compare url strings
    * update upgrade decision view
    * only create upgrade decision for SOftware Instance for now
    * set the compute node on the test allocation supply
    * drop not used scripts
    * remote tests from dropped scripts
    * mark test to check
    * test alarm_instance_tree_create_upgrade_decision
    * drop upgrade_scope
    * test upgrade decision creation for not allocated instance tree
    * test Upgrade for allocated Software Instance
    * drop deprecated tests
    * root shared instance can not be upgraded by Allocation Supply on the same tree
    * upgrade not allocated or allocated on remote slave instances
    * update not allocated in there is instance node supply
    * test upgrade Instance Node
    * drop unused InstanceTree_getUpgradeSubscriptionRelatedValue
    * drop UpgradeDecisionLine_cancel
    * drop Upgrade Decision Line view
    * drop SoftwareProduct_getSortedSoftwareReleaseList
    * fix upgrade decision list view
    * drop InstanceTree_isUpgradePossible
      No way to know which software release url on instance will request.
    * drop UpgradeDecision_processUpgrade
    * drop Base_acceptUpgradeDecision
    * drop Base_rejectUpgradeDecision
    * drop reviewRegistration
    * check created upgrade decision
    * do not propose again to upgrade if decision has been rejected
    * no need to check all rejected decisions
    * do not put newly create decision in started
      Ask review from manager
    * drop UpgradeDecision_notify
    * drop UpgradeDecision_notifyDelivered
    * drop reviewUpgrade
    * drop UpgradeDecision_getAggregateValue
    * drop approveRegistration
    * drop upgrade_slap_interface_workflow
    * drop UpgradeDecision_requestChangeState
    * drop slapos_pdm_auto_cancel_upgrade_decision
    * use UpgradeDecision_processUpgrade
    * alarm to destroy software installation
    * drop unused template
    * configure Upgrade Decision worklist
    * drop not needed script arguments
    * add Upgrade Decision constraints
    * method moved to global mixin
    * do not upgrade if there is no instance yet
    * create upgrade decision as soon as Allocation Supply is validated
    * workaround to prevent breaking the instance when classes are live reloaded
    * test: disable alarm when creating an allocation supply
    * test: disable alarm
    * software_release is an optional variation
      We can not update the Open Order every time one Instance is upgraded
    * sort SoftwareProduct getVariationBaseCategoryList's result
    * update xml format
    * optional_variation_base_category_list are not set in variation_base_category_list
    * force software change from Upgrade Decision
    * drop template_instance_tree
    * allow to force upgrade to a specific release
    * rely on self instead of one a global
    * drop view_related_upgrade_decision on Compute Node
      Deprecated for a long time

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/UpgradeDecision_processUpgrade.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/UpgradeDecision_processUpgrade.py
@@ -1,1 +1,41 @@
-return context.requestUpgrade()
+from DateTime import DateTime
+
+upgrade_decision = context
+if upgrade_decision.getSimulationState() != 'started':
+  # Update Decision is not on started state, Upgrade is not possible!
+  return
+
+instance_tree = upgrade_decision.getAggregateValue("Instance Tree")
+software_release_url = upgrade_decision.getSoftwareReleaseValue().getUrlString()
+
+status = instance_tree.getSlapState()
+
+if status == "start_requested":
+  state = "started"
+elif status == "stop_requested":
+  state = "stopped"
+elif status == "destroy_requested":
+  state = "destroyed"
+else:
+  raise ValueError('Unhandled state: %s' % status)
+
+if state == 'destroyed':
+  # Nothing to do
+  upgrade_decision.reject(
+    comment="Instance Tree destroyed")
+else:
+  person = instance_tree.getDestinationSectionValue(portal_type="Person")
+  person.requestSoftwareInstance(
+    state=state,
+    software_release=software_release_url,
+    software_title=instance_tree.getTitle(),
+    software_type=instance_tree.getSourceReference(),
+    instance_xml=instance_tree.getTextContent(),
+    sla_xml=instance_tree.getSlaXml(),
+    shared=instance_tree.isRootSlave(),
+    project_reference=instance_tree.getFollowUpReference(),
+    force_software_change=True
+  )
+
+  upgrade_decision.stop(
+    comment="Upgrade Processed for the Instance Tree!")

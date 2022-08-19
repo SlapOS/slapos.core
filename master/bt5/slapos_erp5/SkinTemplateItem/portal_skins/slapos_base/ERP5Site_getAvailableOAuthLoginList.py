7b4b670838ef4b63ce0e0b34d0f88481964a5382
Rafael Monnerat <rafael@nexedi.com>
2022-08-19 19:30:26 +0000

    slapos_erp5: Minor fixup
        Fixup condition, since script always return a list-like

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_base/ERP5Site_getAvailableOAuthLoginList.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_base/ERP5Site_getAvailableOAuthLoginList.py
@@ -4,12 +4,12 @@ portal_skin = context.getPortalObject().portal_skins
 
 if getattr(portal_skin, "erp5_oauth_google_login", None) is not None:
   connector = context.ERP5Site_getGoogleConnector()
-  if connector is not None and connector[0].getClientId() is not None:
+  if connector and connector[0].getClientId() is not None:
     oauth_login_list.append("google")
 
 if getattr(portal_skin, "erp5_oauth_facebook_login", None) is not None:
   connector = context.ERP5Site_getFacebookConnector()
-  if connector is not None and connector[0].getClientId() is not None:
+  if connector and connector[0].getClientId() is not None:
     oauth_login_list.append("facebook")
 
 if getattr(portal_skin, "erp5_openid_connect_client", None) is not None:

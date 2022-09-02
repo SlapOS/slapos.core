e4fce1b7e668e5cdce33fa30ff0bdec04f8711af
Rafael Monnerat <rafael@nexedi.com>
2022-09-02 21:35:36 +0000

    slapos_crm: Move rss exclusive code into slapos_rss_style bt5

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Folder_getOpenTicketList.py
+++
@@ -1,5 +0,0 @@
-kw['simulation_state'] = ['validated','submitted', 'suspended', 'invalidated', 
-   # Unfortunally Upgrade decision uses diferent states.
-   'confirmed', 'delivered']
-kw['sort_on'] = [('modification_date', 'DESC'),]
-return context.searchFolder(**kw)

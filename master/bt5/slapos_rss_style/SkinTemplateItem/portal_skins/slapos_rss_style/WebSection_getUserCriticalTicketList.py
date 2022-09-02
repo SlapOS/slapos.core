5e32f8136b0b0f34d002073762710d69c04a7af7
Rafael Monnerat <rafael@nexedi.com>
2022-09-02 17:59:18 +0000

    slapos_rss_style: up

---
+++ master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/WebSection_getUserCriticalTicketList.py
@@ -0,0 +1,15 @@
+portal = context.getPortalObject()
+kw['portal_type'] = ["Support Request", "Regularisation Request", "Upgrade Decision"]
+
+person = portal.portal_membership.getAuthenticatedMember().getUserValue()
+
+if person:
+  kw['default_destination_decision_uid'] = person.getUid()
+  kw['simulation_state'] = "NOT cancelled"
+  kw['sort_on'] = (('modification_date', 'DESC'),)
+  kw['limit'] = 50
+
+  return portal.portal_catalog(**kw)
+
+else:
+  return []

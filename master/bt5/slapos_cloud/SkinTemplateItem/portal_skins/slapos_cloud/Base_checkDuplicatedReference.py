139bce0218afe2d2ccad77e4b63c80a9c7f1af41
Rafael Monnerat <rafael@nexedi.com>
2022-09-14 19:34:37 +0000

    slapos_cloud: Minor fixup
        The document could be unindexed whenever the consistency is checked.

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Base_checkDuplicatedReference.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Base_checkDuplicatedReference.py
@@ -3,11 +3,12 @@ portal = context.getPortalObject()
 error_list = []
 reference = context.getReference()
 
-result = portal.portal_catalog(portal_type=context.getPortalType(),
-                               reference=reference, limit=2)
+result = portal.portal_catalog(reference=reference,
+  portal_type=context.getPortalType(), limit=2)
 
-if len(result) != 1:
-  error_list.append("%s %s has duplicated reference" % (
+if len(result) > 1 or \
+    (len(result) == 1 and result[0].getUid() != context.getUid()):
+  error_list.append("%s (%s) has duplicated reference." % (
     context.getRelativeUrl(), context.getReference()))
 
 return error_list

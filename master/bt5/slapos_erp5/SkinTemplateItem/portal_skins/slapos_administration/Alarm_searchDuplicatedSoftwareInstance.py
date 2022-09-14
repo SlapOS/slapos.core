54e43fa5976b7eab18b1199b6e767420165aafd8
Rafael Monnerat <rafael@nexedi.com>
2022-09-14 03:40:48 +0000

    slapos_erp5: Alarm was replaced by a constraint

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/Alarm_searchDuplicatedSoftwareInstance.py
+++
@@ -1,8 +0,0 @@
-active_process = context.newActiveProcess().getRelativeUrl()
-
-context.getPortalObject().portal_catalog.searchAndActivate(
-      method_id='InstanceTree_checkDuplicatedInstance',
-      method_kw=dict(fixit=fixit, active_process=active_process),
-      activate_kw=dict(tag=tag, priority=5),
-      portal_type="Instance Tree", 
-      validation_state="validated")

efc2107985d6577944ce9e9674eefae86b0a6476
Rafael Monnerat <rafael@nexedi.com>
2022-09-21 18:59:01 +0000

    slapos_rss_style: Refactor base RSS queries
       Reduce the number of scripts and refactor the query script to rely on getMovementHistory.
    
       Before only the latest event was notified, to reduce the list and only to ping the user to access the site. Now all events are considered on the list (not the latest ones) and include some general clean up on base RSS feeds.

--- master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/WebSection_getUserTicketList.py
+++ master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/WebSection_getUserTicketList.py
@@ -1,14 +1,60 @@
+"""Returns all ticket related events for RSS
+"""
+from Products.PythonScripts.standard import Object
 portal = context.getPortalObject()
-kw['portal_type'] = ["Support Request", "Regularisation Request", "Upgrade Decision"]
 
-person = portal.portal_membership.getAuthenticatedMember().getUserValue()
-if person:
-  kw['default_destination_decision_uid'] = person.getUid()
-  kw['simulation_state'] = "NOT cancelled"
-  kw['sort_on'] = (('modification_date', 'DESC'),)
-  kw['limit'] = 50
+# for safety, we limit at 100 lines
+list_lines = min(list_lines, 100)
 
-  return portal.portal_catalog(**kw)
+getTicket_memo = {}
+def getTicketInfo(event):
+  follow_up = event.getFollowUp()
+  try:
+    return getTicket_memo[follow_up]
+  except KeyError:
+    ticket = portal.restrictedTraverse(follow_up, None)
+    if ticket is None:
+      # For corner cases where user has an event for which he cannot access the ticket,
+      # we don't raise error so that others events are visible.
+      return event.getTitle(), '', ''
+    getTicket_memo[follow_up] = (
+      ticket.getTitle(),
+      ticket.getResourceTranslatedTitle() or '',
+      ticket.Base_getTicketUrl(),
+    )
+    return getTicket_memo[follow_up]
 
-else:
-  return []
+data_list = []
+for brain in portal.portal_simulation.getMovementHistoryList(
+    security_query=portal.portal_catalog.getSecurityQuery(),
+    # Limit only to listable portal types
+    portal_type=['Web Message', 'Mail Nessage'],
+    follow_up_simulation_state="NOT cancelled",
+    only_accountable=False,
+    follow_up_portal_type=['Support Request', 'Regularisation Request', 'Upgrade Decision'],
+    omit_input=True,
+    simulation_state=('started', 'stopped', 'delivered'),
+    limit=list_lines,
+    sort_on=(('stock.date', 'desc'),
+             ('uid', 'desc')),):
+  event = brain.getObject()
+  (ticket_title,
+   ticket_category,
+   ticket_link) = getTicketInfo(event)
+
+  data_list.append(
+      Object(**{
+        'title': ticket_title,
+        'category': ticket_category,
+        'author': brain.node_title,
+        'link': ticket_link,
+        'description': event.getTextContent(),
+        'pubDate': brain.date,
+        'guid': '{}-{}'.format(
+                  event.getFollowUp(),
+                  event.getRelativeUrl()),
+        'thumbnail': ( None)
+        })
+      )
+
+return data_list

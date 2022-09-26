6425d6ffcb6727d4ce8d48bfc402172662aaa119
Rafael Monnerat <rafael@nexedi.com>
2022-09-26 19:56:01 +0000

    slapos_crm: Test ComputeNode_getTicketRelatedList

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_getTicketRelatedList.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_getTicketRelatedList.py
@@ -8,14 +8,15 @@ uid_list = [context.getUid()]
 computer_partition_uid_list = [
   cp.getUid() for cp in context.contentValues(portal_type="Compute Partition") if cp.getSlapState() == 'busy']
 
-for instance in portal.portal_catalog(
+if computer_partition_uid_list:
+  for instance in portal.portal_catalog(
     portal_type="Software Instance",
     select_dict={'specialise_uid': None},
     specialise_portal_type="Instance Tree",
     default_aggregate_uid=computer_partition_uid_list):
 
-  if instance.specialise_uid not in uid_list:
-    uid_list.append(instance.specialise_uid)
+    if instance.specialise_uid not in uid_list:
+      uid_list.append(instance.specialise_uid)
 
 kw['default_or_child_aggregate_uid'] = uid_list
 return context.Base_getOpenRelatedTicketList(**kw)

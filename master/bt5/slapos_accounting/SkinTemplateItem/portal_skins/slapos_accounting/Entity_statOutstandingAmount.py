419a820c3f763d0bc0a9eac3555097475f0d6e37
Rafael Monnerat <rafael@nexedi.com>
2022-10-06 18:09:52 +0000

    slapos_accounting: Dont rely on grouping_date for outstanding amount
      We do want to ignore payments that occur after the at_date.
    
      We want to know if at a certain date, there still unpayed documents older them that date, if the document is payed after, (and grouped) we unconsider it.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Entity_statOutstandingAmount.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/Entity_statOutstandingAmount.py
@@ -1,15 +1,10 @@
-from Products.ZSQLCatalog.SQLCatalog import SimpleQuery, ComplexQuery
 portal = context.getPortalObject()
 params = {"node_category_strict_membership": ['account_type/asset/receivable',
                                                'account_type/liability/payable']}
 if at_date:
   params['at_date'] = at_date
-  params['grouping_query'] = ComplexQuery(
-      SimpleQuery(grouping_reference=None),
-      SimpleQuery(grouping_date=at_date, comparison_operator=">"),
-      logical_operator="OR")
-else:
-  params['grouping_reference'] = None
+
+params['grouping_reference'] = None
 
 if resource_uid is not None:
   params["resource_uid"] = resource_uid

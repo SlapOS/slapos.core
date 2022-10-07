df82d816451457fc47c36c577d315a10de822bd6
Rafael Monnerat <rafael@nexedi.com>
2022-10-07 18:41:41 +0000

    slapos_crm: Check also user assigned tickets for auto-cancel

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_updateSupportRequestMonitoringState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_updateSupportRequestMonitoringState.py
@@ -1,11 +1,9 @@
 portal = context.getPortalObject()
 
-
 default_resource_uid = portal.restrictedTraverse("service_module/slapos_crm_monitoring", None).getUid()
-
 portal.portal_catalog.searchAndActivate(
     portal_type='Support Request',
-    simulation_state='validated',
+    simulation_state=['validated', 'suspended'],
     default_resource_uid=default_resource_uid,
     default_aggregate_portal_type=["Instance Tree"],
     method_id='SupportRequest_updateMonitoringState',

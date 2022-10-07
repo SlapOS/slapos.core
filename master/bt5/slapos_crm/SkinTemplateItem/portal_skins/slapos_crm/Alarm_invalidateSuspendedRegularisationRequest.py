851bfc57998e9e997ffbd857cfca0bbf8ad54561
Rafael Monnerat <rafael@nexedi.com>
2022-10-07 17:36:18 +0000

    slapos_crm: Alarm can cancel validated Regularisation Requests
       Regularisation Requests can be auto cancelled even if they are Operator assigned. This prevents extra unnecessary extra mensagens.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm/Alarm_invalidateSuspendedRegularisationRequest.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm/Alarm_invalidateSuspendedRegularisationRequest.py
@@ -1,7 +1,7 @@
 portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
       portal_type="Regularisation Request", 
-      simulation_state=["suspended"],
+      simulation_state=["suspended", "validated"],
       method_id='RegularisationRequest_invalidateIfPersonBalanceIsOk',
       activate_kw={'tag': tag}
       )

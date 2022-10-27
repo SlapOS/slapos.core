19ea8c31585181f753a0b3ddd112a2fff00334b2
Romain Courteaud <romain@nexedi.com>
2022-10-27 15:20:09 +0000

    slapos_panel_ui_test:
    * test panel scenario
    * validate testFunctionalSlapOSPanelStyle
    * dependency
    *  HARDCODED data/user bootstrap
    *  passwd policy
    * use theia directly in the test
    * WIP shared instance test
    * shared instance test
    * limit Software Instance to manager only
    * add basic payable service test
    * randomize logins
    * check consistency at the end of the test
    * slapos_panel_ui_test: check all project related documents as project manager
    * execute all alarms at the end of the test to detect unexpected side effects
    * ensure we can activate slave allocation on same tree
    * test remote node allocation
    * test Slave Instance allocation on Remote Node
    * ensure request limit the list of release/type based on allocation supply
    * test Upgrade Decision actions
    * test Computer Network creation
    * use Person_addVirtualMaster
    * do not explicitely call alarms
      rely on interaction workflow instead
    * add Software Product to create Subscription Request
    * add Sale Supply for virtual master
    * fixup to generate subscription request and use the instance parameter editor
    * fixup Instance editor usage
    * set reference on the Trade Condition
    * fix consistency of other documents before running the tests
    * request instance action has been modified
    * fixup accounting scenario
    * text: configure trade_condition_type on the Sale Trade Condition
    * test: assignments are automatically created when adding a virtual master
    * fix Trade Condition creation
    * update selector used by the new jsoneditor
    * discussion_thread_reader gadget is now used
    * do not check Web Site consistency
    * no need to define the currency manually
    * search link has been removed. Access global search manually
    * do not check System Preference
    * test: set the region on customer accounts

---
+++ master/bt5/slapos_panel_ui_test/SkinTemplateItem/portal_skins/slapos_panel_ui_test/ERP5Site_generateTestUserConfiguration.py
@@ -0,0 +1,10 @@
+from DateTime import DateTime
+
+now = int(DateTime())
+
+return {
+  'customer_login': 'testcustomer%s' % now,
+  'remote_customer_login': 'testremotecustomer%s' % now,
+  'manager_login': 'testmanager%s' % now,
+  'passwd': 'eiChaxo5Eefier9vAek7phie$%s' % now
+}

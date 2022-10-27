ceed2b8c6ebee0af7b3eaa78519abe18e912823b
Ophelie <ophelie.gagnard@nexedi.com>
2022-10-28 01:26:06 +0200

    slapos_abyss: Add ingestion policy.

---
+++ master/bt5/slapos_abyss/PathTemplateItem/portal_callables/MetadataIngestionPolicy_parseTag.py
@@ -0,0 +1,7 @@
+if not reference:
+  raise ValueError('reference is None')
+
+
+return {'resource_reference' : reference,
+        'specialise_reference': reference,
+        'reference': reference   }

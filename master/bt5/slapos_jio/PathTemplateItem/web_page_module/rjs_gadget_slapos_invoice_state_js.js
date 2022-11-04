55ff4dc9c55e1f7198f1f0dc959279552062d025
Rafael Monnerat <rafael@nexedi.com>
2022-11-04 19:46:13 +0000

    slapos_jio: render payment state with fetch
       There isnt a need to fetch on every line, since the information comes from options

--- master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_slapos_invoice_state_js.js
+++ master/bt5/slapos_jio/PathTemplateItem/web_page_module/rjs_gadget_slapos_invoice_state_js.js
@@ -1,4 +1,4 @@
-/*globals console, window, rJS, RSVP, domsugar */
+/*globals console, window, rJS, domsugar */
 /*jslint indent: 2, nomen: true, maxlen: 80*/
 
 (function (window, rJS, domsugar) {
@@ -21,37 +21,31 @@
               [
                 domsugar("a", {
                   class: "ui-btn ui-first-child ui-btn-icon-center",
-                  // XXX Translation
                   text: translation_dict["Pay Now"],
-                  href: gadget.state.hateoas_url + gadget.state.payment_transaction +
+                  href: gadget.state.hateoas_url +
+                    gadget.state.payment_transaction +
                     "/PaymentTransaction_redirectToManualSlapOSPayment"
                 })
               ]);
           } else {
-            link = domsugar("li", {"text": gadget.state.payment_transaction});
+            link = domsugar("li", {"text": gadget.state.payment_state});
           }
           domsugar(gadget.element, {}, [
             domsugar("ul", {"class": "grid-items"}, [link])
           ]);
           return translation_dict;
-        })
+        });
     })
 
     .declareMethod("render", function (options) {
       var gadget = this;
       return gadget.getSetting("hateoas_url")
         .push(function (hateoas_url) {
-          // XXX RAFAEL this should comes from the options and not from a query like this.
-          return gadget.jio_getAttachment(options.value.jio_key,
-                hateoas_url +  options.value.jio_key +
-                "/AccountingTransaction_getPaymentStateAsHateoas")
-            .push(function (state) {
-              return gadget.changeState({
-                payment_transaction: state.payment_transaction,
-                payment_state: state.state,
-                hateoas_url: hateoas_url
-              });
-            });
+          return gadget.changeState({
+            payment_transaction: options.value.payment_transaction,
+            payment_state: options.value.state,
+            hateoas_url: hateoas_url
+          });
         });
     });
 }(window, rJS, domsugar));

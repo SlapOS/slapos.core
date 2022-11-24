57e41beb402038623660a4abce04e546cb929654
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2022-11-24 16:34:11 +0000

    slapos_mysql_innodb_catalog: Add slap_date column to slapos_item table

--- master/bt5/slapos_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_slapos_item_list.sql
+++ master/bt5/slapos_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_catalog_slapos_item_list.sql
@@ -15,7 +15,8 @@ VALUES
 <dtml-in prefix="loop" expr="_.range(_.len(uid))">
 (
   <dtml-sqlvar expr="uid[loop_item]" type="int">,
-  <dtml-sqlvar expr="getSlapState[loop_item]" type="string" optional>
+  <dtml-sqlvar expr="getSlapState[loop_item]" type="string" optional>,
+  <dtml-sqlvar expr="getSlapTimestamp[loop_item]" type="datetime" optional>
 )
 <dtml-if sequence-end><dtml-else>,</dtml-if>
 </dtml-in>
--- master/bt5/slapos_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_slapos_item.sql
+++ master/bt5/slapos_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_create_slapos_item.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `slapos_item` (
   `uid` BIGINT UNSIGNED NOT NULL,
   `slap_state` varchar(255),
+  `slap_date` datetime,
   PRIMARY KEY (`uid`, `slap_state`)
 ) ENGINE=InnoDB;

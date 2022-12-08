25fd8d3acd13577c44f7541480c686be68862302
Rafael Monnerat <rafael@nexedi.com>
2022-12-08 15:26:00 +0000

    slapos_accounting: Create Payment Transaction before redirect to handle external payment redirection

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/PaymentTransaction_redirectToManualSlapOSPayment.py
+++
@@ -1,8 +0,0 @@
-payment_mode = context.getPaymentMode()
-
-if payment_mode == "wechat":
-  return context.PaymentTransaction_redirectToManualWechatPayment(web_site=web_site)
-elif payment_mode == "payzen":
-  return context.PaymentTransaction_redirectToManualPayzenPayment(web_site=web_site)
-
-raise ValueError("%s isn't an acceptable payment mode" % payment_mode)

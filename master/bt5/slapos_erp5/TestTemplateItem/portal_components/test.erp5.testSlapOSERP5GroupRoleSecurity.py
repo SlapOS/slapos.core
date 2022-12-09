aad5a25462a5d242d856e0c694a55994e66db4c3
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2022-12-09 09:15:39 +0100

    slapos_erp5: add security configuration

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5GroupRoleSecurity.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5GroupRoleSecurity.py
@@ -2591,3 +2591,161 @@ class TestUpgradeDecision(TestSlapOSGroupRoleSecurityMixin):
     self.assertRoles(upgrade_decision, person.getUserId(), ['Assignee'])
     self.assertRoles(upgrade_decision, self.user_id, ['Owner'])
     self.assertRoles(upgrade_decision, organisation.getReference(), ['Assignee'])
+
+class TestDataStreamModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataStreamModule(self):
+    module = self.portal.data_stream_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, 'R-COMPUTER', ['Auditor', 'Author'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataStream(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataStream(self):
+    data_stream = self.portal.data_stream_module.newContent(
+        portal_type='Data Stream')
+
+    self.assertSecurityGroup(data_stream,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(data_stream, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_stream, 'R-COMPUTER', ['Assignor'])
+    self.assertRoles(data_stream, self.user_id, ['Owner'])
+
+
+class TestDataIngestionModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataIngestionModule(self):
+    module = self.portal.data_ingestion_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, 'R-COMPUTER', ['Auditor', 'Author'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataIngestion(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataIngestion(self):
+    data_ingestion = self.portal.data_ingestion_module.newContent(
+        portal_type='Data Ingestion')
+
+    self.assertSecurityGroup(data_ingestion,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(data_ingestion, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_ingestion, 'R-COMPUTER', ['Assignor'])
+    self.assertRoles(data_ingestion, self.user_id, ['Owner'])
+
+class TestDataOperationModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataOperationModule(self):
+    module = self.portal.data_operation_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, 'R-COMPUTER', ['Auditor'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataOperation(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataOperation(self):
+    data_operation = self.portal.data_operation_module.newContent(
+        portal_type='Data Operation')
+
+    self.assertSecurityGroup(data_operation,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(data_operation, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_operation, 'R-COMPUTER', ['Auditor'])
+    self.assertRoles(data_operation, self.user_id, ['Owner'])
+
+
+class TestDataSupplyModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataSupplyModule(self):
+    module = self.portal.data_supply_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, 'R-COMPUTER', ['Auditor'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataSupply(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataSupply(self):
+    data_supply = self.portal.data_supply_module.newContent(
+        portal_type='Data Supply')
+
+    self.assertSecurityGroup(data_supply,
+        ['G-COMPANY', 'R-COMPUTER', self.user_id],
+        False)
+    self.assertRoles(data_supply, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_supply, 'R-COMPUTER', ['Auditor'])
+    self.assertRoles(data_supply, self.user_id, ['Owner'])
+
+
+class TestDataMappingModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataMappingModule(self):
+    module = self.portal.data_mapping_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataMapping(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataMapping(self):
+    data_mapping = self.portal.data_mapping_module.newContent(
+        portal_type='Data Mapping')
+
+    self.assertSecurityGroup(data_mapping,
+        ['G-COMPANY', self.user_id],
+        False)
+    self.assertRoles(data_mapping, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_mapping, self.user_id, ['Owner'])
+
+class TestDataTransformationModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataTransformationModule(self):
+    module = self.portal.data_transformation_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataTransformation(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataTransformation(self):
+    data_transformation = self.portal.data_transformation_module.newContent(
+        portal_type='Data Transformation')
+
+    self.assertSecurityGroup(data_transformation,
+        ['G-COMPANY', self.user_id],
+        False)
+    self.assertRoles(data_transformation, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_transformation, self.user_id, ['Owner'])
+
+class TestDataArrayModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataArrayModule(self):
+    module = self.portal.data_array_module
+    self.changeOwnership(module)
+    self.assertSecurityGroup(module,
+        ['G-COMPANY', self.user_id],
+        False)
+    self.assertRoles(module, 'G-COMPANY', ['Auditor', 'Author'])
+    self.assertRoles(module, self.user_id, ['Owner'])
+
+class TestDataArray(TestSlapOSGroupRoleSecurityMixin):
+  def test_DataArray(self):
+    data_array = self.portal.data_array_module.newContent(
+        portal_type='Data Array')
+
+    self.assertSecurityGroup(data_array,
+        ['G-COMPANY', self.user_id],
+        False)
+    self.assertRoles(data_array, 'G-COMPANY', ['Assignor'])
+    self.assertRoles(data_array, self.user_id, ['Owner'])

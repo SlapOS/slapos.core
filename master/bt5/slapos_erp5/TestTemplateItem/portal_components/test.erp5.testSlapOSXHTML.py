8a7b09183623fc08e6d1f09a6dc6426ff246769a
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2022-12-09 09:50:49 +0100

    slapos_erp5: ignore jslint for erp5_wendelin_notebook

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSXHTML.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSXHTML.py
@@ -39,7 +39,8 @@ class TestSlapOSXHTML(SlapOSTestCaseMixin, testXHTML.TestXHTML):
                           'erp5_corporate_identity',
                           'erp5_corporate_identity_web',
                           'erp5_notebook', 'erp5_officejs_notebook',
-                          'erp5_web_js_style_ui', 'slapos_hal_json_style')
+                          'erp5_web_js_style_ui', 'slapos_hal_json_style',
+                          'erp5_wendelin_notebook')
   def afterSetUp(self):
     SlapOSTestCaseMixin.afterSetUp(self)
     # Live tests all uses the same request. For now we remove cell from

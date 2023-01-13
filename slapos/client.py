df42b6c8bdc395a20f05a3f2b1f59748c4834814
Joanne Hugé <joanne.huge@nexedi.com>
2023-01-13 15:19:08 +0100

    client: add getInformation shorthand method

--- slapos/client.py
+++ slapos/client.py
@@ -101,8 +101,11 @@ def init(conf, logger):
   def shorthandSupply(*args, **kwargs):
     # XXX-Cedric Implement computer_group support
     return slap.registerSupply().supply(*args, **kwargs)
+  def shorthandGetInformation(*args, **kwargs):
+    return slap.registerOpenOrder().getInformation(*args, **kwargs)
   local['request'] = shorthandRequest
   local['supply'] = shorthandSupply
+  local['getInformation'] = shorthandGetInformation
   local['product'] = SoftwareProductCollection(logger, slap)
 
   return local

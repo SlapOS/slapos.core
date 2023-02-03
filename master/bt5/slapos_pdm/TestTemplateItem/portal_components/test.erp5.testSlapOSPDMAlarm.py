e70174f4e1c86654f14b83a3020b144ed65f3685
Rafael Monnerat <rafael@nexedi.com>
2023-02-03 20:12:53 +0100

    slapos_pdm: drop _simulateScript and add more tests

--- master/bt5/slapos_pdm/TestTemplateItem/portal_components/test.erp5.testSlapOSPDMAlarm.py
+++ master/bt5/slapos_pdm/TestTemplateItem/portal_components/test.erp5.testSlapOSPDMAlarm.py
@@ -32,132 +32,67 @@ class TestSlapOSUpgradeDecisionProcess(SlapOSTestCaseMixin):
     upgrade_decision = self._makeUpgradeDecision()
     upgrade_decision.start()
     self.tic()
-
-    self._simulateScript('UpgradeDecision_processUpgrade', 'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_started.activeSense()
-      self.tic()
-    finally:
-      self._dropScript('UpgradeDecision_processUpgrade')
-    self.assertEqual(
-        'Visited by UpgradeDecision_processUpgrade',
-        upgrade_decision.workflow_history['edit_workflow'][-1]['comment'])
+    self._test_alarm(
+      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_started,
+      upgrade_decision,
+      'UpgradeDecision_processUpgrade')
 
   def test_alarm_upgrade_decision_process_planned(self):
     upgrade_decision = self._makeUpgradeDecision(confirm=0)
     upgrade_decision.plan()
-    self.tic()
-
-    self._simulateScript('UpgradeDecision_notify', 'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_planned.\
-        activeSense()
-      self.tic()
-    finally:
-      self._dropScript('UpgradeDecision_notify')
-
-    self.assertEqual('Visited by UpgradeDecision_notify',
-      upgrade_decision.workflow_history['edit_workflow'][-1]['comment'])
-    
+    self._test_alarm(
+      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_planned,
+      upgrade_decision,
+      'UpgradeDecision_notify')
 
   def test_alarm_upgrade_decision_process_stopped(self):
     upgrade_decision = self._makeUpgradeDecision()
     upgrade_decision.start()
     upgrade_decision.stop()
     self.tic()
+    self._test_alarm(
+      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_stopped,
+      upgrade_decision,
+      'UpgradeDecision_notifyDelivered')
 
-    self._simulateScript('UpgradeDecision_notifyDelivered',  'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_upgrade_decision_process_stopped.\
-        activeSense()
-      self.tic()
-    finally:
-      self._dropScript('UpgradeDecision_notifyDelivered')
+  def _test_alarm_compute_node_create_upgrade_decision(self, allocation_scope):
+    compute_node = self._makeComputeNode(allocation_scope=allocation_scope)[0]
+    self._test_alarm(
+      self.portal.portal_alarms.slapos_pdm_compute_node_create_upgrade_decision,
+      compute_node,
+      'ComputeNode_checkAndCreateUpgradeDecision')
 
-    self.assertEqual('Visited by UpgradeDecision_notifyDelivered',
-      upgrade_decision.workflow_history['edit_workflow'][-1]['comment'])
+  def test_alarm_compute_node_create_upgrade_decision_public(self):
+    self._test_alarm_compute_node_create_upgrade_decision('open/public')
 
-  def test_alarm_compute_node_create_upgrade_decision(self):
-    compute_node = self._makeComputeNode(allocation_scope = 'open/public')[0]
-    compute_node2 = self._makeComputeNode(allocation_scope = 'open/personal')[0]
-    compute_node3 = self._makeComputeNode(allocation_scope = 'open/friend')[0]
-    self.tic()
+  def test_alarm_compute_node_create_upgrade_decision_personal(self):
+    self._test_alarm_compute_node_create_upgrade_decision('open/personal')
 
-    self._simulateScript('ComputeNode_checkAndCreateUpgradeDecision', 'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_compute_node_create_upgrade_decision.\
-        activeSense()
-      self.tic()
-    finally:
-      self._dropScript('ComputeNode_checkAndCreateUpgradeDecision')
+  def test_alarm_compute_node_create_upgrade_decision_friend(self):
+    self._test_alarm_compute_node_create_upgrade_decision('open/friend')
 
-    self.assertEqual('Visited by ComputeNode_checkAndCreateUpgradeDecision',
-      compute_node.workflow_history['edit_workflow'][-1]['comment'])
-    
-    self.assertEqual('Visited by ComputeNode_checkAndCreateUpgradeDecision',
-      compute_node2.workflow_history['edit_workflow'][-1]['comment'])
+  def test_alarm_compute_node_create_upgrade_decision_subscription(self):
+    self._test_alarm_compute_node_create_upgrade_decision('open/subscription')
 
-    self.assertEqual('Visited by ComputeNode_checkAndCreateUpgradeDecision',
-      compute_node3.workflow_history['edit_workflow'][-1]['comment'])
-  
+  def test_alarm_compute_node_create_upgrade_decision_subscription(self):
+    self._test_alarm_compute_node_create_upgrade_decision('close/outdated')
+
+  def test_alarm_compute_node_create_upgrade_decision_subscription(self):
+    self._test_alarm_compute_node_create_upgrade_decision('close/maintanance')
+
+  def test_alarm_compute_node_create_upgrade_decision_subscription(self):
+    self._test_alarm_compute_node_create_upgrade_decision('close/termination')
+
   def test_alarm_instance_tree_create_upgrade_decision(self):
     instance_tree = self._makeInstanceTree()
-    instance_tree2 = self._makeInstanceTree()
-    instance_tree3 = self._makeInstanceTree()
-
-    self.tic()
-
-    self._simulateScript('InstanceTree_createUpgradeDecision', 'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision.\
-        activeSense()
-      self.tic()
-    finally:
-      self._dropScript('InstanceTree_createUpgradeDecision')
-
-    self.assertEqual('Visited by InstanceTree_createUpgradeDecision',
-      instance_tree.workflow_history['edit_workflow'][-1]['comment'])
-    
-    self.assertEqual('Visited by InstanceTree_createUpgradeDecision',
-      instance_tree2.workflow_history['edit_workflow'][-1]['comment'])
-
-    self.assertEqual('Visited by InstanceTree_createUpgradeDecision',
-      instance_tree3.workflow_history['edit_workflow'][-1]['comment'])
+    self._test_alarm(
+      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision,
+      instance_tree,
+      'InstanceTree_createUpgradeDecision')
 
   def test_alarm_create_upgrade_decision_destroyed_instance_tree(self):
     instance_tree = self._makeInstanceTree(slap_state="destroy_requested")
-    instance_tree2 = self._makeInstanceTree(slap_state="destroy_requested")
-    self.tic()
-
-    self._simulateScript('InstanceTree_createUpgradeDecision', 'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision.\
-        activeSense()
-      self.tic()
-    finally:
-      self._dropScript('InstanceTree_createUpgradeDecision')
-
-    self.assertNotEqual('Visited by InstanceTree_createUpgradeDecision',
-      instance_tree.workflow_history['edit_workflow'][-1]['comment'])
-    
-    self.assertNotEqual('Visited by InstanceTree_createUpgradeDecision',
-      instance_tree2.workflow_history['edit_workflow'][-1]['comment'])
-
-  def test_alarm_create_upgrade_decision_closed_compute_node(self):
-    compute_node = self._makeComputeNode(allocation_scope='close/oudtated')[0]
-    compute_node2 = self._makeComputeNode(allocation_scope='close/maintenance')[0]
-    self.tic()
-
-    self._simulateScript('ComputeNode_checkAndCreateUpgradeDecision', 'True')
-    try:
-      self.portal.portal_alarms.slapos_pdm_compute_node_create_upgrade_decision.\
-        activeSense()
-      self.tic()
-    finally:
-      self._dropScript('ComputeNode_checkAndCreateUpgradeDecision')
-
-    self.assertNotEqual('Visited by ComputeNode_checkAndCreateUpgradeDecision',
-      compute_node.workflow_history['edit_workflow'][-1]['comment'])
-    
-    self.assertNotEqual('Visited by ComputeNode_checkAndCreateUpgradeDecision',
-      compute_node2.workflow_history['edit_workflow'][-1]['comment'])
+    self._test_alarm_not_visited(
+      self.portal.portal_alarms.slapos_pdm_instance_tree_create_upgrade_decision,
+      instance_tree,
+      'InstanceTree_createUpgradeDecision')

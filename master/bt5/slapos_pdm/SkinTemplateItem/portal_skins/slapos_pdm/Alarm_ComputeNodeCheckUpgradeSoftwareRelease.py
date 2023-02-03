b47287d5589f835763d99f2b0f4692a1e2bbec2e
Rafael Monnerat <rafael@nexedi.com>
2023-02-03 20:37:56 +0100

    slapos_pdm: Use upgrade scope rather them allocation scope
      ... to decide which computers to select for check for upgrades

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_ComputeNodeCheckUpgradeSoftwareRelease.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_ComputeNodeCheckUpgradeSoftwareRelease.py
@@ -1,14 +1,16 @@
 portal = context.getPortalObject()
 
-default_allocation_scope_uid = [category.getUid() \
-         for category in portal.portal_categories.allocation_scope.open.objectValues()]
+default_upgrade_scope_uid = [
+         portal_categories.upgrade_scope.auto.getUid(),
+         portal_categories.upgrade_scope.confirmation.getUid()
+    ]
 
 
 if default_allocation_scope_uid:
   portal.portal_catalog.searchAndActivate(
     portal_type='Compute Node',
     validation_state = 'validated',
-    default_allocation_scope_uid=default_allocation_scope_uid,
+    default_upgrade_scope_uid=default_upgrade_scope_uid,
     method_id = 'ComputeNode_checkAndCreateUpgradeDecision',
     activate_kw = {'tag':tag}
   )

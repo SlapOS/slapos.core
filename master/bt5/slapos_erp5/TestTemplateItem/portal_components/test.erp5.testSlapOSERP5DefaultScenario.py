6b6efd1efb8177621d0c1804c7ecbdd55490d38b
Jérome Perrin <jerome@nexedi.com>
2023-02-14 00:34:27 +0900

    *: update business template XML ( DateTime 3 )
    This change is from zope4

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5DefaultScenario.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5DefaultScenario.py
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 ##############################################################################
-
 from erp5.component.test.SlapOSTestCaseDefaultScenarioMixin import DefaultScenarioMixin
 from DateTime import DateTime
 import re

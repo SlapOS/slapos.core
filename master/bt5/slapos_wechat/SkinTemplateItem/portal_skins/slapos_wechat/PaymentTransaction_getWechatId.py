4c6973756d3558387545b83b86d24533f0385496
Romain Courteaud <romain@nexedi.com>
2023-03-01 09:08:01 +0000

    slapos_wechat:
    * drop not needed wechat_sale_trade_condition
    * new id generator per node is used
    * payment transaction ID is used as mapping

--- master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/PaymentTransaction_getWechatId.py
+++ master/bt5/slapos_wechat/SkinTemplateItem/portal_skins/slapos_wechat/PaymentTransaction_getWechatId.py
@@ -7,7 +7,10 @@ integration_site = portal.restrictedTraverse(portal.portal_preferences.getPrefer
 
 wechat_id = integration_site.getCategoryFromMapping('Causality/%s' % context.getId().replace('-', '_'))
 if wechat_id != 'causality/%s' % context.getId().replace('-', '_'):
-  date, _ = wechat_id.split('-', 1)
+  # ok when using per day generator
+  date = wechat_id.split('-', 1)[0]
+  # and then switched to per day / per node generator
+  date = date.split('.', 1)[0]
   return DateTime(date).toZone('UTC'), wechat_id
 else:
   return None, None

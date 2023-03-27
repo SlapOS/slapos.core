0f8a8f097632dc56aa5a880a10dd578ea42d6b9f
Rafael Monnerat <rafael@nexedi.com>
2023-03-27 20:31:57 +0000

    slapos_jio: Add sample for fast input

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/InstanceTree_getFastInputDict.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/InstanceTree_getFastInputDict.py
@@ -1,1 +1,9 @@
+"""
+  Sample of the return:
+    return {
+      'enabled': True,
+      'sla_xml': '<parameter id="instance_guid">%s</parameter>' % context.getSuccessorReference()
+    }
+"""
+
 return {}

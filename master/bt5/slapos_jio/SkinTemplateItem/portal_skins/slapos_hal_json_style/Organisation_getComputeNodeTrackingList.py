0df7e6f4689ce0327445ea45e5e998472d8db463
Rafael Monnerat <rafael@nexedi.com>
2023-05-12 17:56:54 +0000

    slapos_jio: Skip to call catalog if uid_list is empty
        if uid is empty it returns whole catalog

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Organisation_getComputeNodeTrackingList.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Organisation_getComputeNodeTrackingList.py
@@ -13,7 +13,11 @@ def filter_per_portal_type(document):
       and document.getValidationState() == "validated":
     return document
 
-return portal.portal_catalog(uid=[i.uid
-           for i in context.portal_simulation.getCurrentTrackingList(
-             node_uid=context.getUid(), at_date=DateTime())
-             if filter_per_portal_type(i.getObject())], **kw)
+uid_list = [i.uid for i in context.portal_simulation.getCurrentTrackingList(
+    node_uid=context.getUid(), at_date=DateTime())
+    if filter_per_portal_type(i.getObject())]
+
+if not uid_list:
+  return []
+
+return portal.portal_catalog(uid=uid_list, **kw)

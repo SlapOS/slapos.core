93977a368a7158c186d0108fa5170c95476600a1
Rafael Monnerat <rafael@nexedi.com>
2023-05-30 19:02:57 +0000

    slapos_cloud: Defer creation of the Movement
      Hold the request makes the user hangs there waiting for response.

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerNetwork_createMovement.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/ComputerNetwork_createMovement.py
@@ -1,7 +1,8 @@
 """
-  Create an internal Packing List and attach the compute_node
+  Create an internal Packing List and attach the computer network
 """
-context.requestTransfer(
+tag = "transfer_compute_network_%s" % context.getUid()
+context.activate(activity="SQLQueue", tag=tag).requestTransfer(
   destination_project=destination_project,
   destination_section=destination_section
 )

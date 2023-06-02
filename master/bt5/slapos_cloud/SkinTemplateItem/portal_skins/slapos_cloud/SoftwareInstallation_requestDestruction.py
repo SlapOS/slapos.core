a300e603b1d32c580e3061d320a81c2632b69d12
Rafael Monnerat <rafael@nexedi.com>
2023-06-02 20:03:23 +0000

    slapos_cloud: Dont destroy Software Installation if it is used

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/SoftwareInstallation_requestDestruction.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/SoftwareInstallation_requestDestruction.py
@@ -1,2 +1,14 @@
-context.getAggregateValue(portal_type='Compute Node').requestSoftwareRelease(software_release_url=context.getUrlString(), state='destroyed')
-return context.Base_redirect('view', keep_items={'portal_status_message': 'Requested Destruction'})
+compute_node = context.getAggregateValue(
+    portal_type='Compute Node'
+  )
+url_string = context.getUrlString()
+
+if compute_node.ComputeNode_getSoftwareReleaseUsage(url_string) == 0:
+  message = 'Requested Destruction'
+  compute_node.requestSoftwareRelease(
+    software_release_url=url_string,
+    state='destroyed')
+else:
+  message = 'This software installation is used and cannot be destroyed'
+
+return context.Base_redirect('view', keep_items={'portal_status_message': message})

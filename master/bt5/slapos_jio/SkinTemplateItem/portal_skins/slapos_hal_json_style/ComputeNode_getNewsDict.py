ca265db8a347d7f4c1d7a8d31573232ccc18f577
Rafael Monnerat <rafael@nexedi.com>
2023-06-15 02:13:45 +0000

    slapos_cloud&jio: Add API for calculate status (news) for compute partition
      Similar to getComputeNodeInformation, use caching system to not
      recalculate status for the related software instances, only acquire
      from cache if it is updated.

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/ComputeNode_getNewsDict.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/ComputeNode_getNewsDict.py
@@ -2,13 +2,9 @@ from zExceptions import Unauthorized
 if REQUEST is not None:
   raise Unauthorized
 
-def get_compute_partition_dict(reference):
+def get_compute_partition_dict():
   compute_node_dict =  context.getAccessStatus()
-  compute_partition_dict = { }
-  for compute_partition in context.objectValues(portal_type="Compute Partition"):
-    software_instance = compute_partition.getAggregateRelatedValue(portal_type="Software Instance")
-    if software_instance is not None:
-      compute_partition_dict[compute_partition.getTitle()] = software_instance.getAccessStatus()
+  compute_partition_dict = context.getComputePartitionNewsDict()
 
   return {"compute_node": compute_node_dict,
           "partition": compute_partition_dict,
@@ -16,5 +12,4 @@ def get_compute_partition_dict(reference):
           "reference": compute_node_dict['reference'],
           "monitor_url": context.Base_getStatusMonitorUrl()}
 
-# Use Cache here, at least transactional one.
-return get_compute_partition_dict(context.getReference())
+return get_compute_partition_dict()

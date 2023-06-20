d3dfa4a35936a3f0c1ae5f47e57a70f89b1a5cf7
Rafael Monnerat <rafael@nexedi.com>
2023-06-20 20:51:08 +0000

    slapos_jio: Speed up getMonitorUrl for status
      Dont fetch the list twice for Network and Organisation
      We dont use Project anymore
      Direct call API for getAccessStatus instead call ComputeNode_getNewsDict

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getNewsDictFromComputeNodeList.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getNewsDictFromComputeNodeList.py
@@ -2,15 +2,15 @@ from zExceptions import Unauthorized
 if REQUEST is not None:
   raise Unauthorized
 
-compute_node_dict = {}
-compute_partition_dict = {}
+node_dict = {}
+partition_dict = {}
 for compute_node in compute_node_list:
-  news_dict = compute_node.ComputeNode_getNewsDict()
-  compute_node_dict[compute_node.getReference()] = news_dict["compute_node"]
-  compute_partition_dict[compute_node.getReference()] = news_dict["partition"]
+  reference = compute_node.getReference()
+  node_dict[reference] = compute_node.getAccessStatus()
+  partition_dict[reference] = compute_node.getComputePartitionNewsDict()
 
-return {"compute_node": compute_node_dict,
-        "partition": compute_partition_dict,
+return {"compute_node": node_dict,
+        "partition": partition_dict,
         "reference": context.getReference(),
         "portal_type": context.getPortalType(),
-        "monitor_url": context.Base_getStatusMonitorUrl()}
+        "monitor_url": context.Base_getStatusMonitorUrl(compute_node_list=compute_node_list)}

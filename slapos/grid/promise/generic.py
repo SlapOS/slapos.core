8b8c767ca123605be76807cf5125fd566827aaad
Xavier Thompson <xavier.thompson@nexedi.com>
2023-06-30 12:00:18 +0200

    promise: Fix slapgrid version parse for comparison
    Promise launcher compares current slapgrid version with an old version
    number to determine if some feature is supported or if it should use a
    legacy workaround. But the comparison was a simple lexicographic order
    string comparison, and now that we incremented from 1.9.3 to 1.10.0 it
    wrongly compared the versions. Fix it with pkg_resources.parse_version.

--- slapos/grid/promise/generic.py
+++ slapos/grid/promise/generic.py
@@ -41,6 +41,8 @@ import six
 from six import PY3, with_metaclass
 from datetime import datetime, timedelta
 
+from pkg_resources import parse_version
+
 PROMISE_STATE_FOLDER_NAME = '.slapgrid/promise'
 PROMISE_RESULT_FOLDER_NAME = '.slapgrid/promise/result'
 PROMISE_LOG_FOLDER_NAME = '.slapgrid/promise/log'
@@ -493,8 +495,9 @@ class GenericPromise(with_metaclass(ABCMeta, object)):
       ))
     elif (not self.__is_tested and not check_anomaly) or \
         (not self.__is_anomaly_detected and check_anomaly):
-      # Anomaly or Test is disabled on this promise, send empty result
-      if self.getConfig('slapgrid-version', '') <= '1.4.17':
+      # Anomaly or Test is disabled on this promise, send empty 
+      slapgrid_version = self.getConfig('slapgrid-version', '')
+      if parse_version(slapgrid_version) <= parse_version('1.4.17'):
         # old version cannot send EmptyResult
         self.__sendResult(PromiseQueueResult(item=TestResult()))
       else:

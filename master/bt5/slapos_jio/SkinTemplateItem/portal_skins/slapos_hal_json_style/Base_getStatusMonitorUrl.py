230d743271022eb3bb927f79caf144268b384f08
Roque Porchetto <roque.porchetto@nexedi.com>
2023-11-23 17:50:22 +0000

    slapos_jio: script to get monitor app info (yet hardcoded) - new url parameters for dispatch link

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
@@ -1,11 +1,14 @@
-base_url = 'https://monitor.app.officejs.com/#/?page=ojsm_dispatch&query=portal_type:"Software Instance" AND '
+monitor_dict = context.Base_getMonitorDict()
+base_url = monitor_dict["url"] + monitor_dict["dispatch_parameters"]
 
 if context.getPortalType() == "Instance Tree":
   for connection_parameter in context.InstanceTree_getConnectionParameterList(raw=True):
     if connection_parameter['connection_key'] == "monitor-setup-url":
       return connection_parameter['connection_value']
-  base_url = 'https://monitor.app.officejs.com/#/?page=ojsm_dispatch&query=portal_type:"Instance Tree" AND '
-  return base_url + "title:(%s)" % context.getTitle()
+  return base_url + '&query=portal_type:"Instance Tree" AND title:(%s)' % context.getTitle()
 
 if context.getPortalType() in ["Software Instance", "Slave Instance"]:
-  return base_url + "reference:%s" % context.getReference()
+  return base_url + '&query=portal_type:"Software Instance" AND title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle()
+
+#OLD return query_url + "reference:%s" % context.getReference()
+#TODO build a url detach from internal monitoring app stuff, like [app-url]/?page=import{url,username,password,query}

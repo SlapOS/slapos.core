6541bac2c54427ee9b294a4bab59e9dc116d8a10
Roque Porchetto <roque.porchetto@nexedi.com>
2023-11-23 17:50:22 +0000

    slapos_jio: script to get monitor app info (yet hardcoded)

---
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getMonitorDict.py
@@ -0,0 +1,6 @@
+return {
+  'url': 'https://monitor.app.officejs.com/',
+  'dispatch_page' : 'ojsm_dispatch',
+  'configurator_page' : 'settings_configurator'
+}
+# TODO how to get from a global param or context so hardcode is avoided?

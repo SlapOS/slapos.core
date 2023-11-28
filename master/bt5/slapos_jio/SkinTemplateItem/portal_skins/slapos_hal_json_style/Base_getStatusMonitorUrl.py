2dbfe8f118525fd30e9ca8ae64b9115b6bac3081
Roque Porchetto <roque.porchetto@nexedi.com>
2023-11-28 16:28:08 +0000

    slapos_jio: drop hardcoded monitoring

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
@@ -5,7 +5,7 @@ if context.getPortalType() == "Instance Tree":
   for connection_parameter in context.InstanceTree_getConnectionParameterList(raw=True):
     if connection_parameter['connection_key'] == "monitor-setup-url":
       return connection_parameter['connection_value']
-  base_url = 'https://monitor.app.officejs.com/#/?page=ojsm_dispatch&query=portal_type:"Instance Tree" AND '
+  base_url = monitor_url + '#/?page=ojsm_dispatch&query=portal_type:"Instance Tree" AND '
   return base_url + "title:(%s)" % context.getTitle()
 
 if context.getPortalType() in ["Software Instance", "Slave Instance"]:

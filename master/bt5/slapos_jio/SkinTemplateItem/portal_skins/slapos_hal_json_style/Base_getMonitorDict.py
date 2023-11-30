b7a6d5d3ff9b4aa90bff189d66ec7b05e66c9042
Roque Porchetto <roque.porchetto@nexedi.com>
2023-11-30 14:12:19 +0000

    slapos_jio: refactor

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getMonitorDict.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getMonitorDict.py
@@ -1,6 +1,6 @@
 return {
   'url': 'https://monitor.app.officejs.com/',
-  'dispatch_page' : 'ojsm_dispatch',
-  'configurator_page' : 'settings_configurator'
+  'dispatch_parameters' : '#/?page=ojsm_dispatch'
 }
 # TODO how to get from a global param or context so hardcode is avoided?
+# from context.InstanceTree_getConnectionParameterList(raw=True): ?
--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
@@ -1,14 +1,11 @@
 monitor_dict = context.Base_getMonitorDict()
-base_url = monitor_dict["url"] + '#/?page=' + monitor_dict["dispatch_page"]
-
-query_url = base_url + '&query=portal_type:"Software Instance" AND '
+base_url = monitor_dict["url"] + monitor_dict["dispatch_parameters"]
 
 if context.getPortalType() == "Instance Tree":
   for connection_parameter in context.InstanceTree_getConnectionParameterList(raw=True):
     if connection_parameter['connection_key'] == "monitor-setup-url":
       return connection_parameter['connection_value']
-  query_url = base_url + '&query=portal_type:"Instance Tree" AND '
-  return query_url + "title:(%s)" % context.getTitle()
+  return base_url + '&query=portal_type:"Instance Tree" AND title:(%s)' % context.getTitle()
 
 if context.getPortalType() in ["Software Instance", "Slave Instance"]:
-  return query_url + 'title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle()
+  return base_url + '&query=portal_type:"Software Instance" AND title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle()

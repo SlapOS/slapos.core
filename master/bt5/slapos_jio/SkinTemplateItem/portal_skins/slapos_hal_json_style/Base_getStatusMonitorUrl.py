119b88337c88cb7eee640f2887fff362f699cfa5
Roque Porchetto <roque.porchetto@nexedi.com>
2023-12-04 19:49:46 +0000

    slapos_jio: add url-user-pass parameters to monitor url

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
+++ master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Base_getStatusMonitorUrl.py
@@ -5,9 +5,14 @@ if context.getPortalType() == "Instance Tree":
   for connection_parameter in context.InstanceTree_getConnectionParameterList(raw=True):
     if connection_parameter['connection_key'] == "monitor-setup-url":
       return connection_parameter['connection_value']
-  return base_url + '&query=portal_type:"Instance Tree" AND title:(%s)' % context.getTitle()
+  connection_parameter_dict = context.InstanceTree_getMonitorParameterDict()
+  connection_url = '&url=%s'% connection_parameter_dict['url'] + '&username=%s'% connection_parameter_dict['username'] + '&password=%s'% connection_parameter_dict['password']
+  return base_url + '&query=portal_type:"Instance Tree" AND title:(%s)' % context.getTitle() + connection_url
 
 if context.getPortalType() in ["Software Instance", "Slave Instance"]:
-  return base_url + '&query=portal_type:"Software Instance" AND title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle()
+  it = context.getSpecialise()
+  connection_parameter_dict = it.InstanceTree_getMonitorParameterDict()
+  connection_url = '&url=%s'% connection_parameter_dict['url'] + '&username=%s'% connection_parameter_dict['username'] + '&password=%s'% connection_parameter_dict['password']
+  return base_url + '&query=portal_type:"Software Instance" AND title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle() + connection_url
 
 #OLD return query_url + "reference:%s" % context.getReference()

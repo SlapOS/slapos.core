8d9642c755c8242557866bb9eec134512c41d55d
Romain Courteaud <romain@nexedi.com>
2023-12-20 15:12:12 +0000

    slapos_mysql_innodb_catalog:
    * drop default_or_child_aggregate_reference
    * drop  default_or_child_aggregate_uid catalog key
    * drop unused follow_up_default_or_child_aggregate_uid
    * drop causality_subscription_request_related_uid related key

--- master/bt5/slapos_mysql_innodb_catalog/CatalogMethodTemplateItem/portal_catalog/erp5_mysql_innodb/z_related_causality_subscription_request.sql
+++
@@ -1,8 +0,0 @@
-<dtml-var table_0>.base_category_uid = <dtml-var "portal_categories.causality.getUid()">
-AND <dtml-var table_1>.uid = <dtml-var table_0>.uid
-AND <dtml-var table_1>.portal_type = 'Subscription Request'
-AND <dtml-var table_1>.simulation_state != 'deleted'
-
-<dtml-var RELATED_QUERY_SEPARATOR>
-<dtml-var table_0>.category_uid = <dtml-var query_table>.uid
-

843b12c7d9bbb1b23be2c6072c884bff40cad0a1
Romain Courteaud <romain@nexedi.com>
2024-02-05 10:15:37 +0000

    slapos_panel: fixup rebase

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/SoftwareInstance_getAllocationInformation.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/SoftwareInstance_getAllocationInformation.py
@@ -3,9 +3,12 @@ if context.getAggregate() is None:
 
 partition = context.getAggregateValue(checked_permission="View")
 if partition is not None:
-  return "%s (%s)" % (
+  allocation_information = "%s (%s)" % (
     partition.getParentValue().getReference(), 
     partition.getReference())
-
+  network_title = partition.getParentValue().getSubordinationTitle(checked_permission="View")
+  if network_title is not None:
+    allocation_information += " - %s" % network_title
+  return allocation_information
 
 return context.Base_translateString("Restricted information")

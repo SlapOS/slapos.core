39b70b08b66fc2ab4be804247637ff1a2521630b
Romain Courteaud <romain@nexedi.com>
2024-02-06 11:47:10 +0000

    slapos_panel: fixup upgrade decision action

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/InstanceTree_proposeUpgradeDecision.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/InstanceTree_proposeUpgradeDecision.py
@@ -1,9 +1,16 @@
 portal = context.getPortalObject()
 Base_translateString = portal.Base_translateString
+instance_tree = context
+
+software_product, _, software_type = instance_tree.InstanceTree_getSoftwareProduct()
 
 upgrade_decision = context.InstanceTree_createUpgradeDecision(
-  target_software_release=target_software_release,
-  target_software_type=context.getSourceReference()
+  target_software_release=portal.portal_catalog.getResultValue(
+      portal_type="Software Product Release Variation",
+      url_string=target_software_release,
+      parent_uid=software_product.getUid()
+    ),
+  target_software_type=software_type
 )
 
 if upgrade_decision is None:

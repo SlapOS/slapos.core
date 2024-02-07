c31cc31fb2b54201045c40cd7f68c3ee3da05fa5
Rafael Monnerat <rafael@nexedi.com>
2024-02-07 18:39:19 +0000

    slapos_erp5: Updating CodingStyle tests
      Some scripts were removed and on other cases started to be tested.

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSCodingStyle.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSCodingStyle.py
@@ -303,16 +303,7 @@ def makeTestSlapOSCodingStyleTestCase(tested_business_template):
         'slapos_base/Login_notifyPasswordExpire',
         'slapos_base/Person_applyContractInvitation',
         'slapos_base/ERP5Site_getAvailableOAuthLoginList',
-        'slapos_erp5/CatalogTool_checkNoneCreationDateConsistency',
-        'slapos_erp5/CertificateAuthorityTool_checkCertificateAuthorityConsistency',
-        'slapos_erp5/CertificateAuthorityTool_checkSlapOSMachineNonExistenceConsistency',
-        'slapos_erp5/CertificateAuthorityTool_checkSlapOSPASConsistency',
-        'slapos_erp5/Module_checkSlapOSModuleIdGeneratorConsistency',
-        'slapos_erp5/OAuthTool_checkFacebookConnectorExistenceConsistency',
-        'slapos_erp5/OAuthTool_checkGoogleConnectorExistenceConsistency',
-        'slapos_erp5/WebSiteModule_checkShacacheConstraint',
-        'slapos_erp5/SystemPreference_checkConversionServerConsistency',
-        'slapos_erp5/SystemPreference_checkSystemPreferenceConsistency']
+        'slapos_erp5/CatalogTool_checkNoneCreationDateConsistency']
       
       SlapOSTestCaseMixin.afterSetUp(self)
 

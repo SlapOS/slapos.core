2ba21dfe083dbdb386294c4a4d8f5c90e07544d9
Romain Courteaud <romain@nexedi.com>
2024-02-13 08:51:16 +0000

    slapos_crm: only check automated Outstanding Invoices

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm/RegularisationRequest_invalidateIfPersonBalanceIsOk.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm/RegularisationRequest_invalidateIfPersonBalanceIsOk.py
@@ -8,5 +8,5 @@ if (state not in ('suspended', 'validated')) or \
    (person is None):
   return
 
-if not person.Entity_hasOutstandingAmount():
+if not person.Entity_hasOutstandingAmount(ledger_uid=context.getPortalObject().portal_categories.ledger.automated.getUid()):
   context.invalidate(comment="Automatically disabled as balance is ok")

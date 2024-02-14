dee31e90d490597fc2521eb3adbc284fef0af159
Romain Courteaud <romain@nexedi.com>
2024-02-14 10:31:42 +0000

    slapos_accounting: show Cancelled status on reversed Invoices

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/AccountingTransaction_getPaymentState.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_accounting/AccountingTransaction_getPaymentState.py
@@ -14,6 +14,15 @@ else:
 
   if paid:
     result = "Paid"
+
+    reversal = portal.portal_catalog.getResultValue(
+        portal_type="Sale Invoice Transaction",
+        simulation_state="stopped",
+        default_causality_uid=context.getUid()
+      )
+    if reversal is not None and (context.getTotalPrice() + reversal.getTotalPrice()) == 0:
+      result = "Cancelled"
+
   elif context.getTotalPrice() == 0:
     result = "Free!"
   else:

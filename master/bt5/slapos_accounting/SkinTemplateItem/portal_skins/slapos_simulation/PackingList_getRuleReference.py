dcd9ae5f70a5ea0e0cfd7644bc9de3040ccd372f
Romain Courteaud <romain@nexedi.com>
2024-03-21 14:42:28 +0000

    slapos_accounting: try to limit errors in the simulation with checkConsistency

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/PackingList_getRuleReference.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/PackingList_getRuleReference.py
@@ -5,4 +5,8 @@ if source_section == destination_section or source_section is None \
     or destination_section is None:
   return None
 
+# Do not expand if packing list is not consistent
+if context.checkConsistency():
+  return None
+
 return 'default_delivery_rule'

7134546ee51498bda0a0e1fdb1cdc5289f9c742e
Romain Courteaud <romain@nexedi.com>
2024-03-26 18:04:58 +0100

    slapos_pdm: support for the virtual master logic
    A Software Product is a variated resource with 2 axes:
    - Software Product Release Variation (optional variation),
      to not force updating Order/Delivery every time one Instance is upgraded
    - Software Product Type Variation
    It is linked to a Project (virtual master).
    
    Redesign the Upgrade Decision logic.
    It is now a Ticket, generated by the Allocation Supply configuration:
    - upgrade is different for every Project
    - upgrade can be triggered on a specific Node, or for a specific user
    
    Upgrade Decision is created automatically only if there is a single possible release allocable
    for a Software Product.
    Otherwise, it must be created manually.
    
    The Upgrade Decision must be approved by the Project manager,
    who is responsible for the Allocation Supply configuration.
    
    Also in this commit:
    * drop template usage
    * trigger alarm with interaction workflow

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_cancelUpgradeDecision.py
+++
@@ -1,9 +0,0 @@
-portal = context.getPortalObject()
-portal.portal_catalog.searchAndActivate(
-  portal_type="Upgrade Decision Line",
-  simulation_state=["confirmed", "draft", "planned"],
-  method_id = 'UpgradeDecisionLine_cancel',
-  activate_kw = {'tag':tag}
-  )
-
-context.activate(after_tag=tag).getId()

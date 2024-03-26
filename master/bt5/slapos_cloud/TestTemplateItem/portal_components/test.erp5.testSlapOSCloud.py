50c3b3439717b3c44a174016e89edb9a4c108404
Romain Courteaud <romain@nexedi.com>
2024-03-26 14:12:15 +0000

    slapos_cloud: test: unallocate is done by an alarm

--- master/bt5/slapos_cloud/TestTemplateItem/portal_components/test.erp5.testSlapOSCloud.py
+++ master/bt5/slapos_cloud/TestTemplateItem/portal_components/test.erp5.testSlapOSCloud.py
@@ -651,7 +651,12 @@ class TestSlapOSCloudSlapOSComputeNodeMixin_getCacheComputeNodeInformation(
 
     # Remove the slave link to the partition
     # Compute Node should loose permission to access the slave instance
+    self.logout()
+    self.login()
+    # unallocate is done by an alarm
     self.start_requested_slave_instance.setAggregate('')
+    self.logout()
+    self.login(self.compute_node_user_id)
     self.commit()
 
     # 7th access

a379f510d86ab70453dff1e528c3d91db85d9d27
Romain Courteaud <romain@nexedi.com>
2024-04-16 06:44:31 +0000

    slapos_accounting: reduce number of visited sale packing lists

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/Alarm_updateSlapOSDeliverySimulation.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/Alarm_updateSlapOSDeliverySimulation.py
@@ -1,21 +1,29 @@
 portal = context.getPortalObject()
 
 kw = {}
-select_dict= {'causality__related__uid': None}
+select_dict = {'causality__related__uid': None}
 kw.update(
   portal_type='Sale Packing List',
   ledger__uid=portal.portal_categories.ledger.automated.getUid(),
   # This is an optimisation to help mariadb selecting a better index
   simulation_state=['delivered'],
-  left_join_list=select_dict.keys(),
-  causality__related__uid=None
-)
 
-portal.portal_catalog.searchAndActivate(
+  left_join_list=select_dict.keys(),
+  causality__related__uid=None,
+
   method_id='updateSimulation',
   packet_size=1, # Separate calls to many transactions
   method_kw={'expand_root': 1},
   activate_kw={'tag': tag},
+)
+
+portal.portal_catalog.searchAndActivate(
+  causality__portal_type=[
+    # Only Packing List created to generate deposit Invoice need to be expanded
+    'Payment Transaction',
+    # Discount Sale Packing List
+    'Subscription Request'
+  ],
   **kw
 )
 

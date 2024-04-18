2081444bff853c981fe6cad800155408f33b9a27
Rafael Monnerat <rafael@nexedi.com>
2024-04-18 20:53:58 +0200

    *: slapos_jio isnt installed anymore
      So drop entries in tests or upgrades since it is not considered anyway.

--- master/bt5/slapos_configurator/SkinTemplateItem/portal_skins/slapos_configurator/TemplateTool_getSlapOSMasterBusinessTemplateList.py
+++ master/bt5/slapos_configurator/SkinTemplateItem/portal_skins/slapos_configurator/TemplateTool_getSlapOSMasterBusinessTemplateList.py
@@ -5,7 +5,6 @@ bt5_update_catalog_list = bt5_installation_list
 
 keep_bt5_id_list = ['erp5_ui_test',
                     'erp5_ui_test_core',
-                    'slapos_jio_ui_test',
                     # XXX erp5_accounting_l10n_fr should be removed when bug is fixed
                     'erp5_accounting_l10n_fr',
                     'erp5_secure_payment',

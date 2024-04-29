01bd2af34335c9a9931694e6844a154de9f8b342
Rafael Monnerat <rafael@nexedi.com>
2024-04-29 17:18:21 +0000

    slapos_crm: Instance tree are causality
      not the agregate.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_updateSupportRequestMonitoringState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_updateSupportRequestMonitoringState.py
@@ -5,7 +5,7 @@ portal.portal_catalog.searchAndActivate(
     portal_type='Support Request',
     simulation_state=['submitted', 'validated', 'suspended'],
     resource__uid=default_resource_uid,
-    aggregate__portal_type=["Instance Tree"],
+    causality__portal_type=["Instance Tree"],
     method_id='SupportRequest_updateMonitoringState',
     activate_kw={'tag':tag}
   )

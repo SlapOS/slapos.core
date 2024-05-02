aa0d8ad6a595128da496310b9683fc03f33c71ad
Romain Courteaud <romain@nexedi.com>
2024-05-02 15:33:46 +0000

    slapos_panel: manually create a new Software Type

---
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/SoftwareProduct_addSlapOSSoftwareType.py
@@ -0,0 +1,27 @@
+from Products.ERP5Type.Message import translateString
+
+portal = context.getPortalObject()
+software_product = context
+
+# First, search if the type already exists
+software_type_variation = portal.portal_catalog.getResultValue(
+  portal_type="Software Product Type Variation",
+  parent_uid=software_product.getUid(),
+  title=software_type
+)
+if software_type_variation is not None:
+  return software_product.Base_redirect(
+    keep_items={
+      'portal_status_message': translateString('Software Release already exist.')
+    }
+  )
+
+software_product.newContent(
+  portal_type="Software Product Type Variation",
+  title=software_type
+)
+
+return software_product.Base_redirect(
+  keep_items={
+    'portal_status_message': translateString('New Software Release created.')
+  })

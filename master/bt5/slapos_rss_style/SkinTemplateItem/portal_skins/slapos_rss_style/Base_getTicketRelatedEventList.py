04165c013b38979edba39e84e3258402004aa404
Rafael Monnerat <rafael@nexedi.com>
2024-05-08 19:57:52 +0000

    slapos_rss_style: Include Subscription Request portal_type on the rss feed
       This allows, in case of error, verbose what to do

--- master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/Base_getTicketRelatedEventList.py
+++ master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/Base_getTicketRelatedEventList.py
@@ -1,4 +1,4 @@
 return context.Base_getEventList(
   list_lines=list_lines,
-  follow_up_portal_type=['Support Request', 'Upgrade Decision'],
+  follow_up_portal_type=['Support Request', 'Upgrade Decision', 'Subscription Request'],
   context_related=True, **kw)

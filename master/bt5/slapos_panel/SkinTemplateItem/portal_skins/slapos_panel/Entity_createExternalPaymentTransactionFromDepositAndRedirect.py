430504359516fa44d9117e727b84dd5f887f8c89
Rafael Monnerat <rafael@nexedi.com>
2024-05-17 20:41:13 +0000

    slapos_panel: Handle deposit outstanding amount as a list
       Since it should handle multiple *_section.

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Entity_createExternalPaymentTransactionFromDepositAndRedirect.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Entity_createExternalPaymentTransactionFromDepositAndRedirect.py
@@ -10,10 +10,20 @@ if currency_reference:
 if currency_value is None:
   raise ValueError("Unknown Currency: %s" % currency_reference)
 
+ledger_uid = portal.portal_categories.ledger.automated.getUid()
+currency_uid = currency_value.getUid()
+
 payment_mode = context.Base_getPaymentModeForCurrency(currency_value.getUid())
 assert payment_mode is not None
 
-deposit_price = context.Entity_getOutstandingDepositAmount(currency_value.getUid())
+outstanding_amount_list = context.Entity_getOutstandingDepositAmountList(
+  resource_uid=currency_uid, ledger_uid=ledger_uid)
+
+if len(outstanding_amount_list) != 1:
+  raise ValueError("It was expected exactly one amount to pay for %s currency (%s found)" % (
+    currency_uid, outstanding_amount_list))
+
+deposit_price = outstanding_amount_list[0].total_price
 if 0 >= deposit_price:
   raise ValueError("Nothing to pay")
 

d157e505c9f9d2a108be5053247991a171923eec
Rafael Monnerat <rafael@nexedi.com>
2024-05-27 23:12:56 +0000

    slapos_panel: Do not propagate website as parameter
       Use acquisition properly.

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_createExternalPaymentTransactionFromOutstandingAmountAndRedirect.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_createExternalPaymentTransactionFromOutstandingAmountAndRedirect.py
@@ -1,9 +1,15 @@
 portal = context.getPortalObject()
 
+# Get entity from context to preserve path
 entity = portal.portal_membership.getAuthenticatedMember().getUserValue()
+
 outstanding_amount = context
+web_site = outstanding_amount.getWebSiteValue()
+
 assert outstanding_amount.getLedgerUid() == portal.portal_categories.ledger.automated.getUid()
 assert outstanding_amount.getDestinationSectionUid() == entity.getUid()
+assert web_site is not None
+
 
 payment_mode = outstanding_amount.Base_getPaymentModeForCurrency(outstanding_amount.getPriceCurrencyUid())
 
@@ -31,6 +37,9 @@ def wrapWithShadow(entity, outstanding_amount, payment_mode):
       payment_mode=payment_mode
     )
   raise ValueError("Unsupported outstanding amount type: %s" % (portal_type))
+
+# Ensure to re-take the entity under a proper acquisition context
+entity = web_site.restrictedTraverse(entity.getRelativeUrl())
 
 payment_transaction = entity.Person_restrictMethodAsShadowUser(
   shadow_document=entity,

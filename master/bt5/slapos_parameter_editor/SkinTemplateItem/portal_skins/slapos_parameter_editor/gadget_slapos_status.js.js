999f91271990f0d4135f2bdacd0b2262366ecd40
Roque Porchetto <roque.porchetto@nexedi.com>
2024-06-10 16:39:21 +0200

    slapos_parameter_editor: add slapos master url to monitor url

--- master/bt5/slapos_parameter_editor/SkinTemplateItem/portal_skins/slapos_parameter_editor/gadget_slapos_status.js.js
+++ master/bt5/slapos_parameter_editor/SkinTemplateItem/portal_skins/slapos_parameter_editor/gadget_slapos_status.js.js
@@ -234,9 +234,11 @@
             options.hasOwnProperty('result'))) {
         return domsugar(this.element);
       }
-      if (options.result.hasOwnProperty('monitor_url')) {
+      // add current master url to monitor url
+      if (options.result.hasOwnProperty('monitor_url') &&
+          options.result.monitor_url) {
         options.result.monitor_url +=
-          '&slapos_master_url=' + window.location.host;
+          '&slapos_master_url=' + window.location.origin;
       }
       // Save will force the gadget to be updated so
       // result is empty.

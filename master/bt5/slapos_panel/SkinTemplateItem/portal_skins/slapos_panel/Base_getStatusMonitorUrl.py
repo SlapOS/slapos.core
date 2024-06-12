95fd70f13460b244948694f490295ffdaeae9b98
Roque Porchetto <roque.porchetto@nexedi.com>
2024-06-12 17:26:57 +0200

    slapos_panel: drop obsolete setting_configurator url

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_getStatusMonitorUrl.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_getStatusMonitorUrl.py
@@ -29,20 +29,18 @@ url_parameter_kw = { 'page': 'ojsm_landing' }
 instance_tree = context
 if context.getPortalType() in ["Software Instance", "Slave Instance"]:
   instance_tree = context.getSpecialiseValue(portal_type="Instance Tree")
+
 connection_parameter_dict = json.loads(instance_tree.InstanceTree_getMonitorParameterDict(), object_hook=_byteify)
 if all(key in connection_parameter_dict for key in ('username', 'password', 'url')):
   url_parameter_kw['username'] = connection_parameter_dict['username']
   url_parameter_kw['password'] = connection_parameter_dict['password']
   url_parameter_kw['url'] = connection_parameter_dict['url']
 
-if context.getPortalType() == "Instance Tree":
-  for connection_parameter in context.InstanceTree_getConnectionParameterList(raw=True):
-    if 'connection_key' in connection_parameter and connection_parameter['connection_key'] == "monitor-setup-url":
-      return connection_parameter['connection_value']
-  url_parameter_kw['query'] = 'portal_type:"Instance Tree" AND title:"%s"' % context.getTitle()
-
 if not(connection_parameter_dict):
   return ''
+
+if context.getPortalType() == "Instance Tree":
+  url_parameter_kw['query'] = 'portal_type:"Instance Tree" AND title:"%s"' % context.getTitle()
 
 if context.getPortalType() in ["Software Instance", "Slave Instance"]:
   url_parameter_kw['query'] = 'portal_type:"Software Instance" AND title:"%s" AND ' % context.getTitle() + 'specialise_title:"%s"' % context.getSpecialiseTitle()

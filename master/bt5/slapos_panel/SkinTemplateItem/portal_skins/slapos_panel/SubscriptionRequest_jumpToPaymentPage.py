e86285d942b8f1cb16a5441607a61a5c8f4a4abe
Rafael Monnerat <rafael@nexedi.com>
2024-07-05 13:12:37 +0000

    slapos_panel: Jump into instance tree payment if possible
       Prefer use payment page related to the instance tree rather them the generic one, so it reduces confusion.

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/SubscriptionRequest_jumpToPaymentPage.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/SubscriptionRequest_jumpToPaymentPage.py
@@ -1,2 +1,9 @@
+aggregate_value = context.getAggregateValue()
+
+if aggregate_value.getPortalType() == "Instance Tree":
+  return aggregate_value.Base_redirect(
+    'InstanceTree_viewCreateDirectDepositPaymentTransactionOnSlaposPanelDialog'
+  )
+
 return context.getPortalObject().accounting_module.Base_redirect(
   "AccountingTransactionModule_viewCreateExternalPaymentTransactionOnSlaposPanelDialog")

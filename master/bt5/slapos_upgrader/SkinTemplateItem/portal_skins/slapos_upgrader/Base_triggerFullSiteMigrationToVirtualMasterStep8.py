29831af17b1ab190212903334c94abed70586b89
Romain Courteaud <romain@nexedi.com>
2024-08-26 07:31:24 +0000

    slapos_upgrader: trigger open order creation for the project

---
+++ master/bt5/slapos_upgrader/SkinTemplateItem/portal_skins/slapos_upgrader/Base_triggerFullSiteMigrationToVirtualMasterStep8.py
@@ -0,0 +1,9 @@
+portal = context.getPortalObject()
+
+############################################
+# Trigger open order creation for virtual master
+############################################
+alarm = portal.restrictedTraverse('portal_alarms/slapos_subscription_request_validate_submitted')
+alarm.activeSense()
+
+context.activate(after_path=alarm.getPath(), priority=4).getId()

802b79e0df0c6cace3d0cb138d91665f2dafefa2
Rafael Monnerat <rafael@nexedi.com>
2024-08-29 18:01:58 +0000

    slapos_panel_ui_test: Create use different project title every run
       Otherwise, whenever you run the tests multiple times, it ends up failing by selecting the wrong one on the listbox.

--- master/bt5/slapos_panel_ui_test/SkinTemplateItem/portal_skins/slapos_panel_ui_test/ERP5Site_generateTestUserConfiguration.py
+++ master/bt5/slapos_panel_ui_test/SkinTemplateItem/portal_skins/slapos_panel_ui_test/ERP5Site_generateTestUserConfiguration.py
@@ -6,5 +6,6 @@ return {
   'customer_login': 'testcustomer%s' % now,
   'remote_customer_login': 'testremotecustomer%s' % now,
   'manager_login': 'testmanager%s' % now,
-  'passwd': 'eiChaxo5Eefier9vAek7phie$%s' % now
+  'passwd': 'eiChaxo5Eefier9vAek7phie$%s' % now,
+  'project_title': 'TestProject%s' % now
 }

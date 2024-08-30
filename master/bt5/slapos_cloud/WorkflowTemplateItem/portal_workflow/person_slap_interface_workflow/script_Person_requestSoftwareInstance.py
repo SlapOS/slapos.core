f600b30a05fc40e206939a7a4b8f4d64de34613f
Romain Courteaud <romain@nexedi.com>
2024-08-30 07:45:54 +0000

    slapos_cloud: move instance tree update logic into a python script

--- master/bt5/slapos_cloud/WorkflowTemplateItem/portal_workflow/person_slap_interface_workflow/script_Person_requestSoftwareInstance.py
+++ master/bt5/slapos_cloud/WorkflowTemplateItem/portal_workflow/person_slap_interface_workflow/script_Person_requestSoftwareInstance.py
@@ -79,43 +79,9 @@ else:
     activate_kw={'tag': tag},
   )
 
-promise_kw = {
-  'instance_xml': instance_xml,
-  'software_type': software_type,
-  'sla_xml': sla_xml,
-  'software_release': software_release_url_string,
-  'shared': is_slave,
-}
-
-context.REQUEST.set('request_instance_tree', request_instance_tree)
-# Change desired state
-if (root_state == "started"):
-  request_instance_tree.requestStart(**promise_kw)
-elif (root_state == "stopped"):
-  request_instance_tree.requestStop(**promise_kw)
-elif (root_state == "destroyed"):
-  request_instance_tree.requestDestroy(**promise_kw)
-  context.REQUEST.set('request_instance_tree', None)
-else:
-  raise ValueError("state should be started, stopped or destroyed")
-
-request_instance_tree.requestInstance(
-  software_release=software_release_url_string,
-  software_title=software_title,
-  software_type=software_type,
-  instance_xml=instance_xml,
-  sla_xml=sla_xml,
-  shared=is_slave,
-  state=root_state,
+request_instance_tree.InstanceTree_updateParameterAndRequest(
+  root_state, software_release_url_string, software_title, software_type, instance_xml, sla_xml, is_slave
 )
-
-# Change the state at the end to allow to execute updateLocalRoles only once in the transaction
-validation_state = request_instance_tree.getValidationState()
-slap_state = request_instance_tree.getSlapState()
-if validation_state == 'draft':
-  request_instance_tree.portal_workflow.doActionFor(request_instance_tree,
-                                           'validate_action')
-if (validation_state != 'archived') and \
-   (slap_state == 'destroy_requested'):
-  # XXX TODO do not use validation workflow to filter destroyed subscription
-  request_instance_tree.archive()
+context.REQUEST.set('request_instance_tree', request_instance_tree)
+if (root_state == "destroyed"):
+  context.REQUEST.set('request_instance_tree', None)

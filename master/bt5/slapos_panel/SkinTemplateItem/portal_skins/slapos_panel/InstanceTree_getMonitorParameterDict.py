13e242d13475408b645fc61973d045e5139d1484
Rafael Monnerat <rafael@nexedi.com>
2024-09-09 19:24:23 +0000

    slapos_panel: Get monitor-setup-url from '_' parameter
       Whenever the serialization is JSON-in-xml the parameters are uploaded into {'_' : '{}') format.

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/InstanceTree_getMonitorParameterDict.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/InstanceTree_getMonitorParameterDict.py
@@ -24,6 +24,12 @@ if instance is None or instance.getSlapState() == 'destroy_requested':
 
 parameter_dict = instance.getConnectionXmlAsDict()
 
+if parameter_dict.keys() == ["_"]:
+  import json
+  json_connection_dict = json.loads(parameter_dict["_"])
+  if isinstance(json_connection_dict, dict):
+    parameter_dict = json_connection_dict
+
 url_string = parameter_dict.get('monitor-setup-url', '')
 if url_string:
   if 'monitor-user' in parameter_dict and \

80ebb344c17de4834793c9cc05c74726f1417ce8
Rafael Monnerat <rafael@nexedi.com>
2024-09-20 20:17:53 +0000

    slapos_rss_style: Unify RSS feed for panel
       - Drop others rss feeds that are not **slapos_master_panel/feed
       - Keep backward compatibility with **slapos_master_panel/feed/critical, responding same as /feed
       - Keep backward compatibility with **slapos_master_panel/feed/invoice, responding same as legacy messages
       - Drop all feeds that were used over renderjs_runner

--- master/bt5/slapos_rss_style/SkinTemplateItem/portal_skins/slapos_rss_style/SaleInvoiceTransaction_getRSSDescription.py
+++
@@ -1,7 +0,0 @@
-msg = context.Base_translateString(
-  "You have a new invoice %s, please access the our website for more information:") % context.getReference()
-
-return """%s
-
-  %s
-""" % (msg, context.Base_getTicketUrl())

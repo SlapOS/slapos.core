de1f60c2605c9eaf9e1d08cab138c98e4a1e1b78
Rafael Monnerat <rafael@nexedi.com>
2024-09-30 20:23:08 +0000

    slapos_crm: Move check for hasReportedError
       Moving logic to SoftwareInstallation_hasReportedError allow us reuse the logic on SupportRequest_recheckMonitoring script.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_checkSoftwareInstallationState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_checkSoftwareInstallationState.py
@@ -1,7 +1,7 @@
 from DateTime import DateTime
 portal = context.getPortalObject()
 
-if (context.getMonitorScope() == "disabled"):
+if context.getMonitorScope() == "disabled":
   return
   
 project = context.getFollowUpValue()
@@ -16,48 +16,16 @@ software_installation_list = portal.portal_catalog(
     )
 
 support_request_list = []
-compute_node_reference = context.getReference()
-compute_node_title = context.getTitle()
 should_notify = True
 
-tolerance = DateTime()-0.5
+tolerance = DateTime() - 0.5
 for software_installation in software_installation_list:
   should_notify = False
-  if software_installation.getCreationDate() > tolerance:
-    # Give it 12 hours to deploy.
-    continue
-
-  if software_installation.getSlapState() != 'start_requested':
-    continue
-
-  reference = software_installation.getReference()
-  d = software_installation.getAccessStatus()
-  if d.get("no_data", None) == 1:
-    #should_notify = True
-    # We do not create if there is no information for the compilation
-    # it should be reported by more global alarm related to the compute node itself
-    last_contact = "No Contact Information"
-    ticket_title = "[MONITORING] No information for %s on %s" % (reference, compute_node_reference)
-    description = "The software release %s did not started to build on %s since %s" % \
-        (software_installation.getUrlString(), compute_node_title, software_installation.getCreationDate())
-  else:
-    last_contact = DateTime(d.get('created_at'))
-    if d.get("text").startswith("#building"):
-      should_notify = True
-      ticket_title = "[MONITORING] %s is building for too long on %s" % (reference, compute_node_reference)
-      description = "The software release %s is building for mode them 12 hours on %s, started on %s" % \
-              (software_installation.getUrlString(), compute_node_title, software_installation.getCreationDate())
-    elif d.get("text").startswith("#access"):
-      # Nothing to do.
-      pass
-    elif d.get("text").startswith("#error"):
-      should_notify = True
-      ticket_title = "[MONITORING] %s is failing to build on %s" % (reference, compute_node_reference)
-      description = "The software release %s is failing to build for too long on %s, started on %s" % \
-        (software_installation.getUrlString(), compute_node_title, software_installation.getCreationDate())
+  should_notify, ticket_title, description, last_contact = \
+    software_installation.SoftwareInstallation_hasReportedError(
+      tolerance=tolerance)
 
   if should_notify:
-
     project = context.getFollowUpValue()
     support_request = project.Project_createSupportRequestWithCausality(
       ticket_title,
@@ -79,8 +47,9 @@ for software_installation in software_installation_list:
       #language=XXX,
       substitution_method_parameter_dict={
         'compute_node_title':context.getTitle(),
-        'compute_node_id':reference,
-        'last_contact':last_contact
+        # Maybe a mistake on compute_node_id
+        'compute_node_id': software_installation.getReference(),
+        'last_contact': last_contact
       }
     )
 

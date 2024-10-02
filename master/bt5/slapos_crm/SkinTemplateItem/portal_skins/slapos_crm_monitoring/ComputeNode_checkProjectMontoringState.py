ffad06b33b5a3456e370c73b51dfd4f0d16cd3ee
Rafael Monnerat <rafael@nexedi.com>
2024-10-02 20:27:07 +0000

    slapos_crm: Reimplement check Instance Tree check State
        Merge into Project_checkMonitoring for launch per-project basis.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_checkProjectMontoringState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_checkProjectMontoringState.py
@@ -2,19 +2,34 @@ portal = context.getPortalObject()
 monitor_enabled_category = portal.restrictedTraverse(
   "portal_categories/monitor_scope/enabled", None)
 
-if context.Project_isSupportRequestCreationClosed():
+project = context.getFollowUpValue(portal_type='Project')
+assert project is not None
+
+if project.Project_isSupportRequestCreationClosed():
   return
 
 if monitor_enabled_category is not None:
+  project_uid = project.getUid()
   portal.portal_catalog.searchAndActivate(
     portal_type='Compute Node',
     validation_state='validated',
     monitor_scope__uid=monitor_enabled_category.getUid(),
-    follow_up__uid=context.getUid(),
+    follow_up__uid=project_uid,
     method_id='ComputeNode_checkMonitoringState',
     # This alarm bruteforce checking all documents,
     # without changing them directly.
     # Increase priority to not block other activities
     activate_kw={'tag':tag, 'priority': 2}
   )
-context.activate(after_tag=tag).getId()
+
+  portal.portal_catalog.searchAndActivate(
+    # Slave is required due unallocated use case
+    portal_type=['Software Instance' 'Slave Instance'],
+    validation_state='validated',
+    follow_up__uid=project_uid,
+    group_by=['specialise_uid'],
+    method_id='SoftwareInstance_checkInstanceTreeMonitoringState',
+    activate_kw = {'tag':tag}
+  )
+
+project.activate(after_tag=tag).getId()

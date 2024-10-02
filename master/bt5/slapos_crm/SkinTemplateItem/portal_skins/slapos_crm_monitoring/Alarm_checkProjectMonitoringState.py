4d3a37311c88e953d07197a05640084eaa52a047
Rafael Monnerat <rafael@nexedi.com>
2024-10-02 20:27:07 +0000

    slapos_crm: Reimplement check Instance Tree check State
        Merge into Project_checkMonitoring for launch per-project basis.

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkProjectMonitoringState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkProjectMonitoringState.py
@@ -1,8 +1,15 @@
 portal = context.getPortalObject()
+monitor_enabled_category = portal.restrictedTraverse(
+  "portal_categories/monitor_scope/enabled", None)
+
+portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
-    portal_type='Project',
+    portal_type='Compute Node',
     validation_state='validated',
-    method_id='Project_checkMonitoringState',
-    activate_kw={'tag': tag}
+    method_id='ComputeNode_checkProjectMontoringState',
+    monitor_scope__uid=monitor_enabled_category.getUid(),
+    group_by=['follow_up_uid'],
+    method_kw={'tag': tag},
+  activate_kw={'tag': tag, 'priority': 2}
   )
 context.activate(after_tag=tag).getId()

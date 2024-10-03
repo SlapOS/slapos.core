09e3094d5af0b1c446428690ebc35c61ab0aeb75
Romain Courteaud <romain@nexedi.com>
2024-10-03 13:24:49 +0000

    slapos_pdm: do not create small activity group
    As there is no comment, let's try to drop it.

--- master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_checkAndUpgradeInstanceTree.py
+++ master/bt5/slapos_pdm/SkinTemplateItem/portal_skins/slapos_pdm/Alarm_checkAndUpgradeInstanceTree.py
@@ -3,7 +3,6 @@ portal.portal_catalog.searchAndActivate(
   portal_type='Instance Tree',
   validation_state = 'validated',
   method_id = 'InstanceTree_createUpgradeDecision',
-  packet_size=1,
   activate_kw = {'tag':tag},
   **{"slapos_item.slap_state": ['start_requested', 'stop_requested']}
 )

4ba19bd44aa76562a7a24dbf140ac09597441926
Romain Courteaud <romain@nexedi.com>
2024-10-07 13:11:37 +0000

    slapos_crm: add an alarm to deliver stopped event
    Interact with the support request if possible.

---
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkStoppedEventToDeliver.py
@@ -0,0 +1,13 @@
+portal = context.getPortalObject()
+
+activate_kw = {'tag': tag}
+portal.portal_catalog.searchAndActivate(
+  portal_type=portal.getPortalEventTypeList(),
+  simulation_state='stopped',
+  follow_up__portal_type='Support Request',
+  method_id='Event_checkStoppedToDeliver',
+  method_kw={'activate_kw': activate_kw},
+  activate_kw=activate_kw
+)
+
+context.activate(after_tag=tag).getId()
---
+++ master/bt5/slapos_crm/WorkflowTemplateItem/portal_workflow/slapos_crm_interaction_workflow/script_Base_triggerCheckStoppedEventToDeliver.py
@@ -0,0 +1,2 @@
+event = state_change['object']
+return event.Base_reindexAndSenseAlarm(['slapos_crm_check_stopped_event_to_deliver'])

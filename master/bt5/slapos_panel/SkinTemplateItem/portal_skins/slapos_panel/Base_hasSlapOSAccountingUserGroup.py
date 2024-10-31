06d325c5f112e0107862ec00a06c96801b42988c
Rafael Monnerat <rafael@nexedi.com>
2024-10-31 17:42:13 +0000

    slapos_panel: Only limit event creation to Accountant and Seller
       Only owner, Accountant or sales team can respond subscription requests and regularisation requests.
    
       This is out of the scope of Production Manager/Agent, so no reason to allow them to respond those tickets.

---
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_hasSlapOSAccountingUserGroup.py
@@ -0,0 +1,28 @@
+portal = context.getPortalObject()
+checked_permission = 'Access contents information'
+
+if customer_relation == 'destination_decision':
+  destination_decision = context.getDestinationDecisionValue(
+    checked_permission=checked_permission)
+else:
+  raise ValueError('Unexpected customer relation: %s' % customer_relation)
+
+if destination_decision is None:
+  return False
+
+member = portal.portal_membership.getAuthenticatedMember()
+getGroups = getattr(member, 'getGroups', None)
+if getGroups is None:
+  return False
+
+user_group_list = getGroups()
+return (
+  ((customer) and
+   (destination_decision.getPortalType() == 'Person') and
+   (destination_decision.getUserId() in user_group_list)
+  ) or
+  ((accountant) and ('F-ACCOUNTING*' in user_group_list)) or
+  ((seller) and ('F-SALEMAN' in user_group_list)) or
+  ((seller) and ('F-SALEAGT' in user_group_list)) or
+  ((seller) and ('F-SALE*' in user_group_list))
+)

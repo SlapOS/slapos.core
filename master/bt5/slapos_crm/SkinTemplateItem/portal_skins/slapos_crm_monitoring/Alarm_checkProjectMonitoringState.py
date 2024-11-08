fac83d6969ba26f92b26be3345e020917b8dcecf
Romain Courteaud <romain@nexedi.com>
2024-11-08 16:50:44 +0000

    slapos_crm: create monitoring Support Request for Slave Instances in error, and instances on a Remote Node

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkProjectMonitoringState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkProjectMonitoringState.py
@@ -1,13 +1,22 @@
+from Products.ZSQLCatalog.SQLCatalog import SimpleQuery, ComplexQuery
+
 portal = context.getPortalObject()
 monitor_enabled_category = portal.restrictedTraverse(
   "portal_categories/monitor_scope/enabled", None)
 
 portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
-    portal_type='Compute Node',
     validation_state='validated',
     method_id='ComputeNode_checkProjectMontoringState',
-    monitor_scope__uid=monitor_enabled_category.getUid(),
+    node=ComplexQuery(
+      SimpleQuery(portal_type='Remote Node'),
+      ComplexQuery(
+        SimpleQuery(portal_type='Compute Node'),
+        SimpleQuery(monitor_scope__uid=monitor_enabled_category.getUid()),
+        logical_operator='and'
+      ),
+      logical_operator='or'
+    ),
     group_by=['follow_up_uid'],
     method_kw={'tag': tag},
   activate_kw={'tag': tag, 'priority': 2}

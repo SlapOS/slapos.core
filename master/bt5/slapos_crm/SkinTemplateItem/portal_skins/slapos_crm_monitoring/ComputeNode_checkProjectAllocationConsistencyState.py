75e5489fc70c64e3360297dadc3671511f888eaf
Rafael Monnerat <rafael@nexedi.com>
2024-11-25 20:17:41 +0000

    slapos_crm: Implement alarm to check Allocation Consistency
        Add a daily alarm to verify if all instance allocated have proper Allocation Supply configured and if they
        still respect the SLA.
    
        Extend SoftwareInstance_getReportedErrorDict to verify SLA, since, in general the context of the report should be the instance tree. The calculation is cheap to assert the SLA, and this script already has specific checks to inform the user about bad approaches.

---
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/ComputeNode_checkProjectAllocationConsistencyState.py
@@ -0,0 +1,34 @@
+from Products.ZSQLCatalog.SQLCatalog import SimpleQuery, ComplexQuery
+
+portal = context.getPortalObject()
+monitor_enabled_category = portal.restrictedTraverse(
+  "portal_categories/monitor_scope/enabled", None)
+
+project = context.getFollowUpValue(portal_type='Project')
+assert project is not None
+
+if project.Project_isSupportRequestCreationClosed():
+  return
+
+if monitor_enabled_category is not None:
+  project_uid = project.getUid()
+  portal.portal_catalog.searchAndActivate(
+    node=ComplexQuery(
+      SimpleQuery(portal_type='Remote Node'),
+      ComplexQuery(
+        SimpleQuery(portal_type='Compute Node'),
+        SimpleQuery(monitor_scope__uid=monitor_enabled_category.getUid()),
+        logical_operator='and'
+      ),
+      logical_operator='or'
+    ),
+    validation_state='validated',
+    follow_up__uid=project_uid,
+    method_id='ComputeNode_checkAllocationConsistencyState',
+    # This alarm bruteforce checking all documents,
+    # without changing them directly.
+    # Increase priority to not block other activities
+    activate_kw={'tag': tag, 'priority': 2}
+  )
+
+project.activate(after_tag=tag).getId()

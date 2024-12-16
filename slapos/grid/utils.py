3e1aec0ca40b70e4246927023ff09d5e638ff820
Alain Takoudjou <alain.takoudjou@nexedi.com>
2024-12-16 17:41:06 +0100

    slapos.grid: fixup! copy .netrc file to buildout home path
    copy netrc file to software home location with the good ownership.

--- slapos/grid/utils.py
+++ slapos/grid/utils.py
@@ -253,21 +253,6 @@ def getCleanEnvironment(logger, home_path='/tmp'):
     logger.debug('Removed from environment: %s',  ', '.join(sorted(removed_env)))
   return env
 
-def copyNetrcFile(dest_path):
-  user_home = os.environ['HOME']
-  netrc_file = os.path.join(user_home, '.netrc')
-  buildout_netrc = os.path.join(dest_path, '.netrc')
-  if os.path.abspath(netrc_file) == buildout_netrc:
-    return
-  if os.path.exists(netrc_file):
-    shutil.copyfile(netrc_file, buildout_netrc)
-    os.chmod(buildout_netrc, 0o600)
-
-def cleanupNetrcFile(dest_path):
-  buildout_netrc = os.path.join(dest_path, '.netrc')
-  if os.path.exists(buildout_netrc):
-    os.unlink(buildout_netrc)
-
 def setRunning(logger, pidfile):
   """Creates a pidfile. If a pidfile already exists, we exit"""
   if os.path.exists(pidfile):
@@ -455,7 +440,6 @@ def launchBuildout(path, buildout_binary, logger,
       path))
     if timeout is not None:
       logger.debug('Launching buildout with %ss timeout', timeout)
-    copyNetrcFile(path)
     process_handler = SlapPopen(invocation_list,
                                 preexec_fn=lambda: dropPrivileges(uid, gid, logger=logger),
                                 cwd=path,
@@ -472,7 +456,6 @@ def launchBuildout(path, buildout_binary, logger,
     logger.exception(exc)
     raise BuildoutFailedError(exc)
   finally:
-    cleanupNetrcFile(path)
     old_umask = os.umask(umask)
     logger.debug('Restore umask from %03o to %03o' % (old_umask, umask))
 

6efd9d322569527802892dd5f829a5e1472e9538
Rafael Monnerat <rafael@nexedi.com>
2025-01-10 19:18:19 -0300

    slapos_jio*: Drop unused instance and code
      it cause confusion on grep and search, if we need access its code,
      we may rely on some older tag to search the related code

--- master/bt5/slapos_jio/SkinTemplateItem/portal_skins/slapos_hal_json_style/Event_getAcknowledgementDict.py
+++
@@ -1,9 +0,0 @@
-from zExceptions import Unauthorized
-if REQUEST is not None:
-  raise Unauthorized
-
-return {
-  "title": context.getCausalityTitle(),
-  "text_content": context.getTextContent(),
-  "link": context.getCausalityValue(context).getFollowUp(checked_permission='View')
-}

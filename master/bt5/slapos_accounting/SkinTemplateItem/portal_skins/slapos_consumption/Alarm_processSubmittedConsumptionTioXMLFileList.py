d0786dd536c4c66c06403b105f761fe379f643f9
Rafael Monnerat <rafael@nexedi.com>
2025-02-28 18:29:49 +0000

    slapos_accounting: Reimplement ComputerConsumptionTioXMLFile_solveInvoicingGeneration

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_consumption/Alarm_processSubmittedConsumptionTioXMLFileList.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_consumption/Alarm_processSubmittedConsumptionTioXMLFileList.py
@@ -3,7 +3,7 @@ portal = context.getPortalObject()
 portal.portal_catalog.searchAndActivate(
   portal_type="Computer Consumption TioXML File",
   validation_state="submitted",
-  method_id='ComputerConsumptionTioXMLFile_solveInvoicingGeneration',
+  method_id='ComputerConsumptionTioXMLFile_generateSalePackingList',
   activity_count=100,
   packet_size=100,
   activate_kw={'tag': tag, 'priority': 5}

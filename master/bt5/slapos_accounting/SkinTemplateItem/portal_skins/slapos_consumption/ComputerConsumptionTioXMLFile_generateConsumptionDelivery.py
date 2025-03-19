ecb1dee5e562c1c7310c00b3a2576bbc40e024d5
Rafael Monnerat <rafael@nexedi.com>
2025-03-19 20:07:54 +0000

    slapos_accounting: Introduce Consumption Delivery portal type
       It is a Delivery Type, and It works like Sale Packing List, however it will be used to generate to generate Sale Packing Lists rather them invoices.
    
       It was introduced a different portal type to use different simulation tree, and include "Consumption" specific features.

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_consumption/ComputerConsumptionTioXMLFile_generateConsumptionDelivery.py
+++ master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_consumption/ComputerConsumptionTioXMLFile_generateConsumptionDelivery.py
@@ -1,9 +1,8 @@
+import six
 from zExceptions import Unauthorized
 if REQUEST is not None:
-  #raise Unauthorized
-  pass
+  raise Unauthorized
 
-import six
 portal = context.getPortalObject()
 document = context
 result = []
@@ -45,36 +44,43 @@ for movement in tioxml_dict["movement"]:
   if compute_node.getReference() == reference:
     item = compute_node
     destination_decision_value = project_value.getDestinationValue(portal_type="Person")
+    aggregate_value_list = [item]
   else:
+    instance = None
+
     # XXX This looks like bad
     if reference.startswith("slapuser"):
       reference = reference.replace("slapuser", "slappart")
 
     # Find the partition / software instance / user
     partition = portal.portal_catalog.getResultValue(
-      parent_uid=compute_node.getUid(),
       reference=reference,
-      portal_type="Compute Partition",
-      validation_state="validated")
+      parent_uid=compute_node.getUid(),
+      portal_type="Compute Partition")
 
-    if partition is None or partition.getSlapState() != 'busy':
-      continue
+    if partition is not None:
+      if partition.getSlapState() != 'busy':
+        # Instance was already detached
+        continue
 
-    instance = portal.portal_catalog.getResultValue(
-      default_aggregate_uid=partition.getUid(),
-      portal_type="Software Instance",
-      validation_state="validated")
+      instance = portal.portal_catalog.getResultValue(
+        default_aggregate_uid=partition.getUid(),
+        portal_type="Software Instance",
+        validation_state="validated")
 
     if instance is None:
-      # There is no software instance for this partition anymore
-      # so we just skip this partial consumption.
-      # XXX Probably this is bad to skip partial consumption, return this once the rest is done
+      instance = portal.portal_catalog.getResultValue(
+        reference=reference,
+        portal_type=["Software Instance", "Slave Instance"],
+        validation_state="validated")
+
+    if instance is None:
       continue
 
     assert project_value.getRelativeUrl() == instance.getFollowUp(portal_type='Project')
     item = instance.getSpecialiseValue(portal_type="Instance Tree")
     destination_decision_value = item.getDestinationSectionValue(portal_type="Person")
-
+    aggregate_value_list = [item, instance]
     assert item is not None, "Instance has no Instance Tree"
 
   # Fetch related Subscription Request for the service
@@ -96,60 +102,81 @@ for movement in tioxml_dict["movement"]:
                        title=movement['title'],
                        project=project_value,
                        person=destination_decision_value,
-                       quantity=movement['quantity'],
+                       # Quantity is recorded here as negative
+                       quantity=-movement['quantity'],
                        # Should I aggregate instance?
-                       aggregate_value_list=[item],
+                       aggregate_value_list=aggregate_value_list,
                        resource=resource))
+
+module = portal.portal_trash
 
 # Only create if something to be done.
 for movement_entry in six.itervalues(movement_dict):
   subscription_request = movement_entry[0]['subscription']
 
+  tmp_sale_order = module.newContent(
+    portal_type='Sale Order',
+    temp_object=True,
+    trade_condition_type="consumption",
+    source_project_value=subscription_request.getSourceProjectValue(),
+    destination_project_value=subscription_request.getDestinationProjectValue(),
+    ledger_value=subscription_request.getLedgerValue(),
+    start_date=document.getCreationDate(),
+    price_currency_value=subscription_request.getPriceCurrencyValue(),
+    destination_value=destination_decision_value,
+  )
+  tmp_sale_order.SaleOrder_applySaleTradeCondition(batch_mode=1, force=1)
+
+  if tmp_sale_order.getSpecialise(None) is None:
+    raise AssertionError('Can not find a trade condition to generate the Subscription Request')
+
+  specialise_value = tmp_sale_order.getSpecialiseValue()
+  if tmp_sale_order.getTradeConditionType() != specialise_value.getTradeConditionType():
+    raise AssertionError('Unexpected different trade_condition_type: %s %s' % (tmp_sale_order.getTradeConditionType(), specialise_value.getTradeConditionType()))
+
   # Time to create the PL
-  sale_packing_list = portal.sale_packing_list_module.newContent(
-    portal_type="Sale Packing List",
+  consumption_delivery = portal.consumption_delivery_module.newContent(
+    portal_type="Consumption Delivery",
     title=tioxml_dict['title'],
-    # start_date?
-    # stop_date?
-    # Missing specialise?
-    # For reference use the values of the subscription request, can they vary?
-    specialise_value=subscription_request.getSpecialiseValue(),
+
     source_value=subscription_request.getSourceValue(),
     source_section_value=subscription_request.getSourceSectionValue(),
     source_decision_value=subscription_request.getSourceDecisionValue(),
-    source_project_value=subscription_request.getSourceProjectValue(),
     destination_value=subscription_request.getDestinationValue(),
     destination_section_value=subscription_request.getDestinationSectionValue(),
     destination_decision_value=subscription_request.getDestinationDecisionValue(),
-    destination_project_value=subscription_request.getDestinationProjectValue(),
-    ledger_value=subscription_request.getLedgerValue(),
+
+    specialise_value=specialise_value,
+    source_project_value=tmp_sale_order.getSourceProjectValue(),
+    destination_project_value=tmp_sale_order.getDestinationProjectValue(),
+    ledger_value=tmp_sale_order.getLedgerValue(),
     causality_value=document, ## Should be like this?
-    start_date=document.getCreationDate(),
-    price_currency_value=subscription_request.getPriceCurrencyValue(),
+    start_date=tmp_sale_order.getCreationDate(),
+    price_currency_value=tmp_sale_order.getPriceCurrencyValue(),
   )
 
   for movement in movement_entry:
-    service = portal.restrictedTraverse(movement['resource'])
-    line = sale_packing_list.newContent(
-      portal_type="Sale Packing List Line",
+    resource = portal.restrictedTraverse(movement['resource'])
+    line = consumption_delivery.newContent(
+      portal_type="Consumption Delivery Line",
       title=movement['title'],
       quantity=movement['quantity'],
       aggregate_value_list=movement['aggregate_value_list'],
-      resource_value=service,
-      quantity_unit=service.getQuantityUnit(),
-      base_contribution_list=service.getBaseContributionList(),
-      use=service.getUse()
+      resource_value=resource,
+      quantity_unit=resource.getQuantityUnit(),
+      base_contribution_list=resource.getBaseContributionList(),
+      use_list=resource.getUseList()
     )
     assert line.getPrice(), line.getPrice()
-  sale_packing_list.Delivery_fixBaseContributionTaxableRate()
-  sale_packing_list.Base_checkConsistency()
-  sale_packing_list.confirm(comment="Created from %s" % document.getRelativeUrl())
-  sale_packing_list.start()
-  sale_packing_list.stop()
-  sale_packing_list.deliver()
-  sale_packing_list.startBuilding()
+  consumption_delivery.Delivery_fixBaseContributionTaxableRate()
+  consumption_delivery.Base_checkConsistency()
+  consumption_delivery.confirm(comment="Created from %s" % document.getRelativeUrl())
+  consumption_delivery.start()
+  consumption_delivery.stop()
+  consumption_delivery.deliver()
+  consumption_delivery.startBuilding()
 
-  result.append(sale_packing_list.getRelativeUrl())
+  result.append(consumption_delivery.getRelativeUrl())
 
-document.share(comment="Created packing list: %s" % result)
+document.share(comment="Created Delivery: %s" % result)
 return result

958681c6bb3a603b7b8586ea24044cc450301e6d
Romain Courteaud <romain@nexedi.com>
2025-03-20 09:44:22 +0000

    slapos_panel: WIP custom schema url

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Project_getSoftwareReleaseSchemaUrl.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Project_getSoftwareReleaseSchemaUrl.py
@@ -5,5 +5,22 @@ if REQUEST is not None:
 # Ensure this script is called on the expected context
 # for futur compatibility
 assert context.getPortalType() == 'Project'
+portal = context.getPortalObject()
+
+use_category_uid = portal.restrictedTraverse("portal_categories/use/trade/sale").getUid(),
+product_list = portal.portal_catalog(
+  portal_type="Software Product",
+  validation_state=['validated', 'published'],
+  use__uid=use_category_uid,
+  follow_up__uid=context.getFollowUpUid()
+)
+if len(product_list) != 0:
+  software_release = portal.portal_catalog.getResultValue(
+    portal_type="Software Product Release Variation",
+    url_string=software_release_url,
+    parent_uid=[x.getUid() for x in product_list]
+  )
+  if software_release is not None:
+    pass
 
 return software_release_url.split("?")[0] + ".json"

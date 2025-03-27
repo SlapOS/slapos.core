bd52726f13bff054ae776c8e06ec065a9b0dc764
Rafael Monnerat <rafael@nexedi.com>
2025-03-27 20:35:41 +0000

    slapos_panel: Fixup redirect to Manual deposit payment
       Instance tree is not accessible by shadow user, and organisation/payment require it. Refactor the script to proper handle security for the creation/redirect to payment.
    
       Include tests to ensure that script works with or without the presence of subscription_request.

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/InstanceTree_redirectToManualDepositPayment.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/InstanceTree_redirectToManualDepositPayment.py
@@ -9,38 +9,44 @@ person = portal.portal_membership.getAuthenticatedMember().getUserValue()
 # Use proper acquisiton to generate the payment transaction
 person = web_site.restrictedTraverse(person.getRelativeUrl())
 
+subscription_request = portal.portal_catalog.getResultValue(
+  portal_type='Subscription Request',
+  aggregate__uid=context.getUid())
 
-def wrapWithShadowToNotGetUnauthorized(person):
-  # Fetch to know if the subscription request is already created
-  subscription_request = portal.portal_catalog.getResultValue(
-    portal_type='Subscription Request',
-    aggregate__uid=context.getUid())
+if subscription_request is not None:
+  if subscription_request.getSimulationState() != 'submitted':
+    # No need to continue if the subscription is already processed.
+    raise ValueError('No deposit is required, Subscription Request is not on submitted state')
+else:
+  subscription_request = context.Item_createSubscriptionRequest(temp_object=True)
 
-  if subscription_request is not None:
-    if subscription_request.getSimulationState() != 'submitted':
-      # No need to continue if the subscription is already processed.
-      raise ValueError('No deposit is required, Subscription Request is not on submitted state')
-  else:
-    subscription_request = context.Item_createSubscriptionRequest(temp_object=True)
+if subscription_request is None:
+  raise ValueError("Subscription Request was not found or generated")
 
-  if subscription_request is None:
-    raise ValueError("Subscription Request was not found or generated")
+initial_outstanting_list = []
 
+if subscription_request.isTempObject():
+  initial_outstanting_list.append(
+    subscription_request.asContext(
+      total_price=subscription_request.getPrice(None))
+  )
+
+payment_mode = subscription_request.Base_getPaymentModeForCurrency(
+  subscription_request.getPriceCurrencyUid())
+
+def wrapWithShadowToNotGetUnauthorized(person, ledger_uid,
+                                       section, resource_uid,
+                                       payment_mode,
+                                       initial_outstanting_list):
+
+  # Required because user cannot access organisation module
+  section_uid = portal.restrictedTraverse(section).getUid()
   outstanding_amount_list = person.Entity_getOutstandingDepositAmountList(
-    ledger_uid=subscription_request.getLedgerUid(),
-    section_uid=subscription_request.getSourceSectionUid(),
-    resource_uid=subscription_request.getPriceCurrencyUid())
+    ledger_uid=ledger_uid,
+    section_uid=section_uid,
+    resource_uid=resource_uid)
 
-  if subscription_request.isTempObject():
-    outstanding_amount_list.append(
-      #subscription_request
-      subscription_request.asContext(
-        total_price=subscription_request.getPrice(None))
-    )
-
-  payment_mode = subscription_request.Base_getPaymentModeForCurrency(
-    subscription_request.getPriceCurrencyUid())
-
+  outstanding_amount_list.extend(initial_outstanting_list)
   return person.Entity_createDepositPaymentTransaction(
     subscription_list=outstanding_amount_list,
     payment_mode=payment_mode
@@ -49,6 +55,10 @@ def wrapWithShadowToNotGetUnauthorized(person):
 payment_transaction = person.Person_restrictMethodAsShadowUser(
   shadow_document=person,
   callable_object=wrapWithShadowToNotGetUnauthorized,
-  argument_list=[person])
+  argument_list=[person, subscription_request.getLedgerUid(),
+    subscription_request.getSourceSection(),
+    subscription_request.getPriceCurrencyUid(),
+    payment_mode,
+    initial_outstanting_list])
 
 return payment_transaction.PaymentTransaction_redirectToManualPayment()

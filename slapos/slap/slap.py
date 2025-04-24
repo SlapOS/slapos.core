305a7b65f62ef4224efee3a69843dc4c2bf92ea7
Romain Courteaud <romain@nexedi.com>
2025-04-24 11:02:44 +0200

    slap.py: wip lazy

--- slapos/slap/slap.py
+++ slapos/slap/slap.py
@@ -120,12 +120,13 @@ class LazyInstanceParameterDict(dict):
       self['slave_instance_list'] = self.__instance.getSlaveInstanceList()
     return super().__iter__()
 
+  """
   def __len__(self):
     if (not self.__slave_instance_list_calculated):
       self.__slave_instance_list_calculated = True
       self['slave_instance_list'] = self.__instance.getSlaveInstanceList()
     return super().__len__()
-
+"""
   """
   def __missing__(self, key, *args, **kw):
     if (key == 'slave_instance_list') and (not self.__slave_instance_list_calculated):

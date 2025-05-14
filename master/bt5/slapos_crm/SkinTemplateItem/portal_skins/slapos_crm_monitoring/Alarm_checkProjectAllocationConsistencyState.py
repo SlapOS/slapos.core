b6fd6756eedd2109e539779f5c3a529b28244a7d
Romain Courteaud <romain@nexedi.com>
2025-05-14 13:35:35 +0000

    slapos_crm: increase the allocation consistency alarm priority
    This alarm is slow, and should not block other alarms

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkProjectAllocationConsistencyState.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm_monitoring/Alarm_checkProjectAllocationConsistencyState.py
@@ -5,6 +5,7 @@ monitor_enabled_category = portal.restrictedTraverse(
   "portal_categories/monitor_scope/enabled", None)
 
 portal = context.getPortalObject()
+activate_kw = {'tag': tag, 'priority': 5}
 portal.portal_catalog.searchAndActivate(
     validation_state='validated',
     method_id='ComputeNode_checkProjectAllocationConsistencyState',
@@ -18,7 +19,7 @@ portal.portal_catalog.searchAndActivate(
       logical_operator='or'
     ),
     group_by=['follow_up_uid'],
-    method_kw={'tag': tag},
-  activate_kw={'tag': tag, 'priority': 2}
+    method_kw={'activate_kw': activate_kw},
+  activate_kw=activate_kw
   )
 context.activate(after_tag=tag).getId()

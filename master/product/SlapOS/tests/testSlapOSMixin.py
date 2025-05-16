ba5254ab79dc5ce704b03778ac800fc37a0280c9
Rafael Monnerat <rafael@nexedi.com>
2025-05-16 14:44:54 -0300

    testSlapOSMixin: Use module name to reduce colision while running tests with same name

--- master/product/SlapOS/tests/testSlapOSMixin.py
+++ master/product/SlapOS/tests/testSlapOSMixin.py
@@ -82,12 +82,12 @@ class testSlapOSMixin(ERP5TypeTestCase):
       return
 
     ca_path = os.path.join(os.environ['TEST_CA_PATH'],
-                           self.__class__.__name__)
+          self.__class__.__module__, self.__class__.__name__)
 
     if os.path.exists(ca_path):
       shutil.rmtree(ca_path)
 
-    os.mkdir(ca_path)
+    os.makedirs(ca_path, 0o700)
     os.mkdir(os.path.join(ca_path, 'private'))
     os.mkdir(os.path.join(ca_path, 'crl'))
     os.mkdir(os.path.join(ca_path, 'certs'))

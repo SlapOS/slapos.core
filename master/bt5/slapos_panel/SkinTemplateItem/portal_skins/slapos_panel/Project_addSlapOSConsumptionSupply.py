818543ca4499ffc3b93b701fcb1141a49d926dd0
Rafael Monnerat <rafael@nexedi.com>
2025-05-29 20:32:07 +0000

    slapos_panel: Initial Consumption Supply UI implementation

---
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Project_addSlapOSConsumptionSupply.py
@@ -0,0 +1,49 @@
+from Products.ERP5Type.Message import translateString
+
+portal = context.getPortalObject()
+project = context
+
+compute_node_list = portal.portal_catalog(
+  portal_type="Compute Node",
+  validation_state=['validated', 'published'],
+  follow_up__uid=project.getUid(),
+  allocation_scope__uid=portal.restrictedTraverse("portal_categories/allocation_scope/open").getUid(),
+)
+
+# How to filter for the services related to Software Products rather them all
+#software_product_list = portal.portal_catalog(
+#  portal_type="Software Product",
+#  validation_state=['validated', 'published'],
+#  follow_up__uid=project.getUid(),
+#)
+
+
+service_list = portal.portal_catalog(
+  portal_type="Service",
+  validation_state=['validated', 'published'],
+  use__uid=portal.portal_categories.use.trade.consumption.getUid(),
+)
+
+if not len(service_list):
+  return context.Base_redirect(
+    keep_items={'portal_status_message': translateString('No Service found for Consumption use.')}
+)
+
+consumption_supply = portal.consumption_supply_module.newContent(
+  title="All compute nodes %s" % DateTime(),
+  portal_type="Consumption Supply",
+  destination_project_value=project,
+  start_date_range_min=DateTime(),
+  aggregate_list=[x.getRelativeUrl() for x in compute_node_list]
+)
+
+for sql_service in service_list:
+  consumption_supply.newContent(
+    portal_type="Consumption Supply Line",
+    title=sql_service.getTitle(),
+    resource_value=sql_service.getObject()
+  )
+
+return consumption_supply.Base_redirect(
+  keep_items={'portal_status_message': translateString('New Consumption Supply created.')}
+)

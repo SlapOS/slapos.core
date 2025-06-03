9b1e12c576795d194a4647cecac416c668431d43
Rafael Monnerat <rafael@nexedi.com>
2025-06-03 19:18:04 +0000

    slapos_erp5: [Py3] Fixup ERP5Site_assertDumpedConfiguration script
       Be consistent with str vs bytes while split

--- master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/ERP5Site_assertDumpedConfiguration.py
+++ master/bt5/slapos_erp5/SkinTemplateItem/portal_skins/slapos_administration/ERP5Site_assertDumpedConfiguration.py
@@ -1,11 +1,12 @@
 portal = context.getPortalObject()
 import difflib
-current_confguration = portal.ERP5Site_saveDumpedConfiguration(save=0)
+from Products.ERP5Type.Utils import bytes2str
+current_confguration = portal.ERP5Site_saveDumpedConfiguration(save=0).split("\n")
 try:
   save_file = portal.document_module[filename]
 except KeyError:
   raise ValueError("You must save the dumped configuration first with ERP5Site_saveDumpedConfiguration")
 
-saved_configuration = save_file.getData()
+saved_configuration = bytes2str(save_file.getData()).split("\n")
 
-return '\n'.join(difflib.unified_diff(saved_configuration.split("\n"), current_confguration.split("\n")))
+return '\n'.join(difflib.unified_diff(saved_configuration, current_confguration))

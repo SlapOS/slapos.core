4c5bc3546c2de59fa5c3ed596560907e198b7fba
Romain Courteaud <romain@nexedi.com>
2025-09-23 09:46:41 +0000

    slapos_panel: XXX workaround: filter script seems called multiple times
    See https://lab.nexedi.com/nexedi/erp5/-/merge_requests/2162

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/WebSection_filterSlaposPanelActionDict.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/WebSection_filterSlaposPanelActionDict.py
@@ -2,11 +2,14 @@ filtered_action_dict = {}
 for action_category_name, action_list in action_dict.items():
   filtered_action_dict[action_category_name] = []
   for action in action_list:
-    if action['id'] == 'slapos_panel_view':
+    if (action['id'] == 'slapos_panel_view') or \
+       ((action['id'] == 'view') and action.get('_default_view_changed', False)):
       # Rename the webapp default view action to 'view',
       # to be compatible with all the Base_redirect calls
       # hardcoding the 'view' action
       action['id'] = 'view'
+      # XXX XXX This script seems called multiple times...
+      action['_default_view_changed'] = True
       filtered_action_dict[action_category_name].append(action)
     elif 'slapos' in action['id']:
       filtered_action_dict[action_category_name].append(action)

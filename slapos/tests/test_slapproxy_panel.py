7dce8b4b126d4b543bd4aa6750abef9f117265e5
Romain Courteaud <romain@nexedi.com>
2025-10-17 16:19:14 +0200

    slapproxy: panel: add local UI to handle the local services
    - display the list of user's instance trees
    - display the instance trees's connection parameters
    - allow instance tree's parameters modification
    - request new instance
    
    Reuse as much as possible the code from https://panel.rapid.space.
    Fetch all dependencies with a Makefile to simplify code updates.
    Do not use rapid space's icon.
    
    Try to load the software release json schema from slapproxy public directory,
    as this UI will be used on offline servers.

---
+++ slapos/tests/test_slapproxy_panel.py
@@ -0,0 +1,58 @@
+from test_slapproxy import BasicMixin
+import os
+import unittest
+import tempfile
+
+class PanelTestCase(BasicMixin, unittest.TestCase):
+
+  def test_index_page(self):
+    response = self.app.get('/panel/')
+
+    # Response status code
+    assert response.status_code == 200, response.status_code
+
+    # Response body
+    assert b'<script src="panel.js">' in response.data, response.data
+    assert b'<script src="external/renderjs.js">' in response.data, response.data
+
+    # Response header forced
+    assert response.headers["Content-Type"] == \
+        "text/html; charset=utf-8", \
+        response.headers
+    assert response.headers["Content-Length"] == \
+        "3105", \
+        response.headers
+
+  def test_panel_dependencies(self):
+    response = self.app.get('/panel/panel.js')
+
+    # Response status code
+    assert response.status_code == 200, response.status_code
+
+    # Response body
+    assert b'callJsonRpcEntryPoint' in response.data, response.data
+
+    # Response header forced
+    assert response.headers["Content-Type"] == \
+        "application/javascript; charset=utf-8", \
+        response.headers
+    assert response.headers["Content-Length"] == \
+        "32062", \
+        response.headers
+
+  def test_panel_external_dependencies(self):
+    response = self.app.get('/panel/external/renderjs.js')
+
+    # Response status code
+    assert response.status_code == 200, response.status_code
+
+    # Response body
+    assert b'clearGadgetInternalParameters' in response.data, response.data
+
+    # Response header forced
+    assert response.headers["Content-Type"] == \
+        "application/javascript; charset=utf-8", \
+        response.headers
+    assert response.headers["Content-Length"] == \
+        "107174", \
+        response.headers

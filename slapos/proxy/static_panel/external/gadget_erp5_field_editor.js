ae956cc5110ba16357727bb68125954b3c9945bd
Romain Courteaud <romain@nexedi.com>
2025-10-17 16:19:14 +0200

    slapproxy: panel: add local UI to handle the local services
    - display the list of user's instance trees
    - display the instance trees's connection parameters
    - allow instance tree's parameters modification
    - request new instance
    
    Reuse as much as possible the code from https://panel.rapid.space.
    Fetch all dependencies with a Makefile to simplify code updates.
    Do not use rapid space's icon.
    
    Try to load the software release json schema from slapproxy public directory,
    as this UI will be used on offline servers.

---
+++ slapos/proxy/static_panel/external/gadget_erp5_field_editor.js
@@ -0,0 +1,76 @@
+/*global window, rJS */
+/*jslint indent: 2, maxerr: 3 */
+(function (window, rJS) {
+  "use strict";
+
+  rJS(window)
+    .setState({
+      tag: 'div'
+    })
+    .declareMethod('render', function (options) {
+      var field_json = options.field_json || {},
+        state_dict = {
+          value: field_json.value || field_json.default || "",
+          editable: field_json.editable,
+          name: field_json.key,
+          title: field_json.description,
+          hidden: field_json.hidden,
+          // Force calling subfield render
+          // as user may have modified the input value
+          render_timestamp: new Date().getTime()
+        };
+      state_dict.inner_html = state_dict.value;
+      return this.changeState(state_dict);
+    })
+
+    .onStateChange(function (modification_dict) {
+      var element = this.element,
+        gadget = this,
+        url,
+        result;
+
+      if (modification_dict.hasOwnProperty('editable')) {
+        if (gadget.state.editable) {
+          url = 'gadget_html5_textarea.html';
+        } else {
+          url = 'gadget_html5_element.html';
+        }
+        result = this.declareGadget(url, {scope: 'sub'})
+          .push(function (input) {
+            // Clear first to DOM, append after to reduce flickering/manip
+            while (element.firstChild) {
+              element.removeChild(element.firstChild);
+            }
+            element.appendChild(input.element);
+            return input;
+          });
+      } else {
+        result = this.getDeclaredGadget('sub');
+      }
+      return result
+        .push(function (input) {
+          return input.render(gadget.state);
+        });
+    })
+
+    .declareMethod('getContent', function () {
+      if (this.state.editable) {
+        return this.getDeclaredGadget('sub')
+          .push(function (gadget) {
+            return gadget.getContent();
+          });
+      }
+      return {};
+    }, {mutex: 'changestate'})
+
+    .declareMethod('checkValidity', function () {
+      if (this.state.editable) {
+        return this.getDeclaredGadget('sub')
+          .push(function (gadget) {
+            return gadget.checkValidity();
+          });
+      }
+      return true;
+    }, {mutex: 'changestate'});
+
+}(window, rJS));

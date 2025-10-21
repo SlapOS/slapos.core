0b20fa55272caba189a70d77c38fe63dfb70526e
Xavier Thompson <xavier.thompson@nexedi.com>
2025-10-21 14:57:21 +0000

    WIP bouture.py

--- slapos/bouture.py
+++ slapos/bouture.py
@@ -1,12 +1,58 @@
+import json
+import os
+
 from slapos.util import dumps
+from slapos.format import Partition
 
-computer_dict = dict(reference='COMP-4530', address=None, netmask=None)
-computer_dict['partition_list'] = [{'reference': 'slappart3', 'address_list': []}]
-slap.registerComputer('COMP-4530').updateConfiguration(dumps(computer_dict))
 
-supply('bogus://sr', 'COMP-4530')
+COMPUTER_ID = 'COMP-4545'
+INSTANCE_ROOT = '/srv/slapgrid'
+RESOURCE = Partition.resource_file
+BOUTURE = 'bouture.json'
+SLAPPART_BASE = 'slappart'
 
-request('bogus://sr', 'bogus-instance')
+
+candidates = []
+for p in os.listdir(INSTANCE_ROOT):
+    if p.startswith(SLAPPART_BASE):
+        try:
+            int(p[len(SLAPPART_BASE):])
+        except ValueError:
+            continue
+        # p is a partition :)
+        if os.path.exists(os.path.join(INSTANCE_ROOT, p, BOUTURE)):
+            candidates.append(p)
+
+partition_id, = candidates
+assert partition_id == 'slappart5'
+# XXX: bouturing multiple partitions will require partition capabilities
+
+
+with open(os.path.join(INSTANCE_ROOT, partition_id, BOUTURE)) as f:
+    bouture = json.load(f)
+
+software_url = bouture['software-url']
+software_type = bouture['software-type']
+instance_name = bouture['instance-title']
+parameter_dict = bouture['config']
+state = bouture['instance-state']
+
+
+with open(os.path.join(INSTANCE_ROOT, partition_id, RESOURCE)) as f:
+    resource = json.load(f)
+
+address_list = resource['address_list']
+tap = resource['tap']
+partition_list = [
+    {'reference': partition_id, 'address_list': address_list, 'tap': tap}
+]
+computer_dict = dict(reference=COMPUTER_ID, address=None, netmask=None)
+computer_dict['partition_list'] = partition_list
+slap.registerComputer(COMPUTER_ID).updateConfiguration(dumps(computer_dict))
+
+supply(software_url, COMPUTER_ID)
+
+request(software_url, instance_name, parameter_dict, software_type, state=state)
 
 # /etc/opt/slapos/slapos.cfg
 """
@@ -22,4 +68,6 @@ database_uri = /etc/opt/slapos/slapproxy.db
 master_url = http://127.0.0.1:4000
 """
 
-# /etc/opt/slapos# slapos console --cfg slapproxy-client.cfg bouture.py
+# slapos console --cfg /etc/opt/slapos/slapproxy-client.cfg bouture.py
+
+# slapos node instance --master-url http://127.0.0.1:4000 --certificate_repository_path /etc/opt/slapos/slapproxy-ssl/

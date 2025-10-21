7210b542a6b17a871af7ca5fd1d54435d4075ae6
Xavier Thompson <xavier.thompson@nexedi.com>
2025-10-21 14:57:02 +0000

    WIP: bouture: Introduce slapos node bouture
    This command grafts partitions from an existing node onto a new master:
    it imports the compute node, the partition and the instance it contains
    into the new master, preserving instance parameters and node parameters
    such as computer id, partition id, partition IPs.
    
    Instances opt-in by placing a `bouture.json` file at the root of their
    partition. This file contains the instance parameters to import in the
    new master: its software url and type, name, parameters, state.
    
    Currently only a single bouture partition is supported: if several
    partitions contain a `bouture.json` file, the command aborts.

---
+++ slapos/cli/bouture.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2010-2014 Vifib SARL and Contributors.
+# All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+import argparse
+
+from slapos.bouture import bouture
+from slapos.cli.config import ConfigCommand
+
+
+class BoutureCommand(ConfigCommand):
+    """
+    bouture node onto a new master
+    """
+    command_group = 'node'
+
+    def get_parser(self, prog_name):
+        ap = super(BoutureCommand, self).get_parser(prog_name)
+
+        ap.add_argument('--new-master-url',
+                        help='Url of new SlapOS Master onto which to bouture')
+
+        return ap
+
+    def take_action(self, args):
+        configp = self.fetch_config(args)
+        node_conf = {}
+        for section in ("slapformat", "slapos"):
+          node_conf.update(configp.items(section))
+        node_conf = argparse.Namespace(**node_conf)
+        bouture(args, node_conf)
+

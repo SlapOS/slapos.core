19182325e2a4bc31916e24e2a327b2fa34823d8b
Xavier Thompson <xavier.thompson@nexedi.com>
2025-10-22 15:22:42 +0000

    WIP bouture: full failover

--- slapos/bouture.py
+++ slapos/bouture.py
@@ -1,15 +1,26 @@
 import argparse
+import errno
 import json
+import logging
 import os
+import subprocess
 
 import slapos.slap.slap
 from slapos.util import dumps
 from slapos.format import Partition
 
+from slapos.grid.slapgrid import SLAPGRID_SUCCESS, SLAPGRID_OFFLINE_SUCCESS
+from slapos.grid.utils import setRunning, setFinished
+
 from six.moves import configparser
+
 
 RESOURCE_FILE = Partition.resource_file
 BOUTURE_FILE = 'bouture.json'
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('bouture')
 
 
 def find_bouture_partitions(instance_root, slappart_base):
@@ -86,13 +97,87 @@ def bouture(bouture_conf, node_conf):
     # Supply bouture SR
     slap.registerSupply().supply(software_url, computer_id)
     # Request bouture instance
-    slap.registerOpenOrder().request(
+    cp = slap.registerOpenOrder().request(
         software_url,
         instance_name,
         parameter_dict,
         software_type,
         state=state,
     )
+    # Bang the boutured instance to ensure it will be reprocessed
+    # It must be reprocessed because the master changed, so the promises need
+    # to be reconfigured to bang the new master, and possibly some parameters
+    # changed as well, depending on the bouture file.
+    cp.bang()
+
+
+def parse_conf(cfg):
+    configp = configparser.ConfigParser()
+    if configp.read(cfg) != [cfg]:
+       raise Exception("Could not read %s" % cfg)
+    node_conf = {}
+    for section in ("slapformat", "slapos"):
+      node_conf.update(configp.items(section))
+    return argparse.Namespace(**node_conf)
+
+
+def failover():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--node-cfg', required=True)
+    parser.add_argument('--new-master-url', required=True)
+    parser.add_argument('--new-cert-dir', required=True)
+    parser.add_argument('--pidfile', required=True)
+    parser.add_argument('--switchfile', required=True)
+    args = parser.parse_args()
+
+    setRunning(logger, args.pidfile)
+    try:
+        cfg = args.node_cfg
+        node_conf = parse_conf(cfg)
+
+        ret = subprocess.call(('slapos', 'node', 'instance', '--cfg', cfg))
+        if ret == SLAPGRID_OFFLINE_SUCCESS:
+            logger.info(
+                "This node is unable to reach its SlapOS master"
+            )
+            try:
+                os.remove(args.switchfile)
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    raise
+            else:
+                logger.info(
+                    "Bouturing this node onto alternate master at %s",
+                    args.new_master_url,
+                )
+                bouture(args, node_conf)
+            logger.info(
+                "Processing this node from alternate master at %s",
+                args.new_master_url,
+            )
+            subprocess.call((
+                'slapos', 'node', 'instance',
+                '--cfg', cfg,
+                '--master-url', args.new_master_url,
+                '--certificate_repository_path', args.new_cert_dir,
+            ))
+        else:
+            # If ret != 0, likely SlapOS master is reachable and some
+            # promise or buildout is failing. If SlapOS master is not
+            # reachable then it's offline processing that failed, and
+            # that means there is a bug in slapos node instance or an
+            # error in starting instance services, and things are bad
+            # anyway.
+            logger.info(
+                "This node seems able to reach its SlapOS master: "
+                "slapos node instance returned %d",
+                ret,
+            )
+            with open(args.switchfile, 'w'):
+                pass
+
+    finally:
+        setFinished(args.pidfile)
 
 
 def main():
@@ -100,21 +185,12 @@ def main():
     parser.add_argument('--node-cfg', required=True)
     parser.add_argument('--new-master-url', required=True)
     args = parser.parse_args()
-
-    configp = configparser.ConfigParser()
-    if configp.read(args.node_cfg) != [args.node_cfg]:
-       raise Exception("Could not read %s" % args.node_cfg)
-
-    node_conf = {}
-    for section in ("slapformat", "slapos"):
-      node_conf.update(configp.items(section))
-    node_conf = argparse.Namespace(**node_conf)
-
+    node_conf = parse_conf(args.node_cfg)
     bouture(args, node_conf)
 
 
 if __name__ == '__main__':
-    main()
+    failover()
 
 
 # /etc/opt/slapos/slapos.cfg

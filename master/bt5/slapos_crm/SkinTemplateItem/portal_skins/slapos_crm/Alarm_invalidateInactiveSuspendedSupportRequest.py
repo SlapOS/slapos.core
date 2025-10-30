9930dab353bf245358f2e0428c8f8c35951349ac
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2025-10-30 09:47:01 +0000

    Testing

--- master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm/Alarm_invalidateInactiveSuspendedSupportRequest.py
+++ master/bt5/slapos_crm/SkinTemplateItem/portal_skins/slapos_crm/Alarm_invalidateInactiveSuspendedSupportRequest.py
@@ -5,7 +5,7 @@ limit_date = DateTime() - 31
 portal.portal_catalog.searchAndActivate(
       portal_type="Support Request",
       simulation_state=["suspended"],
-      strict_resource_uid=NegatedQuery(Query(strict_resource_uid=portal.service_module.slapos_crm_monitoring.getUid())),
+      #strict_resource_uid=NegatedQuery(Query(strict_resource_uid=portal.service_module.slapos_crm_monitoring.getUid())),
       modification_date="<%s" % limit_date,
       method_id='SupportRequest_closeIfInactiveForAMonth',
       method_kw={"activate_kw": {'tag': tag, 'priority': 2}},

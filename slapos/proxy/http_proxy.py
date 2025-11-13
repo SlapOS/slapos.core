0ba63c6eee39fdd31f607c54688d4ea4aa2e713f
Romain Courteaud <romain@nexedi.com>
2025-11-13 15:09:55 +0100

    slapproxy: change http proxy url to allow client to change the backend url path and query string

--- slapos/proxy/http_proxy.py
+++ slapos/proxy/http_proxy.py
@@ -1,13 +1,14 @@
 from flask import current_app, Blueprint, request, url_for
 from werkzeug.exceptions import BadRequest, HTTPException
 import requests
-from .base64_converter import Base64Converter
+from six.moves.urllib.parse import urlparse
+from .absolute_path_converter import AbsolutePathConverter
 
 #########################################
 # Flask Blueprint
 #########################################
 def register_converter(state):
-  state.app.url_map.converters["base64"] = Base64Converter
+  state.app.url_map.converters["absolute_path"] = AbsolutePathConverter
 http_proxy_blueprint = Blueprint('httpproxy', __name__)
 http_proxy_blueprint.record_once(register_converter)
 
@@ -32,30 +33,24 @@ class HTTPTooManyRedirect(HTTPException):
   description = 'Too Many Redirects'
 
 
-@http_proxy_blueprint.route('/proxy/<base64:url>', methods=['GET'])
-def proxy_request(url):
+@http_proxy_blueprint.route('/<url_scheme>/<url_netloc>', methods=['HEAD', 'GET', 'OPTIONS'])
+@http_proxy_blueprint.route('/<url_scheme>/<url_netloc><absolute_path:url_path>', methods=['HEAD', 'GET', 'OPTIONS'])
+def proxy_request(url_scheme, url_netloc, url_path=''):
+  url = urlparse('')._replace(
+    scheme=url_scheme,
+    netloc=url_netloc,
+    path=url_path,
+    query=request.query_string.decode()
+  ).geturl()
   # Accept-Encoding ? Referer ?
   header_white_list = ["Content-Type", "Accept", "Accept-Language", "Range",
                        "If-Modified-Since", "If-None-Match", "User-Agent",
                        # Authorization is required for the stack private stack monitor
-                       "Authorization"]
+                       "Authorization",
+                       # Allow CORS
+                       "Origin"]
 
-  # Fake user access to the document, instead of an ajax query
-  if request.headers.get('Sec-Fetch-Dest', 'empty') == 'empty':
-    proxy_query_header = {
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1'
-    }
-  else:
-    header_white_list.append('Sec-Fetch-Dest')
-    # Add a fake identical referer if not SecFetchDest == Document
-    proxy_query_header = {
-        'Sec-Fetch-Mode': 'same-origin',
-        'Sec-Fetch-Site': 'same-origin',
-    }
-
+  proxy_query_header = {}
   for k, v in dict(request.headers).items():
     if k in header_white_list:
       proxy_query_header[k] = v
@@ -74,14 +69,14 @@ def proxy_request(url):
   except requests.exceptions.InvalidSchema:
     raise BadRequest('"url" must be http')
   except requests.exceptions.SSLError:
-    raise HTTPSSLError()
+    raise HTTPSSLError(url)
   except requests.exceptions.ConnectionError:
-    raise HTTPConnectionError()
+    raise HTTPConnectionError(url)
   except (requests.exceptions.Timeout,
           requests.exceptions.ChunkedEncodingError):
-    raise HTTPTimeout()
+    raise HTTPTimeout(url)
   except requests.exceptions.TooManyRedirects:
-    raise HTTPTooManyRedirect()
+    raise HTTPTooManyRedirect(url)
 #  data=request.stream,
   proxy_response_headers = {
       # If content type is not present, set it to blob/binary
@@ -94,12 +89,25 @@ def proxy_request(url):
              "Vary", "Cache-Control", "Etag", "Accept-Ranges",
              "Content-Range",
              # Authorization is required for the stack private stack monitor 
-             "Www-Authenticate"]:
+             "Www-Authenticate",
+             # Allow CORS
+             "Access-Control-Allow-Origin",
+             "Access-Control-Allow-Methods"
+             "Access-Control-Expose-Headers",
+             "Access-Control-Allow-Headers",
+             "Access-Control-Allow-Credentials"
+             ]:
       proxy_response_headers[k] = v
     elif k == "Location":
+      parsed_v = urlparse(v)
       # In case of redirect, allow to directly fetch from proxy
-      proxy_response_headers[k] = url_for('.proxy_request', url=v,
+      proxy_response_headers[k] = url_for('.proxy_request',
+                                          url_scheme=parsed_v.scheme,
+                                          url_netloc=parsed_v.netloc,
+                                          url_path=parsed_v.path,
                                           _external=True)
+      if parsed_v.query:
+        proxy_response_headers[k] += '?%s' % parsed_v.query
 
   # XSS protection
   # Prevent browser to display untrusted HTML

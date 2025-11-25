f5807da3dc28195761cf94ea13450577ca5fc80c
Romain Courteaud <romain@nexedi.com>
2025-11-25 12:27:10 +0100

    slapproxy: panel: add support to publish a local directory
    If PUBLIC_DIRECTORY_PATH config parameter is set,
    this directory content will be fetchable.

--- slapos/proxy/__init__.py
+++ slapos/proxy/__init__.py
@@ -96,6 +96,8 @@ def setupFlaskConfiguration(conf):
   app.config['run_id'] = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
   if getattr(conf, 'local_software_release_root', None) is not None:
     app.config['local_software_release_root'] = conf.local_software_release_root
+  if getattr(conf, 'public_directory_path', None) is not None:
+    app.config['PUBLIC_DIRECTORY_PATH'] = conf.public_directory_path
 
 def connectDB():
   # if first connection, create an empty db at DATABASE_URI path

d9ca7f57ed5951d7588cf126d0c6db24e6bc0a8a
Romain Courteaud <romain@nexedi.com>
2025-11-25 12:27:10 +0100

    slapproxy: panel: add support to publish a local directory
    If PUBLIC_DIRECTORY_PATH config parameter is set,
    this directory content will be fetchable.

--- slapos/tests/test_slapproxy_panel.py
+++ slapos/tests/test_slapproxy_panel.py
@@ -56,3 +56,21 @@ class PanelTestCase(BasicMixin, unittest.TestCase):
     assert response.headers["Content-Length"] == \
         "107174", \
         response.headers
+
+  def test_panel_public_directory_not_configured(self):
+    response = self.app.get('/panel/public/foo')
+    assert response.status_code == 403, response.status_code
+    assert b'The public directory path is not configured.' in response.data, response.data
+
+  def test_panel_public_directory_configured(self):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      with tempfile.NamedTemporaryFile(dir=tmpdirname) as fp:
+        fp.write(b'Hello world!')
+        fp.seek(0)
+        base_name = os.path.basename(fp.name)
+        self.app_config['PUBLIC_DIRECTORY_PATH'] = tmpdirname
+        response = self.app.get('/panel/public/' + base_name)
+    self.app_config.pop('PUBLIC_DIRECTORY_PATH')
+    assert response.status_code == 200, response.status_code
+    assert b'Hello world!' == response.data, response.data
+    assert response.headers["Content-Disposition"] == "attachment; filename=%s" % base_name, response.headers

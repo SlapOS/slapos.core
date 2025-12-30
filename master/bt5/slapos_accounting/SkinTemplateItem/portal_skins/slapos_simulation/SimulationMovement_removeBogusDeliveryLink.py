ceba742a48cd3b3576e4590114a199d0c505b5dd
Rafael Monnerat <rafael@nexedi.com>
2025-12-30 17:55:58 -0300

    slapos_accounting: Remove unused alarm
      This was for an old bug from 2012, dont seems applicable 13yrs after

--- master/bt5/slapos_accounting/SkinTemplateItem/portal_skins/slapos_simulation/SimulationMovement_removeBogusDeliveryLink.py
+++
@@ -1,4 +0,0 @@
-if context.getDelivery() is not None and context.getDeliveryValue() is None:
-  activate_kw=dict(tag=tag)
-  context.edit(delivery=None, activate_kw=activate_kw)
-  context.expand(expand_policy='immediate', activate_kw=activate_kw)

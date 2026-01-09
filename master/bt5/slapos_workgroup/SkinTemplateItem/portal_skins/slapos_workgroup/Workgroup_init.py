2b988e5c28d93c3002aa8166ac330f91b31589c3
Rafael Monnerat <rafael@nexedi.com>
2026-01-09 17:57:22 +0000

    slapos_workgroup: Set reference on init script

--- master/bt5/slapos_workgroup/SkinTemplateItem/portal_skins/slapos_workgroup/Workgroup_init.py
+++ master/bt5/slapos_workgroup/SkinTemplateItem/portal_skins/slapos_workgroup/Workgroup_init.py
@@ -7,3 +7,13 @@ if not context.hasUserId():
       ),
     ),
   )
+
+if not context.hasReference():
+  context.setReference(
+    "WGROUP-%s" % (
+      context.getPortalObject().portal_ids.generateNewId(
+        id_group='slap_workgroup_reference',
+        id_generator='uid',
+      ),
+    ),
+  )

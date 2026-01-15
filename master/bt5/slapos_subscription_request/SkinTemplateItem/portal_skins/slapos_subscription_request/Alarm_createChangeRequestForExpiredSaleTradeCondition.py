5e90ba5a593ddfe9464cc464153b7bb94e5f9a60
Romain Courteaud <romain@nexedi.com>
2026-01-15 09:41:07 +0000

    slapos_subscription_request: add an alarm to generate Subscription Change Request from expired Sale Trade Condition
    Keep the original Open Order price in such case.

--- master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/Alarm_createChangeRequestForExpiredSaleTradeCondition.py
+++ master/bt5/slapos_subscription_request/SkinTemplateItem/portal_skins/slapos_subscription_request/Alarm_createChangeRequestForExpiredSaleTradeCondition.py
@@ -1,6 +1,7 @@
 from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
 
 portal = context.getPortalObject()
+ledger_uid = portal.portal_categories.ledger.automated.getUid()
 
 trade_condition_list = portal.portal_catalog(
   portal_type='Sale Trade Condition',
@@ -19,6 +20,18 @@ portal.portal_catalog.searchAndActivate(
   specialise__uid=trade_condition_uid_list,
 
   method_id='SaleTradeCondition_generateNewVersionFromExpiredSaleTradeCondition',
+  method_kw={'activate_kw': {'tag': tag, 'priority': 5}},
+  activate_kw={'tag': tag}
+)
+
+portal.portal_catalog.searchAndActivate(
+  portal_type='Open Sale Order',
+  ledger__uid=ledger_uid,
+  validation_state='validated',
+  expiration_date=None,
+  specialise__uid=trade_condition_uid_list,
+
+  method_id='OpenSaleOrder_generateSubscriptionChangeRequestFromExpiredSaleTradeCondition',
   method_kw={'activate_kw': {'tag': tag, 'priority': 5}},
   activate_kw={'tag': tag}
 )

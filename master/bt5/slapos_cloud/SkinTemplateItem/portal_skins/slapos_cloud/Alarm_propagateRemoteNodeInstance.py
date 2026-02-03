cb32ec2ded323c4263d003a1c279fbb229629285
Romain Courteaud <romain@nexedi.com>
2026-02-03 13:43:31 +0000

    slapos_cloud: Alarm_propagateRemoteNodeInstance: only checked instances indexed since previous run

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_propagateRemoteNodeInstance.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Alarm_propagateRemoteNodeInstance.py
@@ -1,4 +1,9 @@
+from Products.ZSQLCatalog.SQLCatalog import SimpleQuery
+
 portal = context.getPortalObject()
+
+activate_kw={'tag': tag, 'priority': 2}
+previous_run_date = context.Alarm_storeCurrentRunDateAndReturnPreviousRunDate(params)
 
 portal.portal_catalog.searchAndActivate(
   portal_type='Compute Partition',
@@ -9,8 +14,29 @@ portal.portal_catalog.searchAndActivate(
   # This alarm bruteforce checking all documents,
   # without changing them directly.
   # Increase priority to not block other activities
-  method_kw={"activate_kw": {'tag': tag, 'priority': 2}},
-  activate_kw={'tag': tag, 'priority': 2}
+  method_kw={
+    "previous_run_date": previous_run_date,
+    "activate_kw": activate_kw
+  },
+  activate_kw=activate_kw
 )
+
+if previous_run_date is not None:
+  # In such case, the date criteria will only check compute partition
+  # with recently modified instance
+  # It is needed to search all _remote_ instances
+  portal.portal_catalog.searchAndActivate(
+    portal_type='Instance Tree',
+    title='\\_remote\\_%\\_%',
+    indexation_timestamp=SimpleQuery(
+      indexation_timestamp=previous_run_date,
+      comparison_operator=">="
+    ),
+    method_id='InstanceTree_propagateFromRemoteNode',
+    method_kw={
+      "activate_kw": activate_kw
+    },
+    activate_kw=activate_kw
+  )
 
 context.activate(after_tag=tag).getId()

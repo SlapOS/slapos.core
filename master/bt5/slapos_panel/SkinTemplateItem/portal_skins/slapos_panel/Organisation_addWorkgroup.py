23406fe7b4fb23e31f3ca61bfc6e564093c679e1
Rafael Monnerat <rafael@nexedi.com>
2026-03-05 20:21:53 +0000

    slapos_panel: Move action to create workgroups to Organisation
       Use this action to define a default organisation to the workgroup whenever this one joins a project.

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Organisation_addWorkgroup.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Organisation_addWorkgroup.py
@@ -1,19 +1,22 @@
-person = context
+organisation = context
 portal = context.getPortalObject()
 translate = portal.Base_translateString
 
-assert person.getPortalType() == "Person"
+assert organisation.getPortalType() == "Organisation"
 
 workgroup = portal.workgroup_module.newContent(
   portal_type='Workgroup',
   title=title,
-  destination_value=person
+  # This destination section is used whenever we generate a
+  # Sale Trade Condition.
+  destination_section_value=organisation,
+  activate_kw=activate_kw
 )
 
 if len(workgroup.checkConsistency()) != 0:
   raise AssertionError(workgroup.checkConsistency()[0])
 
-workgroup.submit(comment='Created by %s' % person.getRelativeUrl())
+workgroup.submit(comment='Created by %s' % organisation.getRelativeUrl())
 if batch:
   return workgroup
 

b57ad587500c1b9dba4e21240a9231b0aa70ee11
Rafael Monnerat <rafael@nexedi.com>
2026-03-26 19:52:50 +0000

    slapos_panel: Missing commit about test name conflict when join

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Person_acceptSlapOSInvitationToken.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Person_acceptSlapOSInvitationToken.py
@@ -18,9 +18,11 @@ if (invitation_token is None) or (invitation_token.getValidationState() != 'vali
 assert invitation_token.getPortalType() == 'Invitation Token'
 follow_up_value = invitation_token.getFollowUpValue()
 category = 'destination_project'
+instance_to_claim_list = []
 if follow_up_value.getPortalType() == 'Workgroup':
   category = 'destination'
-  if not accept_claim and person.Person_getInstanceTreeListToClaim(invitation_token.getId()):
+  instance_to_claim_list = person.Person_getInstanceTreeListToClaim(invitation_token.getId())
+  if not accept_claim and len(instance_to_claim_list) > 0:
     return person.Base_redirect(
       'Person_viewAcceptSlapOSInvitationTokenWithClaimDialog',
       keep_items={
@@ -48,7 +50,19 @@ assignment_request = portal.assignment_request_module.newContent(
 if len(assignment_request.checkConsistency()) != 0:
   raise AssertionError(assignment_request.checkConsistency()[0])
 
-assignment_request.submit(comment='Created Invitation Token: %s' % invitation_token.getRelativeUrl())
+if len(instance_to_claim_list) == 0:
+  # Nothing blocking move foward
+  assignment_request.submit(comment='Created Invitation Token: %s' % invitation_token.getRelativeUrl())
+else:
+  assignment_request.activate().AssignmentRequest_testNameConflictBeforeSubmit(
+    comment='Created Invitation Token: %s' % invitation_token.getRelativeUrl())
+  return person.Base_redirect(
+    'view',
+    keep_items={
+      'portal_status_message': translate('Your request has been submitted for review and will be processed shortly.'),
+    }
+  )
+
 invitation_token.invalidate(comment='Created by Assignment Request: %s' % assignment_request.getRelativeUrl())
 if batch:
   return person

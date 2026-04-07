fb002122f22a66e2a9566de011c508efef33e4d2
Rafael Monnerat <rafael@nexedi.com>
2026-04-07 18:54:52 +0000

    slapos_workgroup: Do not hardcode function specifically.
       Make it generic so we can use any function on assignments.

--- master/bt5/slapos_workgroup/SkinTemplateItem/portal_skins/slapos_workgroup/AssignmentRequest_suspendDuplicatedAssignmentRequest.py
+++ master/bt5/slapos_workgroup/SkinTemplateItem/portal_skins/slapos_workgroup/AssignmentRequest_suspendDuplicatedAssignmentRequest.py
@@ -14,23 +14,28 @@ if workgroup is None or person is None:
 workgroup_assignment_request_list = portal.portal_catalog(
     portal_type='Assignment Request',
     simulation_state='validated',
-    destination_decision__uid=workgroup.getUid(),
-    function__uid=portal.portal_categories.function.production.customer.getUid()
+    destination_decision__uid=workgroup.getUid()
   )
 
 if not len(workgroup_assignment_request_list):
   return
 
-project_uid_list = [
-  x.getDestinationProjectUid() for x in workgroup_assignment_request_list
-    if x.getDestinationProject() is not None]
+function_map = {}
+for assignment_request in workgroup_assignment_request_list:
+  function_uid = assignment_request.getFunctionUid()
+  if function_uid and assignment_request.getDestinationProject():
+    if function_uid in function_map:
+      function_map[function_uid].append(assignment_request.getDestinationProjectUid())
+    else:
+      function_map[function_uid] = [assignment_request.getDestinationProjectUid()]
 
-query_kw = {
-  'portal_type': 'Assignment Request',
-  'simulation_state': 'validated',
-  'destination_project__uid': project_uid_list,
-  'destination_decision__uid': person.getUid(),
-  'function__uid': portal.portal_categories.function.production.customer.getUid()
-}
-for assignment_request in portal.portal_catalog(**query_kw):
-  assignment_request.suspend(comment='Suspended after join %s' % workgroup.getReference())
+for function_uid in function_map:
+  query_kw = {
+    'portal_type': 'Assignment Request',
+    'simulation_state': 'validated',
+    'destination_project__uid': function_map[function_uid],
+    'destination_decision__uid': person.getUid(),
+    'function__uid': function_uid
+  }
+  for assignment_request in portal.portal_catalog(**query_kw):
+    assignment_request.suspend(comment='Suspended after join %s' % workgroup.getReference())

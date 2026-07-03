6942776aebf7386456d0b31ef083b6f3a562670d
Rafael Monnerat <rafael@nexedi.com>
2026-07-03 17:26:07 -0300

    slapos_cloud: Those scripts are doing nothing.

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Organisation_afterClone.py
+++
@@ -1,8 +0,0 @@
-"""Hook called when a compute_node object is closed.
-
-We want to reset reference, which is the user login in ERP5Security.
-One exception is when a person object is installed from business template.
-"""
-if context.getPortalType() != "Organisation":
-  return
-context.Organisation_init()

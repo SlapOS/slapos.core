9a7704d7bed1a60f9a7b4d064685fd7a97b45d03
Romain Courteaud <romain@nexedi.com>
2026-07-20 14:19:01 +0000

    slapos_panel: allow project's manager to contact the instance owner

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_addSlapOSSupportRequest.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Base_addSlapOSSupportRequest.py
@@ -12,11 +12,19 @@ if context.getPortalType() == 'Project':
 else:
   project = context.getFollowUpValue()
 
+if context.getPortalType() == 'Instance Tree':
+  # Explicitely set the instance owner, so that project's manager
+  # can contact the customer
+  destination_decision = context.getDestinationSection()
+else:
+  destination_decision = project.getDestination()
+
 support_request = person.Entity_createTicketFromTradeCondition(
   resource,
   title,
   description,
   portal_type='Support Request',
+  destination_decision=destination_decision,
   source_project=project.getRelativeUrl(),
   causality=context.getRelativeUrl()
 )

80020b91f245781947fe259cc48ee42b2ab0fdc7
Romain Courteaud <romain@nexedi.com>
2026-07-23 09:30:14 +0000

    slapos_cloud: fixup...
    slapos_cloud: fixup https://lab.nexedi.com/nexedi/slapos.core/-/commit/59398a53e80fb281d58cb3979df11b4e0a641b87?merge_request_iid=842
    
    /reviewed-on https://lab.nexedi.com/rafael/slapos.core/-/merge_requests/3

--- master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Project_getUserWorkgroup.py
+++ master/bt5/slapos_cloud/SkinTemplateItem/portal_skins/slapos_cloud/Project_getUserWorkgroup.py
@@ -1,10 +1,16 @@
 portal = context.getPortalObject()
 
-# First check if project_reference is provided by the user.
 member = portal.portal_membership.getAuthenticatedMember()
 getGroups = getattr(member, 'getGroups', None)
 if getGroups is None:
-  return None
+  if not fallback_user:
+    return None
+  # We are using a manager, so use fallback_user
+  if member.getUserValue() is None:
+    member = portal.acl_users.getUserById(fallback_user.getUserId())
+    getGroups = getattr(member, 'getGroups', None)
+    if getGroups is None:
+      return None
 
 workgroup = None
 user_group_list = getGroups()
@@ -14,7 +20,7 @@ for user_group in user_group_list:
   # provided by the workgroup.
   if user_group.endswith(workgroup_customer_on_project_user_group):
     workgroup_user_id = user_group[:-len(workgroup_customer_on_project_user_group)]
-    if workgroup is not None and workgroup.getUserId() != workgroup_user_id:
+    if (workgroup is not None) and (workgroup.getUserId() != workgroup_user_id):
       raise ValueError("Duplicated found: %s, %s" % (
         workgroup_user_id, workgroup.getUserId()))
 
@@ -22,4 +28,5 @@ for user_group in user_group_list:
       workgroup = portal.portal_catalog.getResultValue(
         portal_type='Workgroup',
         user_id=workgroup_user_id)
-return None
+
+return workgroup

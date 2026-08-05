d7c1d59749d9719d0f0bb707e8f55498624167c9
Romain Courteaud <romain@nexedi.com>
2026-08-05 14:54:45 +0200

    slapos_erp5: implement Workgroup local roles

--- master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5GroupRoleSecurity.py
+++ master/bt5/slapos_erp5/TestTemplateItem/portal_components/test.erp5.testSlapOSERP5GroupRoleSecurity.py
@@ -2052,6 +2052,40 @@ class TestPerson(TestSlapOSGroupRoleSecurityMixin):
     self.assertRoles(delivery, delivery.getUserId(), ['Assignee'])
     self.assertRoles(delivery, 'SHADOW-%s' % delivery.getUserId(), ['Auditor'])
 
+class TestWorkgroupModule(TestSlapOSGroupRoleSecurityMixin):
+  def test_WorkgroupModule(self):
+    module = self.portal.workgroup_module
+    self.assertSecurityGroup(module,
+        ['F-SALE*', 'F-ACCOUNTING*', 'F-CUSTOMER', 'F-PRODUCTION*',
+         'R-SHADOW-PERSON', module.Base_getOwnerId()], False)
+    self.assertRoles(module, 'F-SALE*', ['Auditor', 'Author'])
+    self.assertRoles(module, 'F-ACCOUNTING*', ['Auditor'])
+    self.assertRoles(module, 'F-CUSTOMER', ['Auditor'])
+    self.assertRoles(module, 'R-SHADOW-PERSON', ['Auditor'])
+    self.assertRoles(module, 'F-PRODUCTION*', ['Auditor'])
+    self.assertRoles(module, module.Base_getOwnerId(), ['Owner'])
+
+
+class TestWorkgroup(TestSlapOSGroupRoleSecurityMixin):
+
+  def test_Workgroup_selfWorkgroup(self):
+    document = self.portal.workgroup_module.newContent(
+        portal_type='Workgroup')
+    self.assertSecurityGroup(document,
+        ['F-ACCMAN', 'F-SALEAGT', 'F-ACCAGT', 'F-SALEMAN',
+         document.getUserId(),
+         # XXX RAFAEL: Remove SHADOW if this is not required
+         'SHADOW-%s' % document.getUserId(),
+         self.user_id], False)
+    self.assertRoles(document, self.user_id, ['Owner'])
+    self.assertRoles(document, 'F-SALEAGT', ['Assignee'])
+    self.assertRoles(document, 'F-SALEMAN', ['Assignor'])
+    self.assertRoles(document, 'F-ACCMAN', ['Auditor'])
+    self.assertRoles(document, 'F-ACCAGT', ['Auditor'])
+    self.assertRoles(document, document.getUserId(), ['Auditor'])
+    # XXX RAFAEL: Remove SHADOW if this is not required
+    self.assertRoles(document, 'SHADOW-%s' % document.getUserId(), ['Auditor'])
+
 
 class TestERP5Login(TestSlapOSGroupRoleSecurityMixin):
   def test_ERP5Login_selfUser(self):

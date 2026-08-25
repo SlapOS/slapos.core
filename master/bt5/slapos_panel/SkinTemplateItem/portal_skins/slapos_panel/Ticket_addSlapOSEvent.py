258bdd16b9018552c2d441f6c4f23109498bbb36
Romain Courteaud <romain@nexedi.com>
2026-08-25 18:42:10 +0200

    slapos_panel: add event for workgroup

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Ticket_addSlapOSEvent.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Ticket_addSlapOSEvent.py
@@ -12,7 +12,20 @@ if int(REQUEST.getHeader('Content-Length', 0)) > 3145728:
   return ""
 
 if person_relative_url == ticket.getDestination():
+  # ticket to a person
   direction = 'incoming'
+elif len(portal.portal_catalog(
+  portal_type='Assignment Request',
+  simulation_state='validated',
+  destination__relative_url=ticket.getDestination(),
+  destination_decision__uid=person.getUid(),
+  limit=1
+)) == 1:
+  # current person is member of ticket workgroup
+  direction = 'incoming'
+  # Keep workgroup as source, in order to allow
+  # all workgroup members to see the event
+  person_relative_url = ticket.getDestination()
 else:
   direction = 'outgoing'
 

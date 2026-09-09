42f7197bc5a468f18e9b6db11815e6c329125969
Romain Courteaud <romain@nexedi.com>
2026-09-09 11:23:16 +0000

    slapos_panel: set workgroup on remote node if it exists

--- master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Project_addSlapOSRemoteNode.py
+++ master/bt5/slapos_panel/SkinTemplateItem/portal_skins/slapos_panel/Project_addSlapOSRemoteNode.py
@@ -3,12 +3,15 @@ from Products.ERP5Type.Message import translateString
 portal = context.getPortalObject()
 project = context
 person = portal.portal_membership.getAuthenticatedMember().getUserValue()
+project_value = portal.restrictedTraverse(destination_project)
 
 remote_node = portal.compute_node_module.newContent(
   portal_type='Remote Node',
   title=title,
-  destination_section_value=person,
-  destination_project_value=portal.restrictedTraverse(destination_project),
+  # directly set the workgroup, to reduce number of migrations
+  # if user leaves the project
+  destination_section_value=person.getTypeBasedMethod('getSlaposActorRequester')(project_value),
+  destination_project_value=project_value,
   follow_up_value=project,
   allocation_scope='open'
 )
